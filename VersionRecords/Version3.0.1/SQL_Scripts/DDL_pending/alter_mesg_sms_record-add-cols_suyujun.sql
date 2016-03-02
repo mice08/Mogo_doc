77453e6a29bf11b8ab317aa1b23dfe25cf349bae
yb <yb@mogoroom.com>
2016-03-02 20:54:59 +0800

    new#：提交sql脚本

--- VersionRecords/Version3.0.1/SQL_Scripts/DDL_pending/alter_mesg_sms_record-add-cols_suyujun.sql
+++
@@ -1,8 +0,0 @@
-/*新增字段，保存第三方接口返回的相关信息*/
-use mogoroomdb;
-
-ALTER TABLE mesg_sms_record ADD COLUMN outerMesgId VARCHAR(32) COMMENT '短信编号',
-ADD COLUMN outerStatus INT(1) COMMENT '短信发送结果',
-ADD COLUMN outerDesc VARCHAR(32) COMMENT '短信发送结果描述',
-ADD COLUMN outerWgcode VARCHAR(20) COMMENT '运营商网关返回的原始值',
-ADD COLUMN outerTime datetime COMMENT '状态报告接收时间';
