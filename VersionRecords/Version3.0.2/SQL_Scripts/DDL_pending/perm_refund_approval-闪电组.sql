661c37c3e1d03b61807f727d6ddbcad561feae68
yb <yb@mogoroom.com>
2016-03-10 15:14:23 +0800

    修改脚本

--- VersionRecords/Version3.0.2/SQL_Scripts/DDL_pending/perm_refund_approval-闪电组.sql
+++ VersionRecords/Version3.0.2/SQL_Scripts/DDL_pending/perm_refund_approval-闪电组.sql
@@ -6,10 +6,12 @@ drop table if exists perm_refund_approval;
 create table perm_refund_approval
 (
    id                   int(11) not null auto_increment comment '退款审核表id',
+   billId               int(11) not null comment '订单id',
+   billType             tinyint(2) not null comment '订单类型',
    landlordId           int(11) not null comment '房东id',
    renterId             int(11) not null comment '租客id',
    roomId               int(11) not null comment '退款对应的房源id',
-   refundType           int(2) not null comment '退款记录产生来源',
+   refundSourceType     tinyint(2) not null comment '退款来源类型(参考字典表组名:refundSourceType)  ',
    refundAmount         decimal(10,2) not null comment '退款总金额',
    refundReason         varchar(300) comment '退款原因',
    approvalStatus       int(1) not null default 0 comment '退款审批状态(0:待审批 1:审批通过 2:审批不通过)',
@@ -21,14 +23,3 @@ create table perm_refund_approval
    approvalTime         datetime comment '退款审批时间',
    primary key (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='退款审批表';
-
-
-drop table if exists perm_refund_detail;
-create table perm_refund_detail
-(
-   id                   int(11) not null auto_increment comment '退款明细id',
-   approvalId           int(11) not null comment '退款审批表id',
-   refundType           int(11) not null comment '退款类型，(参考acct_billdtltype)  ',
-   refundAmount         decimal(10,2) not null comment '退款金额',
-   primary key (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='退款审批明细表';
