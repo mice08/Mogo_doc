7460b2a2e892048628b68a93bbd8ef125809050a
mxm <mxm@mogoroom.com>
2016-03-16 14:22:12 +0800

    new@3.0.3 add cols and index

--- VersionRecords/Version3.0.3/SQL_Scripts/DDL_reviewed/flat_roompricebase-acct_busibilldtl-mxm.sql
+++ VersionRecords/Version3.0.3/SQL_Scripts/DDL_reviewed/flat_roompricebase-acct_busibilldtl-mxm.sql
@@ -6,7 +6,7 @@ DROP INDEX `idx_goodsId_goodsIdType` ,
 ADD UNIQUE INDEX `idx_goodsId_goodsIdType` (`goodsId`, `goodsIdType`) USING BTREE ;
 
 # 给acct_busibilldtl增加滞纳金总额字段
-ALTER TABLE `acct_busibilldtl`
+ALTER TABLE acct.`acct_busibilldtl`
 MODIFY COLUMN `beforeAmount`  decimal(10,2) NOT NULL COMMENT '账单支付前的未付金额' AFTER `amount`,
 ADD COLUMN `penaltyAmount`  decimal(10,2) NULL COMMENT '账单支付前的滞纳金总额' AFTER `beforeAmount`;
 
