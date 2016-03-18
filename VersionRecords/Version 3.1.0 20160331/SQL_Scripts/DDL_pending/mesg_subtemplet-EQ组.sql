088e46641d07fbe313f2b90cce8712dfd9a1162f
yb <yb@mogoroom.com>
2016-03-18 15:33:51 +0800

    修改脚本

--- VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DDL_pending/mesg_subtemplet-EQ组.sql
+++ VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DDL_pending/mesg_subtemplet-EQ组.sql
@@ -2,3 +2,5 @@
 use mogoroomdb;
 
 ALTER TABLE mesg_subtemplet MODIFY COLUMN templetContent VARCHAR(3000) NOT NULL COMMENT '模板内容';
+
+ALTER TABLE mesg_email_tocheck MODIFY COLUMN emailContent VARCHAR(3000) NOT NULL COMMENT '邮件内容';
