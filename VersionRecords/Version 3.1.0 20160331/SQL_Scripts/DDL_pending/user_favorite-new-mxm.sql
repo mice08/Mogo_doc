75baaf119c3ea31b6cd25593c34f8f7a14c00f00
mxm <mxm@mogoroom.com>
2016-03-21 11:42:11 +0800

    new@3.1.0#:1.acct报表不需要建视图。2.用户收藏表ddl

---
+++ VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DDL_pending/user_favorite-new-mxm.sql
@@ -0,0 +1,21 @@
+use mogoroomdb;
+
+CREATE TABLE IF NOT EXISTS `user_favorite` (
+  `id` INT(11) NOT NULL COMMENT '主键自增Id',
+  `userId` INT(11) NOT NULL COMMENT '用户Id',
+  `userType` TINYINT(3) NOT NULL COMMENT '用户类型，groupName=userType',
+  `favoriteType` TINYINT(3) NOT NULL COMMENT '收藏项类型，groupName=userFavoriteType',
+  `favoriteId` INT(11) NOT NULL COMMENT '收藏项Id,例如roomId, topicId',
+  `status` TINYINT(3) NOT NULL COMMENT '状态位 0:无效 1:有效 ',
+  `channel` TINYINT(3) NOT NULL,
+  `createBy` INT(11) NOT NULL COMMENT '创建人',
+  `createByType` TINYINT(3) NOT NULL COMMENT '创建人员类型，groupName=userType',
+  `createTime` DATETIME NOT NULL COMMENT '创建时间',
+  `updateBy` INT(11) NOT NULL COMMENT '修改人员',
+  `updateByType` TINYINT(3) NOT NULL COMMENT '更新人员类型，groupName=userType',
+  `updateTime` DATETIME NOT NULL DEFAULT current_timestamp COMMENT '更新时间',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+COMMENT = '用户收藏表，存放用户感兴趣的房子Id,图片Id,';
