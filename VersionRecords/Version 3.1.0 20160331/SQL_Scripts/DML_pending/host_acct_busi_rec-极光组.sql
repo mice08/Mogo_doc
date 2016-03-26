9a3ca183e8b2b404d5a0ac404a143fe2a2c41b43
yb <yb@mogoroom.com>
2016-03-26 17:46:18 +0800

    修改

--- VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DML_pending/host_acct_busi_rec-极光组.sql
+++ VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DML_pending/host_acct_busi_rec-极光组.sql
@@ -1,4 +1,4 @@
-/*  Database name `mogoroomdb` ，host_acct_busi_rec的hostFlatRelationId初始化*/
+/*  Database name `mogoroomdb` ，host_acct_busi_rec的hostFlatRelId初始化*/
 use mogoroomdb;
 /*第一步*/
 UPDATE `host_acct_busi_rec` t1,
@@ -7,8 +7,8 @@ FROM `host_acct_busi_rec` t1
 LEFT JOIN `host_flats_relation` t2 
 ON t1.`host_id`=t2.`host_id` AND t1.`flat_id`=t2.`flat_id` AND t1.`landlord_id`=t2.`landlord_id` 
 	AND t1.`create_time`>=DATE_ADD(t2.`create_time`, INTERVAL -1 SECOND) AND t1.`create_time`<=DATE_ADD(t2.`create_time`, INTERVAL 1 SECOND)
-WHERE t1.`STATUS`=1 AND t1.`hostFlatRelationId` IS NULL) t2
-SET t1.hostFlatRelationId = t2.id 
+WHERE t1.`STATUS`=1 AND t1.`hostFlatRelId` =0) t2
+SET t1.hostFlatRelId = t2.id 
 WHERE t1.`host_id`=t2.host_id AND t1.`flat_id`=t2.flat_id AND t1.`landlord_id`=t2.landlord_id;
 
 
@@ -19,6 +19,6 @@ FROM `host_acct_busi_rec` t1
 LEFT JOIN `host_flats_relation` t2 
 ON t1.`host_id`=t2.`host_id` AND t1.`flat_id`=t2.`flat_id` AND t1.`landlord_id`=t2.`landlord_id` 
 	AND t1.`create_time`>=DATE_ADD(t2.`create_time`, INTERVAL -1 MINUTE) AND t1.`create_time`<=DATE_ADD(t2.`create_time`, INTERVAL 1 MINUTE)
-WHERE t1.`STATUS`=1 AND t1.`hostFlatRelationId` IS NULL) t2
-SET t1.hostFlatRelationId = t2.id 
+WHERE t1.`STATUS`=1 AND t1.`hostFlatRelId` =0) t2
+SET t1.hostFlatRelId = t2.id 
 WHERE t1.`host_id`=t2.host_id AND t1.`flat_id`=t2.flat_id AND t1.`landlord_id`=t2.landlord_id;
