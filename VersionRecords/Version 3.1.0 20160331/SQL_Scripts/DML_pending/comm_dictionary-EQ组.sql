4a28b718a394ed26cfe1a342b94ff376c51082c0
yb <yb@mogoroom.com>
2016-03-29 15:26:11 +0800

    修改

--- VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DML_pending/comm_dictionary-EQ组.sql
+++ VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DML_pending/comm_dictionary-EQ组.sql
@@ -1,8 +1,8 @@
 /* Database name `mogoroomdb` , 3.1.0版本app消息推送增加四种消息logo记录，3.1.0版本app消息推送更新推送消息所属类型*/
-INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`) VALUES ('denglu.png', 'msgico', '1', '11', '登录'); 
-INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`) VALUES ('tousu.png', 'msgico', '1', '12', '投诉'); 
-INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`) VALUES ('zufang.png', 'msgico', '1', '13', '租房'); 
-INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`) VALUES ('zuke.png', 'msgico', '1', '14', '欢迎租客'); 
+INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`,`enValue`) VALUES ('denglu.png', 'msgico', '1', '11', '登录',''); 
+INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`,`enValue`) VALUES ('tousu.png', 'msgico', '1', '12', '投诉',''); 
+INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`,`enValue`) VALUES ('zufang.png', 'msgico', '1', '13', '租房',''); 
+INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`,`enValue`) VALUES ('zuke.png', 'msgico', '1', '14', '欢迎租客',''); 
 
 
 #更新发送短信子模板(蘑菇宝审核资料有误)
