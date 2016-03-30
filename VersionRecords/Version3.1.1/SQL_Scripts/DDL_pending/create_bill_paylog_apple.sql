42f8aaf7e6bcdc05364fe9e5aa9969fd1b992832
yb <yb@mogoroom.com>
2016-03-30 16:29:38 +0800

    修改

--- VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/create_bill_paylog_apple.sql
+++ VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/create_bill_paylog_apple.sql
@@ -1,10 +1,11 @@
+/*  Database name `mogoroomdb` ，新建支付回调记录表*/
 use mogoroomdb;
 /*==============================================================*/
 /* Table: bill_paylog_apple                                     */
 /*==============================================================*/
 create table bill_paylog_apple
 (
-   id                   int(11) not null comment '支付记录表id',
+   id                   int(11) auto_increment not null comment '支付记录表id',
    sign                 varchar(255) not null comment '签名',
    sign_type            varchar(10) not null comment '签名算法',
    oid_partner          varchar(32) not null comment '商户编号',
@@ -18,9 +19,7 @@ create table bill_paylog_apple
    pay_type             varchar(10) not null comment '支付方式',
    create_time          datetime not null comment '记录创建时间',
    primary key (id)
-);
-
-alter table bill_paylog_apple comment '连连支付回调记录表';
+)ENGINE=InnoDB COMMENT='连连支付回调记录表';
 
 /*==============================================================*/
 /* Index: idx_oid_paybill                                       */
