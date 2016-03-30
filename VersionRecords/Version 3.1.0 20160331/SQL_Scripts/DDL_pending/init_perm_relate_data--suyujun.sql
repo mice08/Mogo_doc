c5f1a5e4b552bcc770b3afd87afc5dabe51bc835
suyujun <syj@mogoroom.com>
2016-03-30 19:49:48 +0800

    opt@3.1.0#:修改权限初始化脚本

--- VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DDL_pending/init_perm_relate_data--suyujun.sql
+++ VersionRecords/Version 3.1.0 20160331/SQL_Scripts/DDL_pending/init_perm_relate_data--suyujun.sql
@@ -1,15 +1,5 @@
 use mogoroomdb;
 
-DELETE FROM perm_functioninfo where channel in(4,5);
-
-DELETE FROM perm_role where channel = 5;
-
-DELETE FROM perm_menu_group where menuGroupName='房东PC子账号菜单分组' or menuGroupName='房东APP子账号菜单分组';
-
-DELETE FROM perm_menu_group_rel;
-
-DELETE FROM perm_menugroup_role_rel;
-
 /*房东默认的子账号初始化*/
 INSERT INTO perm_role(roleName,status,createTime,createEmp,channel) VALUES('子账号默认角色',1,NOW(),0,5);
 
