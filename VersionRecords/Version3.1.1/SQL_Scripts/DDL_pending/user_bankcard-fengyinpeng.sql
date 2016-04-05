60a638da25ca2f198f7b1f22a550ed5798d5fb0c
fyp <charles@mogoroom.com>
2016-04-05 15:05:01 +0800

    opt@3.1.1#:格式修改

--- VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/user_bankcard-fengyinpeng.sql
+++ VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/user_bankcard-fengyinpeng.sql
@@ -1,3 +1,2 @@
 use mogoroomdb;
-ALTER TABLE   `user_bankcard` ADD COLUMN  isCorp INT(2) COMMENT '银行卡
-归属于(0:个人 1:公司)';
+ALTER TABLE   `user_bankcard` ADD COLUMN  isCorp INT(2) COMMENT '银行卡归属于(0:个人 1:公司)';
