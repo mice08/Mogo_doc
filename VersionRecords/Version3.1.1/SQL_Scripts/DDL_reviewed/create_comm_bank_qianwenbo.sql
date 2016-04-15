9eed2076979551485504ea28b077ae16d0d30ffb
qianwenbo <qwb@mogoroom.com>
2016-04-15 11:53:25 +0800

    opt@3.1.1#:修改创建银行卡表的sql

--- VersionRecords/Version3.1.1/SQL_Scripts/DDL_reviewed/create_comm_bank_qianwenbo.sql
+++ VersionRecords/Version3.1.1/SQL_Scripts/DDL_reviewed/create_comm_bank_qianwenbo.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS `mydb`.`comm_bank` (
+﻿use mogoroomdb;
+CREATE TABLE IF NOT EXISTS `comm_bank` (
   `id` INT NOT NULL,
   `bankName` VARCHAR(25) NOT NULL COMMENT '银行名称',
   `bankNameEn` VARCHAR(8) NOT NULL COMMENT '银行名称英文缩写',
@@ -6,3 +7,4 @@ CREATE TABLE IF NOT EXISTS `mydb`.`comm_bank` (
   `valid` BIT(1) NULL DEFAULT '1' COMMENT '是否可用。1代表可用于绑卡',
   `sort` INT NULL COMMENT '排序标识',
   PRIMARY KEY (`id`))
+ ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='银行卡信息表';
