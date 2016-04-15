faa8cc38780512f604ae7de3bd0d51b338c49ae3
qianwenbo <qwb@mogoroom.com>
2016-04-15 11:49:55 +0800

    add@3.1.1#:添加创建银行卡表的sql

---
+++ VersionRecords/Version3.1.1/SQL_Scripts/DDL_reviewed/create_comm_bank_qianwenbo.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`comm_bank` (
+  `id` INT NOT NULL,
+  `bankName` VARCHAR(25) NOT NULL COMMENT '银行名称',
+  `bankNameEn` VARCHAR(8) NOT NULL COMMENT '银行名称英文缩写',
+  `status` BIT(1) NULL DEFAULT '1' COMMENT '是否有效。0代表该数据已废除',
+  `valid` BIT(1) NULL DEFAULT '1' COMMENT '是否可用。1代表可用于绑卡',
+  `sort` INT NULL COMMENT '排序标识',
+  PRIMARY KEY (`id`))
