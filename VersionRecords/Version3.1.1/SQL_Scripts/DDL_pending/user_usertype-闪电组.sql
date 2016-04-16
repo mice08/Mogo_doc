c0e88b930526f8f8bb0589a2d8278e72391f1ca9
ad <ad@mogoroom.com>
2016-04-16 15:33:19 +0800

    new@3.1.1#:usertype初始化脚本

--- VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/user_usertype-闪电组.sql
+++ VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/user_usertype-闪电组.sql
@@ -8,9 +8,3 @@ insert into user_usertype(userId, userType, soDoneCode) select id, 6, id+9000000
 insert into user_usertype(userId, userType, soDoneCode) select id, 0, id+900000000 from user_info where id >= 3000000 and id < 4000000;
 /*租客*/
 insert into user_usertype(userId, userType, soDoneCode) select id, 4, id+900000000 from user_info where id < 2000000;
-/*子账号
-insert into user_usertype(userId, userType, soDoneCode) select id, 7, id+900000000 from user_info where id >= 4000000 and (phoneExt is null or phoneExt = 0) and id not in (select userId from cntr_salecontractuserrel where userId is not null) and id in (select userId from user_usertype where userType = 7);
-*/
-/*员工
-insert into user_usertype(userId, userType, soDoneCode) select id, 5, id+900000000 from user_info where id >= 2000000 and id < 3000000;
-*/
