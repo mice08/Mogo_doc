7bccec427d5354cdb2f66338608c4cd58ec774a3
yb <yb@mogoroom.com>
2016-04-19 10:22:21 +0800

    1

--- VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/orga_org_position-闪电组.sql
+++ VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/orga_org_position-闪电组.sql
@@ -10,7 +10,7 @@ create table orga_org
 (
     id                   int(11) not null auto_increment comment '组织表id',
     parentId             int(11) comment '上级组织id',
-    thirdId              int(20) comment '第三方组织id',
+    thirdId              int(20) comment '第三方系统的关联组织id',
     orgName              varchar(50) not null comment '组织名称',
     shortName            varchar(50) comment '组织简称',
     orgCode              varchar(10) not null comment '组织编码',
@@ -39,8 +39,8 @@ create table orga_org_position
     id                   int(11) not null auto_increment comment '用户岗位关系表id',
     userId               int(11) not null comment '用户id',
     orgId                int(11) not null comment '组织id',
-    positionId               int(11) not null comment '岗位ID',
-    status               tinyint(1) comment '状态(0:无效,1:有效)',
+    positionId           int(11) not null comment '岗位ID',
+    status               tinyint(1) comment '状态(0:无效 1:有效)',
     createBy             int(11) comment '创建人ID',
     createByType         int(1) comment '创建人类型(参照groupName=userType)',
     createTime           datetime comment '创建时间',
@@ -59,9 +59,10 @@ drop table if exists orga_position;
 create table orga_position
 (
     id                   int(11) not null auto_increment comment '用户岗位id',
-    positionName              varchar(50) not null comment '岗位名称',
-    status               tinyint(1) comment '岗位状态（0:无效,1:有效）',
-    positionType              tinyint(2) comment '分类(参照groupName=userType)',
+    positionName         varchar(50) not null comment '岗位名称',
+    positionGroup        tinyint(2) comment '分组(1:部门管理者 2:普通员工)',
+    status               tinyint(1) comment '岗位状态（0:无效 1:有效）',
+    positionType         tinyint(2) comment '分类(参照groupName=userType)',
     createBy             int(11) not null comment '创建人id',
     createByType         tinyint(2) not null comment '创建人类型(参照groupName=userType)',
     createTime           datetime not null comment '创建时间',
