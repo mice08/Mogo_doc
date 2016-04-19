b32d1c8866fa6f672972bda90e0377bcdc26a9e1
qianwenbo <qwb@mogoroom.com>
2016-04-19 16:31:38 +0800

    opt@3.1.1:loan_bank_mapping表添加字段，初始化数据

---
+++ VersionRecords/Version3.1.1/SQL_Scripts/DDL_pending/loan_bank_mapping-闪电组.sql
@@ -0,0 +1,4 @@
+/*  Database name `mogoroomdb` ，loan_bank_mapping表添加字段*/
+USE mogoroomdb;
+ALTER TABLE loan_bank_mapping ADD COLUMN credits DECIMAL(12,2) COMMENT"单笔限额",
+ADD COLUMN isSmsVerified TINYINT(1) COMMENT"银行是否支持短信验证(0:否 1:是)";
---
+++ VersionRecords/Version3.1.1/SQL_Scripts/DML_pending/loan_bank_mapping-闪电组.sql
@@ -0,0 +1,22 @@
+/*  Database name `mogoroomdb` ，loan_bank_mapping表初始化数据*/
+USE mogoroomdb;
+INSERT INTO loan_bank_mapping (
+  CODE,
+  channel,
+  channelBankCode,
+  credits,
+  isMesgSupported
+) 
+VALUES
+  ('ICBC', 3, 01020000, 50000, 0),
+  ('ABC', 3, 01030000, 20000, 0),
+  ('CBC', 3, 01050000, 50000, 1),
+  ('BOC', 3, 01040000, 10000, 1),
+  ('PSBC', 3, 01000000, 50000, 1),
+  ('CMB', 3, 03080000, 50000, 1),
+  ('CEB', 3, 03030000, 50000, 0),
+  ('CGB', 3, 03060000, 50000, 1),
+  ('PAB', 3, 05105840, 50000, 0),
+  ('SPDB', 3, 03100000, 4999, 1),
+  ('CMBC', 3, 03050000, 50000, 1),
+  ('CIB',3, 03090010, 50000, 0)
