6cb73d7cbdcbef78dbad25494baf2c26b7b62014
zhangfei92000 <zhangfei@mogoroom.com>
2016-04-20 16:18:31 +0800

    初始化钉钉企业号参数

---
+++ VersionRecords/Version3.1.2/SQL_Scripts/DML_pending/comm_sysconfig_insert-闪电组.sql
@@ -0,0 +1,4 @@
+use mogoroomdb;
+
+/*钉钉凭证参数*/
+INSERT INTO `comm_sysconfig` (`keyName`, `val`, `groupName`, `name`, `remark`, `updateTime`, `updateByType`, `updateBy`, `extra1`, `extra2`) VALUES ('dingTalkSecret', '{\"corpid\":\"ding38dbe9c6e5a95973\",\"corpsecret\":\"oFBoqCsW6rUyaSWfMWuFzDMbqMdcWkzsZ3LDVZD2v7XdVnBpK_zds9B5kr-XgVRx\"}', NULL, '钉钉企业参数', NULL, now(), 5, 1, NULL, NULL);
