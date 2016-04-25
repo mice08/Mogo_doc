78d24f17ac6df583fa9aefcf81cbd7d8344bd36f
qianwenbo <qwb@mogoroom.com>
2016-04-25 15:51:52 +0800

    1

--- VersionRecords/Version3.1.2/SQL_Scripts/DDL_pending/comm_bank闪电组.sql
+++ VersionRecords/Version3.1.2/SQL_Scripts/DDL_pending/comm_bank闪电组.sql
@@ -1,4 +1,4 @@
 /*  Database name `mogoroomdb` ，修改comm_bank表sort字段名称为hot，
 之前sort用于排序，后来讨论使用hot表示该行数据是否常用*/
 USE mogoroomdb;
-ALTER TABLE comm_bank CHANGE COLUMN  sort hot TINYINT(1) COMMENT"是否常用(1:是 0:否)"
+ALTER TABLE comm_bank CHANGE COLUMN  sort isHot TINYINT(1) COMMENT"是否常用(1:是 0:否)"
