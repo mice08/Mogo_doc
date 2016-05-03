3829f18e7cc19a77d10ee8df3f1a8f54cf6cf74c
songwie <songwei@mogoroom.com>
2016-05-03 11:48:00 +0800

    修改注册统计脚本

--- VersionRecords/Version3.1.2/SQL_Scripts/DDL_pending/user_regist_report-架构组.sql
+++ VersionRecords/Version3.1.2/SQL_Scripts/DDL_pending/user_regist_report-架构组.sql
@@ -9,8 +9,8 @@ ALTER TABLE `user_renter`
 
 --添加新的渠道、渠道明细，归属地 字段
 ALTER TABLE `user_info`
-	ADD COLUMN `regChannel` INT(1) NOT NULL DEFAULT '3' COMMENT '渠道类型(1:租客APP 2:BS后台 3:官网 4:房东APP 5:房东PC 6:定时任务 7:后台操作 8:搜索引擎 9:租客WAP)' ,
-	ADD COLUMN `regChannelDtl` INT(1) NOT NULL DEFAULT '2' COMMENT '渠道明细' AFTER `regChannel`,
+	ADD COLUMN `regChannel` INT(1) NOT NULL DEFAULT '3' COMMENT '渠道类型,关联字典表comm_dictionary值groupName=channel' ,
+	ADD COLUMN `regChannelDtl` INT(1) NOT NULL DEFAULT '2' COMMENT '渠道明细,关联字典表comm_dictionary值groupName=regChannelDtl' AFTER `regChannel`,
 	ADD COLUMN `regMobileCity` VARCHAR(10) NOT NULL DEFAULT '' COMMENT '手机归属地编码 关联表comm_mobile_city.areaCode ，需要手机前7位与归属地 保证唯一' AFTER `regChannelDtl`;
 
 
