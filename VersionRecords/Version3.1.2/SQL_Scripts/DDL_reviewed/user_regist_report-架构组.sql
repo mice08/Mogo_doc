66808aaf9ff7b0bf9318482192323658daf35b2d
scieny <dragonsboy@21cn.com>
2016-05-03 13:47:14 +0800

    new@3.1.2#:微调列名

--- VersionRecords/Version3.1.2/SQL_Scripts/DDL_reviewed/user_regist_report-架构组.sql
+++ VersionRecords/Version3.1.2/SQL_Scripts/DDL_reviewed/user_regist_report-架构组.sql
@@ -3,15 +3,15 @@ use mogoroomdb;
 
 --添加新的渠道、渠道明细，归属地 字段
 ALTER TABLE `user_renter`
-	ADD COLUMN `regChannel` INT(1) NOT NULL DEFAULT '3' COMMENT '渠道类型,关联字典表comm_dictionary值groupName=channel' ,
-	ADD COLUMN `regChannelDtl` INT(1) NOT NULL DEFAULT '2'  COMMENT '渠道明细,关联字典表comm_dictionary值groupName=regChannelDtl' AFTER `regChannel`,
-	ADD COLUMN `regMobileCity` VARCHAR(10) NOT NULL DEFAULT '' COMMENT '手机归属地编码' AFTER `regChannelDtl`;
+	ADD COLUMN `regChannel` INT(1) NOT NULL DEFAULT '3' COMMENT '渠道类型(关联字典表comm_dictionary值groupName=channel)' ,
+	ADD COLUMN `regChannelDtl` INT(1) NOT NULL DEFAULT '2'  COMMENT '渠道明细(关联字典表comm_dictionary值groupName=regChannelDtl)' AFTER `regChannel`,
+	ADD COLUMN `regMobileCity` VARCHAR(10) NOT NULL DEFAULT '' COMMENT '手机归属地编码 关联表city.id ，需要手机前7位与归属地 保证唯一' AFTER `regChannelDtl`;
 
 --添加新的渠道、渠道明细，归属地 字段
 ALTER TABLE `user_info`
-	ADD COLUMN `regChannel` INT(1) NOT NULL DEFAULT '3' COMMENT '渠道类型,关联字典表comm_dictionary值groupName=channel' ,
-	ADD COLUMN `regChannelDtl` INT(1) NOT NULL DEFAULT '2' COMMENT '渠道明细,关联字典表comm_dictionary值groupName=regChannelDtl' AFTER `regChannel`,
-	ADD COLUMN `regMobileCity` VARCHAR(10) NOT NULL DEFAULT '' COMMENT '手机归属地编码 关联表comm_mobile_city.areaCode ，需要手机前7位与归属地 保证唯一' AFTER `regChannelDtl`;
+	ADD COLUMN `regChannel` INT(1) NOT NULL DEFAULT '3' COMMENT '渠道类型(关联字典表comm_dictionary值groupName=channel)' ,
+	ADD COLUMN `regChannelDtl` INT(1) NOT NULL DEFAULT '2' COMMENT '渠道明细(关联字典表comm_dictionary值groupName=regChannelDtl)' AFTER `regChannel`,
+	ADD COLUMN `regMobileCity` VARCHAR(10) NOT NULL DEFAULT '' COMMENT '手机归属地编码 关联表city.id ，需要手机前7位与归属地 保证唯一' AFTER `regChannelDtl`;
 
 
 --添加手机归属地离线库表
@@ -23,4 +23,4 @@ CREATE TABLE `comm_mobile_city` (
 	`areaCode` VARCHAR(10) NULL DEFAULT NULL COMMENT '地区码',
 	`postCode` VARCHAR(50) NULL DEFAULT NULL COMMENT '邮编',
 	PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='租户手机归属地表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='手机归属地表';
