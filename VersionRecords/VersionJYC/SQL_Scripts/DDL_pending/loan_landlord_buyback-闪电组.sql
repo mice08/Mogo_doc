7c4253b08bc65eb05e1032c208f954a95f85e9e2
sw160202 <sw160202@mogoroom.com>
2016-05-05 13:55:27 +0800

    new@jyc#:新增了贷款合同表字段和房东买回表字段

--- VersionRecords/VersionJYC/SQL_Scripts/DDL_pending/loan_landlord_buyback-闪电组.sql
+++ VersionRecords/VersionJYC/SQL_Scripts/DDL_pending/loan_landlord_buyback-闪电组.sql
@@ -1,8 +1,8 @@
 /*房东申请贷款 新增mogo申请时间字段 在使用mogo宝时更新下这个字段时间*/
-ALTER TABLE  loan_landlord_contract ADD COLUMN mogoApplyTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP  COMMENT '蘑菇宝申请时间' AFTER applyTime;
+ALTER TABLE  loan_landlord_contract ADD COLUMN mogoApplyTime DATETIME NOT NULL COMMENT '蘑菇宝申请时间' AFTER applyTime;
 
 /*房东贷款申请his 新增mogo宝申请使用时间字段*/
-ALTER  TABLE loan_landlord_contract_his ADD COLUMN mogoApplyTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '蘑菇宝申请时间' AFTER applyTime;
+ALTER  TABLE loan_landlord_contract_his ADD COLUMN mogoApplyTime DATETIME NOT NULL  COMMENT '蘑菇宝申请时间' AFTER applyTime;
 
 /*************************************************************************************
  贷款合同表与贷款买回表涉及到的字段新增如下:
