928b58e1d3dbd4f1175f03bf0d96ba1c12e46975
sw160202 <sw160202@mogoroom.com>
2016-05-05 12:55:28 +0800

    new@jyc#:新增了贷款合同表字段和房东买回表字段

--- VersionRecords/VersionJYC/SQL_Scripts/DDL_pending/loan_landlord_contract_his-闪电组.sql
+++ VersionRecords/VersionJYC/SQL_Scripts/DDL_pending/loan_landlord_contract_his-闪电组.sql
@@ -107,14 +107,41 @@ ALTER TABLE comm_sysconfig_his
   CHANGE `val` `val` VARCHAR(4096) NULL  COMMENT '变更前配置项值',
   CHANGE `newVal` `newVal` VARCHAR(4096)  NULL  COMMENT '变更后配置项值';
 
+  /*房东贷款申请 approvalTime1 字段类型修改成datetime*/
+ALTER  TABLE loan_landlord_contract MODIFY COLUMN approvalTime1 DATETIME NULL  comment '二审时间' after approvalUser1;
+
 /*房东申请贷款 新增mogo申请时间字段 在使用mogo宝时更新下这个字段时间*/
 ALTER TABLE  loan_landlord_contract ADD COLUMN mogoApplyTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP  COMMENT '蘑菇宝申请时间' AFTER applyTime;
 
 /*房东贷款申请his 新增mogo宝申请使用时间字段*/
 ALTER  TABLE loan_landlord_contract_his ADD COLUMN mogoApplyTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '蘑菇宝申请时间' AFTER applyTime;
 
-/*房东贷款申请 approvalTime1 字段类型修改成datetime*/
-ALTER  TABLE loan_landlord_contract MODIFY COLUMN approvalTime1 DATETIME NULL  comment '二审时间' after approvalUser1;
+/*************************************************************************************
+ 贷款合同表与贷款买回表涉及到的字段新增如下:
+ *************************************************************************************/
+/*贷款合同表  新增贷款保证金*/
+ALTER TABLE loan_contract ADD COLUMN depositAmount DECIMAL(12,2) DEFAULT 0 COMMENT  '借款保证金' AFTER loanChannel;
+/*贷款合同表  新增贷款费率*/
+ALTER TABLE  loan_contract ADD COLUMN feeRate INT(11) DEFAULT 0 NULL COMMENT '借款费率万分比' AFTER depositAmount;
+
+/*贷款合同表  新增实际到手金额*/
+ALTER TABLE  loan_contract ADD COLUMN actualAmount DECIMAL(12,2) DEFAULT 0 NULL COMMENT '实际放款金额' AFTER feeRate;
+
+/*贷款合同表  新增商品id*/
+ALTER TABLE  loan_contract ADD COLUMN prodId INT(11) COMMENT '产品id' AFTER actualAmount;
+/*贷款合同表  新增商品type*/
+ALTER TABLE  loan_contract ADD COLUMN prodType INT(11) COMMENT '产品类型' AFTER prodId;
+/*贷款合同表  新增关联贷款id*/
+ALTER TABLE  loan_contract ADD COLUMN relContractId INT(11) COMMENT '关联贷款id' AFTER prodType;
+
+/*房东买回 新增贷款来源*/
+ALTER TABLE  loan_landlord_buyback ADD COLUMN loanChannel INT(11) COMMENT '贷款来源 参考字典表 loan_channel(1:拉卡拉 2:蘑菇 3:聚有财)' AFTER `status`;
+/*房东买回 新增租客逾期滞纳金*/
+ALTER TABLE  loan_landlord_buyback ADD COLUMN renterPenalty DECIMAL(12,2) DEFAULT 0 COMMENT '租客逾期还款滞纳金' AFTER backFee;
+/*房东买回 新增已扣租客逾期滞纳金*/
+ALTER TABLE  loan_landlord_buyback ADD COLUMN repayedRenterPenalty DECIMAL(12,2) DEFAULT 0 COMMENT '已扣租客逾期还款滞纳金' AFTER repayedLateFee;
+/*房东买回 新增待扣租客逾期滞纳金*/
+ALTER TABLE loan_landlord_buyback ADD COLUMN waitRepayRenterPenalty DECIMAL(12,2) DEFAULT 0 COMMENT '待扣租客逾期滞纳金' AFTER waitRepayLateFee;
 
 
 
