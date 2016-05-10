b4a1dffb117998705253ca79d4a76849c553b86c
qianwenbo <qwb@mogoroom.com>
2016-05-10 15:13:30 +0800

    1

--- VersionRecords/Version3.1.3/SQL_Scripts/DML_pending/loan_bank_mapping-闪电组.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DML_pending/loan_bank_mapping-闪电组.sql
@@ -9,3 +9,8 @@ UPDATE loan_bank_mapping SET CODE = 'CCB' WHERE CODE='CBC';
 UPDATE loan_bank_mapping SET CODE = 'CITIC' WHERE CODE='CNCB';
 UPDATE loan_bank_mapping SET CODE = 'GDB' WHERE CODE='CGB';
 UPDATE loan_bank_mapping SET CODE = 'SZPAB' WHERE CODE='PAB';
+
+UPDATE loan_renter_info SET debitbank = 'CCB' WHERE debitbank='CBC';
+UPDATE loan_renter_info SET debitbank = 'CITIC' WHERE debitbank='CNCB';
+UPDATE loan_renter_info SET debitbank = 'GDB' WHERE debitbank='CGB';
+UPDATE loan_renter_info SET debitbank = 'SZPAB' WHERE debitbank='PAB';
