0bec1180b4c0e4ca71796d018e8205000edca746
sw160202 <sw160202@mogoroom.com>
2016-05-11 19:23:01 +0800

    new@jyc#:租金宝二审or房东信用额度需求新加字段

--- VersionRecords/VersionJYC/SQL_Scripts/DDL_pending/2-loan_landlord_contract-闪电组.sql
+++ VersionRecords/VersionJYC/SQL_Scripts/DDL_pending/2-loan_landlord_contract-闪电组.sql
@@ -1,17 +1,11 @@
+/*租金宝合同 新增 soDoneCode */
+ALTER TABLE loan_landlord_contract ADD COLUMN soDoneCode INT(11) null COMMENT '业务操作(参考字典表组名:comm_business_record)' AFTER credits;
+
+/*租金宝合同his 新增 soDoneCode */
+ALTER TABLE loan_landlord_credit_his ADD COLUMN soDoneCode INT(11) null COMMENT '业务操作(参考字典表组名:comm_business_record)' AFTER  credits;
+
 /*租金宝合同 新增最长蘑菇宝租期*/
-ALTER TABLE loan_landlord_contract ADD COLUMN maximumPeriod INT(11) NULL COMMENT '最长蘑菇宝租期' AFTER shortestTerm;
-
-/*租金宝合同 新增暂停操作人*/
-ALTER  TABLE loan_landlord_contract ADD COLUMN  pauseUser INT(11) NULL  COMMENT '暂停租金宝操作人' AFTER maximumPeriod;
-
-/*租金宝合同 新增暂停操作时间*/
-ALTER  TABLE loan_landlord_contract ADD COLUMN pauseTime DATETIME NULL COMMENT '暂停租金宝时间' AFTER pauseUser;
-
-/*租金宝合同 新增终止操作人*/
-ALTER  TABLE loan_landlord_contract ADD  COLUMN stopUser INT(11) NULL COMMENT '终止租金宝操作人' AFTER  pauseTime;
-
-/*租金宝合同 新增终止操作时间*/
-ALTER TABLE  loan_landlord_contract ADD COLUMN  stopTime DATETIME null COMMENT '终止租金宝时间' AFTER stopUser;
+ALTER TABLE loan_landlord_contract ADD COLUMN longestTerm INT(11) NULL COMMENT '最长蘑菇宝租期' AFTER shortestTerm;
 
 /*房东信用额度 新增A类额度系数百分比*/
 ALTER  TABLE loan_landlord_credit ADD COLUMN rangeAmountRate INT(11) NULL COMMENT 'A类额度系数百分比' AFTER remark;
@@ -20,20 +14,7 @@ ALTER  TABLE loan_landlord_credit ADD COLUMN rangeAmountRate INT(11) NULL COMMEN
 ALTER  TABLE loan_landlord_credit ADD COLUMN  onlineTradingAmountRate INT(11) NULL COMMENT 'B类额度系数百分比' AFTER rangeAmountRate;
 
 /*租金宝合同his 新增最长蘑菇宝租期*/
-ALTER TABLE loan_landlord_contract_his ADD COLUMN maximumPeriod INT(11) NULL COMMENT '最长蘑菇宝租期' AFTER shortestTerm;
-
-/*租金宝合同his 新增暂停操作人*/
-ALTER  TABLE loan_landlord_contract_his ADD COLUMN  pauseUser INT(11) NULL  COMMENT '暂停租金宝操作人' AFTER maximumPeriod;
-
-/*租金宝合同his 新增暂停操作时间*/
-ALTER  TABLE loan_landlord_contract_his ADD COLUMN pauseTime DATETIME NULL COMMENT '暂停租金宝时间' AFTER pauseUser;
-
-/*租金宝合同his 新增终止操作人*/
-ALTER  TABLE loan_landlord_contract_his ADD  COLUMN stopUser INT(11) NULL COMMENT '终止租金宝操作人' AFTER  pauseTime;
-
-/*租金宝合同his 新增终止操作时间*/
-ALTER TABLE  loan_landlord_contract_his ADD COLUMN  stopTime DATETIME NULL COMMENT '终止租金宝时间' AFTER stopUser;
-
+ALTER TABLE loan_landlord_contract_his ADD COLUMN longestTerm INT(11) NULL COMMENT '最长蘑菇宝租期' AFTER shortestTerm;
 
 /*房东信用额度his 新增A类额度系数百分比*/
 ALTER  TABLE loan_landlord_credit_his ADD COLUMN rangeAmountRate INT(11) NULL COMMENT 'A类额度系数百分比' AFTER comments;
@@ -42,13 +23,10 @@ ALTER  TABLE loan_landlord_credit_his ADD COLUMN rangeAmountRate INT(11) NULL CO
 ALTER  TABLE loan_landlord_credit_his ADD COLUMN  onlineTradingAmountRate INT(11) NULL COMMENT 'B类额度系数百分比' AFTER rangeAmountRate;
 
 /*房东贷款合同 新增申请城市*/
-ALTER TABLE  loan_landlord_contract ADD applyCityId INT(11) NULL comment '贷款申请城市' after pauseTime;
+ALTER TABLE  loan_landlord_contract ADD applyCityId INT(11) NULL comment '贷款申请城市' after longestTerm;
 
 /*房东贷款合同his 新增申请城市*/
-ALTER TABLE  loan_landlord_contract_his ADD applyCityId INT(11) NULL comment '贷款申请城市' after pauseTime;
-
-/*房东贷款申请his 修改mogo宝申请使用时间字段  为了兼容老接口 允许为空*/
-ALTER  TABLE loan_landlord_contract_his MODIFY COLUMN mogoApplyTime DATETIME NULL  COMMENT '最近一次蘑菇宝申请时间' AFTER applyTime;
+ALTER TABLE  loan_landlord_contract_his ADD applyCityId INT(11) NULL comment '贷款申请城市' after longestTerm;
 
 /*房东买回 新增贷款来源为了兼容老数据 必须给默认值*/
 ALTER TABLE  loan_landlord_buyback MODIFY COLUMN loanChannel INT(11) NOT NULL DEFAULT 1 COMMENT '贷款来源(参考字典表组名:loan_channel)' AFTER `status`;
