5d2b5de4ab11089409535f48be9b7c121b2bbcf2
雷传盛 <lcs@mogoroom.com>
2016-05-11 14:41:15 +0800

    opt@3.1.3#:增加拼音字段

--- VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/flat_community-闪电组.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/flat_community-闪电组.sql
@@ -3,6 +3,4 @@ use mogoroomdb;
 
 -- 增加一个拼音字段
 alter table `flat_community` 
-   add column `namePinyin` varchar(100) CHARSET utf8 COLLATE utf8_general_ci NULL COMMENT '小区名拼音' after `name`,
-   change `id` `id` int(11) NOT NULL AUTO_INCREMENT comment 'id', 
-   change `name` `name` varchar(30) character set utf8 collate utf8_general_ci NULL  comment '小区名';
+   add column `namePinyin` varchar(100) CHARSET utf8 COLLATE utf8_general_ci NULL COMMENT '小区名拼音' after `name`;
