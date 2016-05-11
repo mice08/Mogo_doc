6b50b3f80a2527aa6bd872a2f5236b6c1c59a898
雷传盛 <lcs@mogoroom.com>
2016-05-11 14:30:43 +0800

    opt@3.1.3#:删除全文索引

--- VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/flat_community-闪电组.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/flat_community-闪电组.sql
@@ -6,9 +6,3 @@ alter table `flat_community`
    add column `namePinyin` varchar(100) CHARSET utf8 COLLATE utf8_general_ci NULL COMMENT '小区名拼音' after `name`,
    change `id` `id` int(11) NOT NULL AUTO_INCREMENT comment 'id', 
    change `name` `name` varchar(30) character set utf8 collate utf8_general_ci NULL  comment '小区名';
-   
--- 为拼音字段增加全文索引
-CREATE FULLTEXT INDEX full_index_community_name ON flat_community(name);
-
--- 为字段弄增加全文索引
-CREATE FULLTEXT INDEX full_index_community_nong ON flat_community(nong);
--- VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/flat_flats-闪电组.sql
+++
@@ -1,5 +0,0 @@
-/* 公寓表的字段roomNum和flatsNum增加全文索引 */
-use mogoroomdb;
-
-CREATE FULLTEXT INDEX full_index_flats_roomNum ON flat_flats(roomNum);
-CREATE FULLTEXT INDEX full_index_flats_flatsNum ON flat_flats(flatsNum);
