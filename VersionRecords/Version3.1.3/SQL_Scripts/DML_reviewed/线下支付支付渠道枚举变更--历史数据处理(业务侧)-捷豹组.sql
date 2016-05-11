6e9681bcd4f80d3f63ae86b24f924dce0e07bbe8
zsj <zsj@mogoroom.com>
2016-05-11 11:27:20 +0800

    1

--- VersionRecords/Version3.1.3/SQL_Scripts/DML_reviewed/线下支付支付渠道枚举变更--历史数据处理(业务侧)-捷豹组.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DML_reviewed/线下支付支付渠道枚举变更--历史数据处理(业务侧)-捷豹组.sql
@@ -6,8 +6,7 @@ use mogoroomdb;
 select exempttype,count(*) from bill_salebill 	where 	exempt = 1 group by exempttype;
  
 -- step 2 update
-UPDATE bill_salebill old,(select id,case when exempttype = 1 then 104 else 199	END as fundchl from bill_salebill where exempt = 1) new
-set old.exempttype = new.fundchl where old.id = new.id AND old.exempt = 1;
+UPDATE bill_salebill old,(select id,case when exempttype = 1 then 104 else 199 END as fundchl from bill_salebill where exempt = 1)new set old.exempttype = new.fundchl where old.id = new.id AND old.exempt = 1;
 -- step 3 check   '104' count ==  '1' count    '199' count == '2 + null' count             查过生产数据  目前只有一笔为null的记录
 select exempttype,count(*) from bill_salebill 	where 	exempt = 1 group by exempttype;
 
