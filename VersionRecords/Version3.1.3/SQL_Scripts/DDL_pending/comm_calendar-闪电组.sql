acdf1fa461c808178e7301cd975cb967103c4933
ad <ad@mogoroom.com>
2016-05-14 17:14:13 +0800

    new@3.1.3#:初始化日历存储过程

--- VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/comm_calendar-闪电组.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/comm_calendar-闪电组.sql
@@ -16,4 +16,33 @@ create table comm_calendar (
 	`updateByType`  tinyint(2) default null comment '修改人类型(参考字典表组名:userType)',
 	`updateTime` datetime default null comment '修改时间',
 	primary key(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='日历表'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='日历表';
+
+/*初始化日历存储过程*/
+DELIMITER //
+DROP PROCEDURE IF EXISTS calendarinit //
+CREATE PROCEDURE calendarinit()
+BEGIN
+	declare num int;
+	declare dt date;
+	declare wd int;
+	declare type int;
+	
+	set num = 1;
+	set dt = CURRENT_DATE();
+	
+	while num < 1096 do
+		set wd = DAYOFWEEK(dt)-1;
+		if wd = 0 then
+			set wd = 7;
+		end if;
+		if wd > 5 then
+			set type = 0;
+		else
+			set type = 1;
+		end if;
+		insert into comm_calendar(date, weekday, type) values(dt, wd, type);
+		set num = num + 1;
+		set dt = DATE_ADD(dt, INTERVAL 1 DAY);
+	end while;
+END //
