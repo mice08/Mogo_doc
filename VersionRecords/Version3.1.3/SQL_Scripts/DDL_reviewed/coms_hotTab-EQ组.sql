2f563a4ef9c2ef1b4c8aacfbe5cd87b581efd526
zsj <zsj@mogoroom.com>
2016-05-16 17:12:54 +0800

    1

--- VersionRecords/Version3.1.3/SQL_Scripts/DDL_reviewed/coms_hotTab-EQ组.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DDL_reviewed/coms_hotTab-EQ组.sql
@@ -5,20 +5,19 @@ use mogoroomdb;
 /*==============================================================*/
 /* Table: coms_hotTab                           */
 /*==============================================================*/
-update table coms_hotTab add column
-   pId                  int unsigned comment '父ID',
-   tabType              tinyint(1) not null comment '标签类型(1:推荐房源标签 2:推荐品牌标签)',
-   channel              int not null comment '渠道(枚举:字典表groupname=channel)',
-   menucode             int not null comment '发布位置(枚举:首页,精品专区首页,品牌专区首页,品牌专区推荐,合作页品牌推荐)',
-   brief                varchar(300) not null comment '摘要',
-   description          varchar(3000) comment '描述',
-   status               tinyint(1) not null default 1 comment '状态(1:上线 0:下线)',
-   valid                tinyint(1) not null default 1 comment '是否有效(1:有效 0:无效)',
-   jumpCode             varchar(10) comment '跳转编码',
-   jumpValue            varchar(20) comment '跳转值',
-   contentType          int comment '内容类型(1:文字 2:图片 3:房源 4:品牌 5:地理位置)',
-   pageview             int default 0 comment '浏览量',
-   remark               varchar(200) comment '备注',
+alter table coms_hotTab add column pId  int unsigned comment '父ID',
+						add column tabType tinyint(1) not null comment '标签类型(1:推荐房源标签 2:推荐品牌标签)',
+						add column channel int not null comment '渠道(枚举:字典表groupname=channel)',
+						add column menucode int not null comment '发布位置(枚举:首页,精品专区首页,品牌专区首页,品牌专区推荐,合作页品牌推荐)',
+						add column brief varchar(300) not null comment '摘要',
+						add column description varchar(3000) comment '描述',
+						add column status tinyint(1) not null default 1 comment '状态(1:上线 0:下线)',
+						add column valid tinyint(1) not null default 1 comment '是否有效(1:有效 0:无效)',
+						add column jumpCode varchar(10) comment '跳转编码',
+						add column jumpValue varchar(20) comment '跳转值',
+						add column contentType int comment '内容类型(1:文字 2:图片 3:房源 4:品牌 5:地理位置)',
+						add column pageview int default 0 comment '浏览量',
+						add column remark varchar(200) comment '备注';
 
 
 create table coms_hotbrand
@@ -35,6 +34,6 @@ create table coms_hotbrand
    updateBy             INT(11) COMMENT  '修改人',
    updateByType         INT(2) COMMENT '修改人类型(参考groupName=userType)',
    updateTime           datetime DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
-   remark               varbinary(200) comment '备注',
+   remark               varchar(200) comment '备注',
    primary key (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '品牌推荐表';
