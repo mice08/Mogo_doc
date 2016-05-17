14235ef8dc494b3c225309250f2f0ecabe20f0d2
雷传盛 <lcs@mogoroom.com>
2016-05-17 14:04:27 +0800

    opt@3.1.3#:增加字典项

---
+++ VersionRecords/Version3.1.3/SQL_Scripts/DML_pending/comm_dictionary-闪电组.sql
@@ -0,0 +1,6 @@
+/* Database name `mogoroomdb`，增加用户喜欢类型的字典项 */
+
+use mogoroomdb;
+
+INSERT INTO comm_dictionary(CODE,groupName,sort,keyPro,VALUE,STATUS,enValue)VALUES('userFavoriteType-1','userFavoriteType',1,1,'房源',1,'userFavoriteType-1');
+INSERT INTO comm_dictionary(CODE,groupName,sort,keyPro,VALUE,STATUS,enValue)VALUES('userFavoriteType-2','userFavoriteType',2,2,'品牌',1,'userFavoriteType-2');
