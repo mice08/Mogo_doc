7c8ca68339e64e46934b00c84510ba2803b729fd
yb <yb@mogoroom.com>
2016-05-18 14:09:28 +0800

    1

--- VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/brand-闪电组.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DDL_pending/brand-闪电组.sql
@@ -92,3 +92,11 @@ create table brand_promise
    updateTime           datetime default CURRENT_TIMESTAMP comment '修改人时间',
    primary key (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '品牌承诺表';
+
+alter table `brand` add index `brand_landlordId_idx` (`landlordId`);
+
+alter table `brand_assessment` add index `assessment_brandId_idx` (`brandId`);
+
+alter table `brand_comment` add unique `comment_unique_idx` (`objectType`, `objectId`);
+
+alter table `brand_promise` add unique `promise_unique_idx` (`brandId`, `promiseItemKey`);
