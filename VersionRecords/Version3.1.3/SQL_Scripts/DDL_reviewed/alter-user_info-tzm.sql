d2d728e709c0de24cc156c73c5d1203d852eb375
tzm <tzm@mogoroom.com>
2016-05-23 16:17:14 +0800

    opt@3.1.4#:修改个人信息表

--- VersionRecords/Version3.1.3/SQL_Scripts/DDL_reviewed/alter-user_info-tzm.sql
+++ VersionRecords/Version3.1.3/SQL_Scripts/DDL_reviewed/alter-user_info-tzm.sql
@@ -7,7 +7,7 @@ ALTER TABLE user_info ADD COLUMN companyAddress varchar(200) DEFAULT NULL COMMEN
 
 ALTER TABLE user_info ADD COLUMN occupation int(11) DEFAULT NULL COMMENT '所属行业';
 
-ALTER TABLE user_info ADD COLUMN companTel varchar(50) DEFAULT NULL COMMENT '公司电话';
+ALTER TABLE user_info ADD COLUMN companyTel varchar(50) DEFAULT NULL COMMENT '公司电话';
 
 ALTER TABLE user_info ADD COLUMN position int(11) DEFAULT NULL COMMENT '所属职位';
 
@@ -18,6 +18,6 @@ ALTER TABLE user_info_his ADD COLUMN companyAddress varchar(200) DEFAULT NULL CO
 
 ALTER TABLE user_info_his ADD COLUMN occupation int(11) DEFAULT NULL COMMENT '所属行业';
 
-ALTER TABLE user_info_his ADD COLUMN companTel varchar(50) DEFAULT NULL COMMENT '公司电话';
+ALTER TABLE user_info_his ADD COLUMN companyTel varchar(50) DEFAULT NULL COMMENT '公司电话';
 
 ALTER TABLE user_info_his ADD COLUMN position int(11) DEFAULT NULL COMMENT '所属职位';
