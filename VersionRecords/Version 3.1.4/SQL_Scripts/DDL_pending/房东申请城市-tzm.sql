899e805318d078697e0c149f45ab788fcdf45d8b
tzm <tzm@mogoroom.com>
2016-06-02 12:15:33 +0800

    opt@3.1.4#:补充历史表字段

--- VersionRecords/Version 3.1.4/SQL_Scripts/DDL_pending/房东申请城市-tzm.sql
+++ VersionRecords/Version 3.1.4/SQL_Scripts/DDL_pending/房东申请城市-tzm.sql
@@ -4,3 +4,8 @@ use mogoroomdb;
 ALTER TABLE user_landlord_info ADD COLUMN applyCityId INT(11) DEFAULT NULL COMMENT '房东申请城市id';
 
 ALTER TABLE user_landlord  ADD COLUMN applyCityId INT(11) DEFAULT NULL COMMENT '房东申请城市id';
+
+
+ALTER TABLE user_landlord_info_his ADD COLUMN applyCityId INT(11) DEFAULT NULL COMMENT '房东申请城市id';
+
+ALTER TABLE user_landlord_his  ADD COLUMN applyCityId INT(11) DEFAULT NULL COMMENT '房东申请城市id';
