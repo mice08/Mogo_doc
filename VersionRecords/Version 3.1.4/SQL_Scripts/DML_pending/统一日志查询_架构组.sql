0432dec91d54cd72b9e7da4ddfaa9f51b85e7bb2
songwie <songwei@mogoroom.com>
2016-06-03 15:47:36 +0800

    opt@3.1.4#:日志编码 修改

---
+++ VersionRecords/Version 3.1.4/SQL_Scripts/DML_pending/统一日志查询_架构组.sql
@@ -0,0 +1,13 @@
+/** 业务编码初始化脚本**/
+use mogoroomdb;
+	
+begin
+ 
+delete from `comm_busitype` where busiType=20101004;
+
+INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES('20101004','预定支付成功',1,1,3,1,'20102');
+
+
+
+
+
