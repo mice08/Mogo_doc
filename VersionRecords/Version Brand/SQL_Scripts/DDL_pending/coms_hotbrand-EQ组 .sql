612bcd46a1171244b9a33c93e13707df5b998368
yb <yb@mogoroom.com>
2016-06-06 16:59:22 +0800

    1

--- VersionRecords/Version Brand/SQL_Scripts/DDL_pending/coms_hotbrand-EQ组 .sql
+++ VersionRecords/Version Brand/SQL_Scripts/DDL_pending/coms_hotbrand-EQ组 .sql
@@ -1,4 +1,4 @@
 /* Database name `mogoroomdb` , coms_hotbrand表添加序列字段 */
 use mogoroomdb
 
-alter table flat_room add column seq  int(4) not null default 0 comment '推荐品牌的序列';
+alter table coms_hotbrand add column seq  int(4) not null default 0 comment '推荐品牌的序列';
