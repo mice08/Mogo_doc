8726adf133616cb9f0295068d8cc247aab4e98ca
tzm <tzm@mogoroom.com>
2016-06-12 14:15:56 +0800

    opt@3.1.5#:合并添加salebill字段sql

--- VersionRecords/Version 3.1.5/SQL_Scripts/DDL_pending/新帐务页面需求-tzm.sql
+++ VersionRecords/Version 3.1.5/SQL_Scripts/DDL_pending/新帐务页面需求-tzm.sql
@@ -3,8 +3,8 @@ use mogoroomdb;
 
 ALTER TABLE acct.acct_bill ADD COLUMN minPayAmount decimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
 
-ALTER TABLE bill_salebill  ADD COLUMN minPayAmount decimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
-
-ALTER TABLE bill_salebill  ADD COLUMN unpayAmount decimal(10,2) DEFAULT NULL COMMENT '未支付金额';
+ALTER TABLE bill_salebill  
+			ADD COLUMN minPayAmount decimal(10,2) DEFAULT NULL COMMENT '最小支付金额',
+			ADD COLUMN unpayAmount decimal(10,2) DEFAULT NULL COMMENT '未支付金额';
 
 ALTER TABLE acct.acct_busirec  ADD COLUMN verified tinyint(3) DEFAULT NULL COMMENT '核销标识(0:未核销 1:已核销)';
