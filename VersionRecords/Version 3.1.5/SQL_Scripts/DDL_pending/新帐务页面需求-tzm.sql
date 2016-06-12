f848329b711339ff0d185963f05ed841a2a02c93
tzm <tzm@mogoroom.com>
2016-06-12 14:08:33 +0800

    opt@3.1.5#:修改帐务新页面sql

--- VersionRecords/Version 3.1.5/SQL_Scripts/DDL_pending/新帐务页面需求-tzm.sql
+++ VersionRecords/Version 3.1.5/SQL_Scripts/DDL_pending/新帐务页面需求-tzm.sql
@@ -1,10 +1,10 @@
 /*  Database name `mogoroomdb` ，为新帐务页面需求添加字段*/
 use mogoroomdb;
 
-ALTER TABLE acct.acct_bill ADD COLUMN minPayAmount BigDecimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
+ALTER TABLE acct.acct_bill ADD COLUMN minPayAmount decimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
 
-ALTER TABLE bill_salebill  ADD COLUMN minPayAmount BigDecimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
+ALTER TABLE bill_salebill  ADD COLUMN minPayAmount decimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
 
-ALTER TABLE bill_salebill  ADD COLUMN unpayAmount BigDecimal(10,2) DEFAULT NULL COMMENT '未支付金额';
+ALTER TABLE bill_salebill  ADD COLUMN unpayAmount decimal(10,2) DEFAULT NULL COMMENT '未支付金额';
 
-ALTER TABLE acct.acct_busirec  ADD COLUMN verified tinyint(3) DEFAULT NULL COMMENT '核销标识';
+ALTER TABLE acct.acct_busirec  ADD COLUMN verified tinyint(3) DEFAULT NULL COMMENT '核销标识(0:未核销 1:已核销)';
