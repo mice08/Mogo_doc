5e9ee5566119963bcc89f4121cc76dfa72a5263a
zsj <zsj@mogoroom.com>
2016-06-13 15:13:35 +0800

    1

--- VersionRecords/Version 新官网 20151112(2.0.0)/SQL_Scripts/DBA已审核通过的SQL/city_subway-pengxiaochun.sql
+++
@@ -1,2 +0,0 @@
-use mogoroomdb;
-ALTER TABLE city_subway ADD column enName VARCHAR(20) COMMENT 'Ó¢ÎÄÈ«³Æ»ò¼ò³Æ(seo)';
--- VersionRecords/Version 新官网 20151112(2.0.0)/SQL_Scripts/DBA已审核通过的SQL/oder_reservationorder-leichuansheng.sql
+++
@@ -1,2 +0,0 @@
-use mogoroomdb;
-alter table `oder_reservationorder` add column `reservationNum` varchar(30) NULL COMMENT '预约单号' after `landlordId`;
