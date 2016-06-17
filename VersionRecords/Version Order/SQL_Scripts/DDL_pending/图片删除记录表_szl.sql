1b721ce6039e927fc78d929a47ae9486aedc8059
szl <szl@mogoroom.com>
2016-06-17 12:02:31 +0800

    opt@3.2.0#:添加图片删除记录表

--- VersionRecords/Version Order/SQL_Scripts/DDL_pending/图片删除记录表_szl.sql
+++ VersionRecords/Version Order/SQL_Scripts/DDL_pending/图片删除记录表_szl.sql
@@ -2,5 +2,8 @@ use mogoroomdb;
 CREATE TABLE `mogo_dev`.`comm_picture_del_his`(  
   `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `url` VARCHAR(1024) NOT NULL COMMENT '图片URL',
+  `deleteTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP  COMMENT '删除时间',
+  `operatorType` INT NOT NULL COMMENT '操作人员类型',
+  `operator` INT NOT NULL COMMENT '操作人员',
   PRIMARY KEY (`id`)
-) ENGINE=INNODB CHARSET=utf8 COMMENT='图片删除记录表';
+) ENGINE=INNODB COMMENT='图片删除记录表';
