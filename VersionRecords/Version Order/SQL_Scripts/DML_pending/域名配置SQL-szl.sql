1bfcea87bbc6bd0104ff3c74209d5a0d3f8330a3
szl <szl@mogoroom.com>
2016-06-20 18:40:13 +0800

    opt@3.2.0#:域名配置SQL

--- VersionRecords/Version Order/SQL_Scripts/DML_pending/域名配置SQL-szl.sql
+++ VersionRecords/Version Order/SQL_Scripts/DML_pending/域名配置SQL-szl.sql
@@ -9,7 +9,7 @@ INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, 
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-gz','gz.mogoroom.com','DOMAIN_CONFIG_DEV','官网域名-广州',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-sz','sz.mogoroom.com','DOMAIN_CONFIG_DEV','官网域名-深圳',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-hz','hz.mogoroom.com','DOMAIN_CONFIG_DEV','官网域名-杭州',NULL,'5','0');
-INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bj.mogoroom.com','DOMAIN_CONFIG_DEV','官网域名-杭州',NULL,'5','0');
+INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bj.mogoroom.com','DOMAIN_CONFIG_DEV','官网域名-北京',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('search','search.api.mogoroom.com','DOMAIN_CONFIG_DEV','搜索域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('pay','pay.api.mogoroom.com','DOMAIN_CONFIG_DEV','支付域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('bs','bs.mogoroom.com','DOMAIN_CONFIG_DEV','BS域名',NULL,'5','0');
@@ -23,7 +23,7 @@ INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, 
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-gz','gz.mogoroom.com','DOMAIN_CONFIG_TEST','官网域名-广州',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-sz','sz.mogoroom.com','DOMAIN_CONFIG_TEST','官网域名-深圳',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-hz','hz.mogoroom.com','DOMAIN_CONFIG_TEST','官网域名-杭州',NULL,'5','0');
-INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bj.mogoroom.com','DOMAIN_CONFIG_TEST','官网域名-杭州',NULL,'5','0');
+INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bj.mogoroom.com','DOMAIN_CONFIG_TEST','官网域名-北京',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('search','search.api.mogoroom.com','DOMAIN_CONFIG_TEST','搜索域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('pay','pay.api.mogoroom.com','DOMAIN_CONFIG_TEST','支付域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('bs','bs.mogoroom.com','DOMAIN_CONFIG_TEST','BS域名',NULL,'5','0');
@@ -37,7 +37,7 @@ INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, 
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-gz','gz.mogoroom.com','DOMAIN_CONFIG_PRO','官网域名-广州',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-sz','sz.mogoroom.com','DOMAIN_CONFIG_PRO','官网域名-深圳',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-hz','hz.mogoroom.com','DOMAIN_CONFIG_PRO','官网域名-杭州',NULL,'5','0');
-INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bj.mogoroom.com','DOMAIN_CONFIG_PRO','官网域名-杭州',NULL,'5','0');
+INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bj.mogoroom.com','DOMAIN_CONFIG_PRO','官网域名-北京',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('search','search.api.mogoroom.com','DOMAIN_CONFIG_PRO','搜索域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('pay','pay.api.mogoroom.com','DOMAIN_CONFIG_PRO','支付域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('bs','bs.mogoroom.com','DOMAIN_CONFIG_PRO','BS域名',NULL,'5','0');
@@ -51,7 +51,7 @@ INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, 
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-gz','gzdemo.mogoroom.com','DOMAIN_CONFIG_DEMO','官网域名-广州',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-sz','szdemo.mogoroom.com','DOMAIN_CONFIG_DEMO','官网域名-深圳',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-hz','hzdemo.mogoroom.com','DOMAIN_CONFIG_DEMO','官网域名-杭州',NULL,'5','0');
-INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bjdemo.mogoroom.com','DOMAIN_CONFIG_DEMO','官网域名-杭州',NULL,'5','0');
+INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('renterpc-bj','bjdemo.mogoroom.com','DOMAIN_CONFIG_DEMO','官网域名-北京',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('search','searchdemo.api.mogoroom.com','DOMAIN_CONFIG_DEMO','搜索域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('pay','paydemo.api.mogoroom.com','DOMAIN_CONFIG_DEMO','支付域名',NULL,'5','0');
 INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateByType`, `updateBy`) VALUES('bs','bsdemo.mogoroom.com','DOMAIN_CONFIG_DEMO','BS域名',NULL,'5','0');
