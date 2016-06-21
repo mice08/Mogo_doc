b24c8e8163e253178b33c24ff73d5423076b0280
qwb <qwb@mogoroom.com>
2016-06-21 16:16:46 +0800

    1

--- VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/mogo_da-user_logintotal_daily-闪电组.sql
+++ VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/mogo_da-user_logintotal_daily-闪电组.sql
@@ -5,7 +5,7 @@ CREATE TABLE `user_logintotal_daily` (
   `userid` INT(11) DEFAULT NULL COMMENT '用户id',
   `userType` INT(11) DEFAULT NULL COMMENT '用户类型,参考(groupName=userType)，如果是子账号登录此处记录房东id',
   `loginCount` INT(11) DEFAULT NULL COMMENT '每天的登录统计',
-  `lastActiveTime` DATE DEFAULT NULL COMMENT '当天用户最后活跃时间',
+  `lastActiveTime` DATETIME DEFAULT NULL COMMENT '当天用户最后活跃时间',
   `lastAcitveUserType` INT(11) DEFAULT NULL COMMENT '当天最后活跃的用户类型,参考(groupName=userType)',
   `loginDate` DATE DEFAULT NULL COMMENT '登录时间',
   PRIMARY KEY (`id`),
