6b96dda088e666b46450efb21a17c16515f1561c
sw <sw160202@mogoroom.com>
2016-06-21 14:30:16 +0800

    new@jyc#:房东信用额度表新增额度调整字段

--- VersionRecords/Version JYC/SQL_Scripts/DDL_pending/jyc_ddl-闪电组.sql
+++ VersionRecords/Version JYC/SQL_Scripts/DDL_pending/jyc_ddl-闪电组.sql
@@ -267,11 +267,6 @@ ALTER TABLE loan_landlord_contract ADD COLUMN creditHisId INT(11) NULL COMMENT '
 /*房东贷款资质申请 新增信用配置hisId*/
 ALTER TABLE loan_landlord_contract_his ADD COLUMN creditHisId INT(11) NULL COMMENT '信用配置hisId(loan_landlord_credit_his)';
 
-/*房东信用额度 新增额度调理*/
-ALTER TABLE loan_landlord_credit ADD COLUMN adjustAmount decimal(12,2) DEFAULT '0.00' COMMENT '额度调理(-500,000到2,000,000)' AFTER credits2;
-
-/*房东信用额度_his 新增额度调理*/
-ALTER  TABLE  loan_landlord_credit_his ADD COLUMN adjustAmount  decimal(12,2) DEFAULT '0.00' COMMENT '额度调理(-500,000到2,000,000)' AFTER credits2;
 
 
 
