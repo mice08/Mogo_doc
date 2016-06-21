31143ffef2690e7120370a79fd3172d729054c5e
sw <sw160202@mogoroom.com>
2016-06-21 19:31:32 +0800

    new@jyc#:按照规范修改sql

--- VersionRecords/Version JYC/SQL_Scripts/DML_pending/loan_landlord_buyback-payplan-闪电组.sql
+++ VersionRecords/Version JYC/SQL_Scripts/DML_pending/loan_landlord_buyback-payplan-闪电组.sql
@@ -8,7 +8,6 @@ INNER JOIN loan_renter_info ON loan_renter_contract.infoid = loan_renter_info.id
 WHERE loan_renter_contract.channel = 2 AND loan_landlord_buyback.loanid  = loan_landlord_mgmt.id
 );
 
-USE mogoroomdb;
 UPDATE loan_landlord_buyback SET loan_landlord_buyback.loanChannel  = 1
 WHERE EXISTS(
  SELECT 1 FROM
@@ -18,7 +17,6 @@ INNER JOIN loan_renter_info ON loan_renter_contract.infoid = loan_renter_info.id
 WHERE loan_renter_contract.channel = 1 AND loan_landlord_buyback.loanid  = loan_landlord_mgmt.id
 );
 
-USE mogoroomdb;
 UPDATE loan_landlord_payplan SET loan_landlord_payplan.loanChannel = 2
 WHERE EXISTS(
    SELECT 1 FROM
@@ -28,7 +26,6 @@ INNER JOIN loan_renter_info ON loan_renter_contract.infoid = loan_renter_info.id
 WHERE loan_renter_contract.channel = 2 AND loan_landlord_payplan.loanid  = loan_landlord_mgmt.id
 )
 
-USE mogoroomdb;
 UPDATE loan_landlord_payplan SET loan_landlord_payplan.loanChannel = 1
 WHERE EXISTS(
    SELECT 1 FROM
