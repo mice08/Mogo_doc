565a3b14880289fe71652ce182c2bf3a4549cad6
tzm <tzm@mogoroom.com>
2016-06-24 11:57:18 +0800

    opt@房东帐务#:修改错误数据类型

--- VersionRecords/Version 房东帐务/SQL_Scripts/DDL_pending/房东帐务-tzm.sql
+++ VersionRecords/Version 房东帐务/SQL_Scripts/DDL_pending/房东帐务-tzm.sql
@@ -1,10 +1,10 @@
 /*  Database name `mogoroomdb` ，为帐务需求添加字段*/
 use mogoroomdb;
 
-ALTER TABLE acct.acct_bill ADD COLUMN minPayAmount BigDecimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
+ALTER TABLE acct.acct_bill ADD COLUMN minPayAmount Decimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
 
-ALTER TABLE bill_salebill  ADD COLUMN minPayAmount BigDecimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
+ALTER TABLE bill_salebill  ADD COLUMN minPayAmount Decimal(10,2) DEFAULT NULL COMMENT '最小支付金额';
 
-ALTER TABLE bill_salebill  ADD COLUMN unpayAmount BigDecimal(10,2) DEFAULT NULL COMMENT '未支付金额';
+ALTER TABLE bill_salebill  ADD COLUMN unpayAmount Decimal(10,2) DEFAULT NULL COMMENT '未支付金额';
 
 ALTER TABLE acct.acct_busirec  ADD COLUMN verified tinyint(3) DEFAULT NULL COMMENT '核销标识';
