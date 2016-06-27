8a7f2f86a03beb2b283f100de8ee0d4dbdda5e35
tzm <tzm@mogoroom.com>
2016-06-27 12:17:02 +0800

    opt@3.2.0#:添加收款日期字段

--- VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/帐务界面改造-tzm.sql
+++ VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/帐务界面改造-tzm.sql
@@ -2,12 +2,8 @@
 use mogoroomdb;
 
 ALTER TABLE acct.acct_busirec 
-					ADD COLUMN seq Int(11) DEFAULT NULL COMMENT '属于某统一订单的收款序列号',
-					ADD COLUMN feeDate datetime DEFAULT NULL COMMENT '收款日期';
+					ADD COLUMN origDate datetime DEFAULT NULL COMMENT '原始单据日期';
 															
-															
-ALTER TABLE acct.acct_bill 
-						ADD COLUMN seq Int(11) DEFAULT NULL COMMENT '属于某统一订单的账单序列号';		
 
 
 
