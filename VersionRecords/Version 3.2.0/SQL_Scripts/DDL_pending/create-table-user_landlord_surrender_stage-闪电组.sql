c7cd74d6bcc868d5579110312e2af255da361a80
yb <yb@mogoroom.com>
2016-06-27 17:05:43 +0800

    1

--- VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/create-table-user_landlord_surrender_stage-闪电组.sql
+++ VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/create-table-user_landlord_surrender_stage-闪电组.sql
@@ -1,4 +1,5 @@
-drop table if exists user_landlord_surrender_stage;
+
+use mogoroomdb;
 
 /*==============================================================*/
 /* Table: user_landlord_surrender_stage                         */
@@ -10,10 +11,10 @@ create table user_landlord_surrender_stage
    stage                int(1) not null comment '对应该房东解约的步骤(1:确认解约日期 2:下架官网房源 3:处理在住签约单 4:禁用房东账号)',
    stageValue           varchar(100) comment '对应步骤中产生的值，json格式保存',
    createBy             int(11) not null comment '对应步骤的操作人',
-   createByType         int(1) not null comment '操作人类型(参照groupName=userType)',
+   createByType         int(1) not null comment '操作人类型(参照字典表组名:userType)',
    createTime           datetime not null comment '对应步骤的创建时间',
    updateBy             int(11) comment '对应步骤的修改人',
-   updateByType         char(10) comment '修改人类型(参照groupName=userType)',
+   updateByType         char(10) comment '修改人类型(参照字典表组名:userType)',
    updateTime           datetime not null comment '对应步骤的修改时间',
    primary key (id)
 );
