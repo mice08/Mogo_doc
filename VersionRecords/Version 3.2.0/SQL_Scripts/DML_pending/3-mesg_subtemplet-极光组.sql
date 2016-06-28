bf3884275ad7598b8d8271896d66538054e5efb6
yb <yb@mogoroom.com>
2016-06-28 11:52:27 +0800

    1

--- VersionRecords/Version 3.2.0/SQL_Scripts/DML_pending/3-mesg_subtemplet-极光组.sql
+++ VersionRecords/Version 3.2.0/SQL_Scripts/DML_pending/3-mesg_subtemplet-极光组.sql
@@ -1,6 +1,8 @@
 /* 聚有财-租金宝、蘑菇宝管理模板 */
 USE mogoroomdb;
 
+INSERT INTO mesg_templet (templetCode, templetName, templetDesc, STATUS, createBy, createTime, createByType, updateBy, updateTime, updateByType, valid, businessType) VALUES ('sms_jyc_bill_dueDate_toLandLord', '聚有财常规账单逾期提醒', '聚有财常规账单逾期提醒', 1, 2, STR_TO_DATE('2016-06-16 12:35:55','%Y-%m-%d %H:%i:%s'), 3, NULL, NULL, NULL, 1, 5);
+
 INSERT INTO mesg_subtemplet (templetId, templetType, templetTitle, templetContent, STATUS, valid, outTempletId, jumpCode) VALUES ((SELECT id FROM mesg_templet WHERE templetCode = 'sms_jyc_bill_dueDate_toLandLord'), 1, '聚有财常规账单逾期提醒', '[蘑菇宝]尊敬的房东，今天是您的租客${renterName}（${roomAddress}）${rentMonth}月租金的最后还款日，请督促租客及时缴纳，以免产生滞纳金。', 1, 1, '', NULL);
 
 INSERT INTO mesg_subtemplet (templetId, templetType, templetTitle, templetContent, STATUS, valid, outTempletId, jumpCode) VALUES ((SELECT id FROM mesg_templet WHERE templetCode = 'sms_jyc_bill_dueDate_toLandLord'), 3, '聚有财常规账单逾期提醒', '[蘑菇宝]尊敬的房东，今天是您的租客${renterName}（${roomAddress}）${rentMonth}月租金的最后还款日，请督促租客及时缴纳，以免产生滞纳金。', 1, 1, '', NULL);
