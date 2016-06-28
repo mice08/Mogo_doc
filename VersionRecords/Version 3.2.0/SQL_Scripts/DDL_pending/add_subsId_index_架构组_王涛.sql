78ac6f198776f2546930196e136e574ac4cd5805
wangtao <wangtao@mogoroom.com>
2016-06-28 14:13:09 +0800

    new@3.2.0#: add index for column subsId.

--- VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/add_subsId_index_架构组_王涛.sql
+++ VersionRecords/Version 3.2.0/SQL_Scripts/DDL_pending/add_subsId_index_架构组_王涛.sql
@@ -1,10 +1,11 @@
+use `mogoroomdb`;
+ALTER TABLE `oder_bookorder`  ADD  INDEX `index_subsId` (`subsId`);
+ALTER TABLE `oder_signedorder`  ADD  INDEX `index_subsId` (`subsId`);
+ALTER TABLE `loan_landlord_mgmt`  ADD  INDEX `index_subsId` (`subsId`);
+ALTER TABLE `loan_landlord_buyback`  ADD  INDEX `index_subsId` (`subsId`);
+ALTER TABLE `loan_landlord_payplan`  ADD  INDEX `index_subsId` (`subsId`);
+ALTER TABLE `loan_landlord_repayplan`  ADD  INDEX `index_subsId` (`subsId`);
 
-ALTER TABLE `mogoroomdb`.`oder_bookorder`  ADD  INDEX `index_subsId` (`subsId`);
-ALTER TABLE `mogoroomdb`.`oder_signedorder`  ADD  INDEX `index_subsId` (`subsId`);
+use `acct`;
 ALTER TABLE `acct`.`acct_bill`  ADD  INDEX `index_subsId` (`subsId`);
-
-ALTER TABLE `mogoroomdb`.`loan_landlord_mgmt`  ADD  INDEX `index_subsId` (`subsId`);
-ALTER TABLE `mogoroomdb`.`loan_landlord_buyback`  ADD  INDEX `index_subsId` (`subsId`);
-ALTER TABLE `mogoroomdb`.`loan_landlord_payplan`  ADD  INDEX `index_subsId` (`subsId`);
-ALTER TABLE `mogoroomdb`.`loan_landlord_repayplan`  ADD  INDEX `index_subsId` (`subsId`);
 
