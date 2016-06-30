7ac4b390c710ee1c7b4455c410578043f86c8139
lvchongxin <lcx@mogoroom.com>
2016-06-30 18:59:43 +0800

    opt#3.2.0.2 菜单sql2

--- VersionRecords/Version 3.2.0.2/SQL_Scripts/DML_pending/报表菜单配置.sql
+++ VersionRecords/Version 3.2.0.2/SQL_Scripts/DML_pending/报表菜单配置.sql
@@ -19,7 +19,7 @@ VALUES (@fcode, '查询组织关系', 'report/getNextOrg', '1', '3', @id, '0', NULL, N
 
 
 select @id:=id from perm_functioninfo where fname = '房东登录情况统计报表';
-select @fcode:= CONCAT('3',lpad(@id,4,0),'01' ) from perm_functioninfo where fname = '房东登录情况统计报表';
+select @fcode:= CONCAT('3',lpad(@id,4,0),'02' ) from perm_functioninfo where fname = '房东登录情况统计报表';
 INSERT INTO `perm_functioninfo` 
 ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) 
 VALUES (@fcode, '历史报表', 'report/gethisToryXls', '2', '3', @id, '0', NULL, NULL, NULL, '1', '1', NULL, NULL, NULL, NULL, '1', '2');
@@ -28,7 +28,7 @@ VALUES (@fcode, '历史报表', 'report/gethisToryXls', '2', '3', @id, '0', NULL, NU
 
 
 select @id:=id from perm_functioninfo where fname = '房东登录情况统计报表';
-select @fcode:= CONCAT('3',lpad(@id,4,0),'01' ) from perm_functioninfo where fname = '房东登录情况统计报表';
+select @fcode:= CONCAT('3',lpad(@id,4,0),'03' ) from perm_functioninfo where fname = '房东登录情况统计报表';
 INSERT INTO `perm_functioninfo` 
 ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) 
 VALUES (@fcode, '获取历史信息', 'report/gethisTory', '3', '3', @id, '0', NULL, NULL, NULL, '1', '1', NULL, NULL, NULL, NULL, '1', '2');
