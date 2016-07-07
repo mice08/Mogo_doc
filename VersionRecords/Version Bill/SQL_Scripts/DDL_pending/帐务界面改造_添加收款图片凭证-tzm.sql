c0ae3247c37dfe5429110779729322bb70a26418
tzm <tzm@mogoroom.com>
2016-07-07 15:20:20 +0800

    opt@bill#:添加收款凭证字段

---
+++ VersionRecords/Version Bill/SQL_Scripts/DDL_pending/帐务界面改造_添加收款图片凭证-tzm.sql
@@ -0,0 +1,7 @@
+/*  Database name `mogoroomdb` ，为新帐务页面需求添加字段*/
+use mogoroomdb;
+
+ALTER TABLE acct.acct_busirec 
+ADD COLUMN prooofPicId int(11) DEFAULT NULL COMMENT '凭证图片',
+ADD COLUMN prooofNumber String(45) DEFAULT NULL COMMENT '凭证编号';
+									
