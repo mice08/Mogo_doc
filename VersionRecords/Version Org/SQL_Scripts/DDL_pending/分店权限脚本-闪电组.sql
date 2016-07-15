7458c880f02c05ebf301bf3a6274123a339a6279
syj <syj@mogoroom.com>
2016-07-15 15:49:58 +0800

    脚本修改

--- VersionRecords/Version Org/SQL_Scripts/DDL_pending/分店权限脚本-闪电组.sql
+++ VersionRecords/Version Org/SQL_Scripts/DDL_pending/分店权限脚本-闪电组.sql
@@ -169,3 +169,5 @@ create table perm_group_rel
 ALTER TABLE perm_role ADD COLUMN `roleCode` varchar(10) DEFAULT NULL COMMENT '角色编码';
 
 ALTER TABLE perm_role ADD COLUMN `roleScope` int(2) DEFAULT 1 COMMENT '角色应用范围(0:为部分用户特供的角色 1:全局共享角色)';
+
+ALTER TABLE mesg_category ADD COLUMN remark VARCHAR(500) COMMENT '备注'; 
