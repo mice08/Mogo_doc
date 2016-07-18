7e1d9b513059de58f89fc9dbe5767cd7b101a70b
jingang <jg@mogoroom.com>
2016-07-18 19:28:50 +0800

    opt@3.3.2#: rollback

--- VersionRecords/Version 3.3.2/SQL_Scripts/DDL_pending/用户行为记录表增加uuid字段脚本.sql
+++ VersionRecords/Version 3.3.2/SQL_Scripts/DDL_pending/用户行为记录表增加uuid字段脚本.sql
@@ -4,12 +4,3 @@ use  mogoroomdb;
 ALTER TABLE user_behavior_record ADD COLUMN device_uuid VARCHAR(50) DEFAULT NULL COMMENT '终端设备唯一标识';
 
 ALTER TABLE user_renter ADD COLUMN device_uuid VARCHAR(50) DEFAULT NULL COMMENT '终端设备唯一标识';
-
-ALTER TABLE user_interest_tag ADD COLUMN device_uuid VARCHAR(50) DEFAULT NULL COMMENT '终端设备唯一标识' AFTER `regId`;
-
-/* mogo_da库 */
-use mogo_da;
-
-ALTER TABLE user_interest_tag ADD COLUMN device_uuid VARCHAR(50) DEFAULT NULL COMMENT '终端设备唯一标识' AFTER `regId`;
-
-ALTER TABLE user_behavior_tag ADD COLUMN device_uuid VARCHAR(50) DEFAULT NULL COMMENT '终端设备唯一标识' AFTER `regId`;
