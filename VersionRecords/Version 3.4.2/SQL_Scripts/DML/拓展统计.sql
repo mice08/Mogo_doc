fc42ee1bda0e9538ba4154eac27c3b4f6429bffb
zhanghao <zhanghao@mogoroom.com>
2016-07-28 14:31:53 +0800

    opt@3.4.2#：修改拓展统计SQL报错问题

--- VersionRecords/Version 3.4.2/SQL_Scripts/DML/拓展统计.sql
+++ VersionRecords/Version 3.4.2/SQL_Scripts/DML/拓展统计.sql
@@ -18,11 +18,10 @@ INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLeve
 VALUES ( @fcode, '签约人首页', 'bd/findbdContractTotleList', @seq, '2', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
 
 select @id:=id from perm_functioninfo where fname = '签约人首页';
-select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@id;
-select @fcode:= CONCAT('3',lpad(@id,4,0),@seq ) from perm_functioninfo where fname = '签约人首页';
+select @fcode:= CONCAT('3',lpad(@id,4,0),1 ) from perm_functioninfo where fname = '签约人首页';
 
 INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
-VALUES ( @fcode, '签约人日报', 'bd/findTodayBdContractEmployeeTotleList', @seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+VALUES ( @fcode, '签约人日报', 'bd/findTodayBdContractEmployeeTotleList', 1, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
 
 /*签约组首页 菜单 */
 select @id:=id from perm_functioninfo where fname = '用户首页';
@@ -33,11 +32,10 @@ INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLeve
 VALUES ( @fcode, '签约组首页', 'bd/findbdContractManagerTotleList', @seq, '2', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
 
 select @id:=id from perm_functioninfo where fname = '签约组首页';
-select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@id;
-select @fcode:= CONCAT('3',lpad(@id,4,0),@seq ) from perm_functioninfo where fname = '签约组首页';
+select @fcode:= CONCAT('3',lpad(@id,4,0),1 ) from perm_functioninfo where fname = '签约组首页';
 
 INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
-VALUES ( @fcode, '签约组日报', 'bd/findTodayBdContractManagerTotleList', @seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+VALUES ( @fcode, '签约组日报', 'bd/findTodayBdContractManagerTotleList', 1, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
 
 select @id:=id from perm_functioninfo where fname = '签约组首页';
 select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@id;
