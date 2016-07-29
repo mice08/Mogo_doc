65740d6054d133c6a214bcebde23938fa9559c0b
songwei <songwei@mogoroom.com>
2016-07-29 18:11:44 +0800

    支付押金

--- VersionRecords/Version 3.4.2/SQL_Scripts/DML/统一日志管理_架构组.sql
+++ VersionRecords/Version 3.4.2/SQL_Scripts/DML/统一日志管理_架构组.sql
@@ -10,6 +10,9 @@ delete from `comm_busitype` where busiType in ('160102008');
 INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES('160102008','修改角色',1,1,3,1,'160102');
 
 
+INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES(10101013,'支付押金',1,1,3,1,10103);
+
+
 delete from comm_busitype  
 where id not in
   (
