8b249998f18935eaf2ea6aa56d48cd7a9ef55ae2
tzm <tzm@mogoroom.com>
2016-08-04 15:24:13 +0800

    opt@bill#:修改对账sql

--- VersionRecords/Version Bill/SQL_Scripts/DDL_pending/准实时对账-tzm.sql
+++ VersionRecords/Version Bill/SQL_Scripts/DDL_pending/准实时对账-tzm.sql
@@ -5,10 +5,6 @@ ALTER TABLE acct.acct_check ADD COLUMN beginTime datetime DEFAULT NULL COMMENT '
 ADD COLUMN endTime datetime DEFAULT NULL COMMENT '对账结束时间';
 															
 
-ALTER TABLE acct.acct_check ADD COLUMN beginTime datetime DEFAULT NULL COMMENT '对账开始时间',
-ADD COLUMN endTime datetime DEFAULT NULL COMMENT '对账结束时间';
-
-
 ALTER TABLE acct.acct_checkbillfunddetail modify COLUMN content varchar(256) DEFAULT NULL COMMENT '描述信息';
 
 ALTER TABLE acct.acct_checkbilltradedetail ADD COLUMN billId int(11) DEFAULT NULL COMMENT '账单id';
