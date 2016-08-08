1228482f2f66cffd7a03331e42bd59d15f260bd9
sw <sw160202@mogoroom.com>
2016-08-08 10:31:27 +0800

    new@pixiu#:新增业务操作唯一控制表

--- VersionRecords/Version 5.0.0/SQL_Scripts/DDL_pending/业务操作唯一性控制表-闪电组-孙伟.sql
+++ VersionRecords/Version 5.0.0/SQL_Scripts/DDL_pending/业务操作唯一性控制表-闪电组-孙伟.sql
@@ -1,7 +1,7 @@
-/*  Database name `mogoroomdb` 新增comm_biztypeOrder_record表*/
+/*  Database name `mogoroomdb` 新增comm_unique_operation表*/
 USE mogoroomdb;
-DROP TABLE IF EXISTS comm_biztypeOrder_record;
-CREATE TABLE  comm_biztypeOrder_record (
+DROP TABLE IF EXISTS comm_unique_operation;
+CREATE TABLE  comm_unique_operation (
 	id INT(11) PRIMARY KEY  AUTO_INCREMENT COMMENT '主键id',
 	busiType INT(11) NOT NULL COMMENT '业务类型',
 	orderType INT(11) NOT NULL COMMENT '订单类型',
@@ -12,4 +12,4 @@ CREATE TABLE  comm_biztypeOrder_record (
 	createByType INT(11) NULL COMMENT '创建人类型(参考字典表组名:userType)',
 	remark  VARCHAR(500) NULL COMMENT '备注',
 	UNIQUE INDEX busiType_orderType_oderId(busiType,orderType,orderId)
-)ENGINE = INNODB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT =1 COMMENT '需要处理业务操作记录';
+)ENGINE = INNODB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT =1 COMMENT '业务操作唯一性控制表';
