857fa9f3290e30a442323c302786db39fd7bb36e
qwb <qwb@mogoroom.com>
2016-08-09 10:12:05 +0800

    1

--- VersionRecords/Version 3.4.3/SQL_Scripts/DDL/拓展首页报表相关表添加字段.sql
+++ VersionRecords/Version 3.4.3/SQL_Scripts/DDL/拓展首页报表相关表添加字段.sql
@@ -5,3 +5,13 @@ ALTER TABLE repo_landlord_house_detail ADD COLUMN customerMgr INT(11) COMMENT '·
 ALTER TABLE repo_landlord_house_detail ADD COLUMN customerMgrName VARCHAR(11) COMMENT '·şÎñÈËĞÕÃû' AFTER customerMgr;
 ALTER TABLE repo_landlord_house_sum ADD COLUMN customerMgr INT(11) COMMENT '·şÎñÈËid' AFTER salesmanName;
 ALTER TABLE repo_landlord_house_sum ADD COLUMN customerMgrName VARCHAR(11) COMMENT '·şÎñÈËĞÕÃû' AFTER customerMgr;
+
+ALTER TABLE repo_landlord_house_detail ADD COLUMN customerMgrDeptName VARCHAR(50) COMMENT '·şÎñÈËËùÊô²¿ÃÅ' AFTER salesmanDeptLevel;
+ALTER TABLE repo_landlord_house_detail ADD COLUMN customerMgrDeptId INT(11) COMMENT '·şÎñÈËËùÊô²¿ÃÅid' AFTER customerMgrDeptName;
+ALTER TABLE repo_landlord_house_detail ADD COLUMN customerMgrDeptFullPath VARCHAR(50) COMMENT '·şÎñÈËËùÊô²¿ÃÅÈ«±àÂë' AFTER customerMgrDeptId;
+ALTER TABLE repo_landlord_house_detail ADD COLUMN customerMgrDeptLevel INT(11) COMMENT '·şÎñÈËËùÊô²¿ÃÅ¼¶±ğ' AFTER customerMgrDeptFullPath;
+
+ALTER TABLE repo_landlord_house_sum ADD COLUMN customerMgrDeptName VARCHAR(50) COMMENT '·şÎñÈËËùÊô²¿ÃÅ' AFTER salesmanDeptLevel;
+ALTER TABLE repo_landlord_house_sum ADD COLUMN customerMgrDeptId INT(11) COMMENT '·şÎñÈËËùÊô²¿ÃÅid' AFTER customerMgrDeptName;
+ALTER TABLE repo_landlord_house_sum ADD COLUMN customerMgrDeptFullPath VARCHAR(50) COMMENT '·şÎñÈËËùÊô²¿ÃÅÈ«±àÂë' AFTER customerMgrDeptId;
+ALTER TABLE repo_landlord_house_sum ADD COLUMN customerMgrDeptLevel INT(11) COMMENT '·şÎñÈËËùÊô²¿ÃÅ¼¶±ğ' AFTER customerMgrDeptFullPath;
