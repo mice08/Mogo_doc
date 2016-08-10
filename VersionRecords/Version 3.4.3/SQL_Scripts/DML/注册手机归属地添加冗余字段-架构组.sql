929725a51d9adee5b27ac350d4c249a843ba6bf3
songwei <songwei@mogoroom.com>
2016-08-10 16:48:42 +0800

    去掉冗余

--- VersionRecords/Version 3.4.3/SQL_Scripts/DML/注册手机归属地添加冗余字段-架构组.sql
+++ VersionRecords/Version 3.4.3/SQL_Scripts/DML/注册手机归属地添加冗余字段-架构组.sql
@@ -7,4 +7,8 @@ where t.regMobileCity=mc.areaCode
 and t.regMobileCity is not null
 and t.regMobileCity!='';
 
- 
+ update user_renter ur,comm_dictionary cd,comm_dictionary cd2
+set ur.regChannelDtl=cd.value,ur.regChannelDtlValue=cd2.value
+where 1=1
+ and cd.keyPro=ur.regChannel and cd.groupName='channel'
+ and  cd2.keyPro=ur.regChannelDtl and cd2.groupName='regChannelDtl'
