b263ebc280c6b9544c9431d3da9765560cc57d7c
tzm <tzm@mogoroom.com>
2016-08-10 14:24:03 +0800

    opt@bill#:添加db schema

--- VersionRecords/Version Bill/SQL_Scripts/DML_pending/新增优惠账单类型-tzm.sql
+++ VersionRecords/Version Bill/SQL_Scripts/DML_pending/新增优惠账单类型-tzm.sql
@@ -2,7 +2,7 @@
 use mogoroomdb;
 
 INSERT INTO acct.`acct_billtype` (billType,billName,STATUS,sort,penaltyRate,displayName,remark) VALUES(10010,'退房优惠账单',1,10010,NULL,'退房优惠账单','退房优惠账单');
-insert  into `acct_billtypetofundtype`(`billType`,`fromFundType`,`toFundType`) values (10008,101,101);
-insert  into `acct_billtypetofundtype`(`billType`,`fromFundType`,`toFundType`) values (10009,101,101);
-insert  into `acct_billtypetofundtype`(`billType`,`fromFundType`,`toFundType`) values (10010,101,101);
+insert  into acct.`acct_billtypetofundtype`(`billType`,`fromFundType`,`toFundType`) values (10008,101,101);
+insert  into acct.`acct_billtypetofundtype`(`billType`,`fromFundType`,`toFundType`) values (10009,101,101);
+insert  into acct.`acct_billtypetofundtype`(`billType`,`fromFundType`,`toFundType`) values (10010,101,101);
 
