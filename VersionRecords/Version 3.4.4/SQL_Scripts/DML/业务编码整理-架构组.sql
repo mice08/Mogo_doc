0ecfada3bd0c677255577ac59dd64e69fa4efeb8
songwei <songwei@mogoroom.com>
2016-08-11 19:41:55 +0800

    业务编码

--- VersionRecords/Version 3.4.4/SQL_Scripts/DML/业务编码整理-架构组.sql
+++ VersionRecords/Version 3.4.4/SQL_Scripts/DML/业务编码整理-架构组.sql
@@ -4,6 +4,6 @@ use mogoroomdb;
 delete from `comm_busitype` where busiType in (50102007);
 INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES(50102007,'房东密码修改',1,1,3,1,50102);
 
-INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES( 50106,'其他',1,1,2,0,501 );
+INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES( 50106,'系统',1,1,2,0,501 );	
 delete from `comm_busitype` where busiType in (50106001);	
 INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES(50106001,'房东账号登陆',1,1,3,1,50106);
