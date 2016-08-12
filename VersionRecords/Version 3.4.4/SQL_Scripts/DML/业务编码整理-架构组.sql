009279385bf57eeaca80318feda2303408b2cc37
songwei <songwei@mogoroom.com>
2016-08-12 12:15:36 +0800

    全量业务编码

--- VersionRecords/Version 3.4.4/SQL_Scripts/DML/业务编码整理-架构组.sql
+++
@@ -1,9 +0,0 @@
-/*  Database name `mogoroomdb` 删除重复业务编码*/
-use mogoroomdb;
-  
-delete from `comm_busitype` where busiType in (50102007);
-INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES(50102007,'房东密码修改',1,1,3,1,50102);
-
-INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES( 50106,'系统',1,1,2,0,501 );	
-delete from `comm_busitype` where busiType in (50106001);	
-INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES(50106001,'房东账号登陆',1,1,3,1,50106);
