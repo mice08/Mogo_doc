268c4af67236b7f98683c0a8debe19b637434d44
yb <yb@mogoroom.com>
2016-08-18 15:30:09 +0800

    1

---
+++ VersionRecords/Version 5.0.0/SQL_Scripts/DML_pending/用户绑定来源备注修改 - EQ组 - 副本.sql
@@ -0,0 +1,4 @@
+﻿/*  Database name `mogoroomdb` ，新增支付类型*/
+use mogoroomdb;
+
+insert into `comm_paytype` (`name`, `rentPeriods`, `foregiftPeriods`, `finProdType`, `payTypeGroup`) values('付一押一','1','1','0','1');
