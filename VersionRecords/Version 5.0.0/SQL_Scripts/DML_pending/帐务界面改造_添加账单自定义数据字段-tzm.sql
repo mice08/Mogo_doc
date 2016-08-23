95b7a91a0c01d9c1f9ab67671e8fbd86d36b7850
tzm <tzm@mogoroom.com>
2016-08-23 19:10:56 +0800

    opt@pixiu#:删除多余sql

--- VersionRecords/Version 5.0.0/SQL_Scripts/DML_pending/帐务界面改造_添加账单自定义数据字段-tzm.sql
+++
@@ -1,4 +0,0 @@
-/*  Database name `mogoroomdb` ，为新帐务添加新帐务明细类型*/
-use mogoroomdb;
-
-insert into `acct_billdtltype` (`billDtlType`, `billDtlName`, `status`, `sort`, `usageType`, `displayName`, `remark`) values('100082','账单优惠','1','100082','1','账单优惠','账单优惠');
