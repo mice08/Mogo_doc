b6a7158336e8513d28c848038129ebfde2cb240f
zlj <zlj@mogoroom.com>
2016-08-23 19:55:57 +0800

    增加登录记录表字段

--- VersionRecords/Version 5.0.0/SQL_Scripts/DDL_pending/user_loginlog-发现组.sql
+++ VersionRecords/Version 5.0.0/SQL_Scripts/DDL_pending/user_loginlog-发现组.sql
@@ -1,4 +1,4 @@
 /*user_loginlog表添加是否真实登录字段*/
 USE mogoroomdb;
 
-ALTER TABLE user_loginlog add column isValidated tinyint(1) COMMENT '是否真实登录(0:否 1:是)';  
+ALTER TABLE user_loginlog add column credential varchar(16) COMMENT '用户凭据，如password/token';   
