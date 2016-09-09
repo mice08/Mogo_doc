0be1e6ab85b1bfcc7c21abb1274cd2ff3d325de3
ad <ad@mogoroom.com>
2016-09-09 14:58:51 +0800

    opt@5.0.1#:撤下脚本

--- VersionRecords/Version 5.0.1/SQL_Scripts/DDL_pending/增加是否有效状态字段-马丁组.sql
+++
@@ -1,6 +0,0 @@
-/* 增加是否有效字段 */
-use mogoroomdb;
-
-ALTER TABLE city_community_station ADD COLUMN valid tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否有效(0:无效 1:有效)';
-ALTER TABLE flat_joint_58community ADD COLUMN valid tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否有效(0:无效 1:有效)';
-ALTER TABLE flat_joint_ganjicommunity ADD COLUMN valid tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否有效(0:无效 1:有效)';
