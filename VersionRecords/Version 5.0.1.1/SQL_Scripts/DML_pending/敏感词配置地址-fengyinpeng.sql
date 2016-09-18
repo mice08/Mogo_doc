8f5c77812d253330d727def52a40f151475749f4
tzm <tzm@mogoroom.com>
2016-09-18 19:22:41 +0800

    opt@hotfix0914#:添加查询支付状态sql

--- VersionRecords/Version 5.0.1.1/SQL_Scripts/DML_pending/敏感词配置地址-fengyinpeng.sql
+++ VersionRecords/Version 5.0.1.1/SQL_Scripts/DML_pending/敏感词配置地址-fengyinpeng.sql
@@ -1,2 +1,4 @@
 use mogoroomdb;
 UPDATE comm_sysconfig SET val='/var/lib/docker/contract/sensitive_words/SensitiveWord.txt' WHERE keyName='all-sensitiveword-addr';
+
+UPDATE comm_sysconfig SET val='172.16.1.3' WHERE keyName='bs-order-query';
