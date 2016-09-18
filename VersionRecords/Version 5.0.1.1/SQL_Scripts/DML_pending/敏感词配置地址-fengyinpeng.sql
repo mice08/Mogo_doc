91422cdacb910752812bec26e682e208f34796de
tzm <tzm@mogoroom.com>
2016-09-18 19:43:05 +0800

    opt@hotfix0914#:修改payapi 查询为内部ip

--- VersionRecords/Version 5.0.1.1/SQL_Scripts/DML_pending/敏感词配置地址-fengyinpeng.sql
+++ VersionRecords/Version 5.0.1.1/SQL_Scripts/DML_pending/敏感词配置地址-fengyinpeng.sql
@@ -1,4 +1,4 @@
 use mogoroomdb;
 UPDATE comm_sysconfig SET val='/var/lib/docker/contract/sensitive_words/SensitiveWord.txt' WHERE keyName='all-sensitiveword-addr';
 
-UPDATE comm_sysconfig SET val='pay.api.mogoroom.com' WHERE keyName='bs-order-query';
+UPDATE comm_sysconfig SET val='172.16.1.1:8020' WHERE keyName='bs-order-query';
