d6258f56212df2d31b317a9830bef8ade2bbe943
ad <ad@mogoroom.com>
2016-09-20 10:10:11 +0800

    opt@comm#:小区合并添加菜单SQL

--- VersionRecords/Version 5.0.2/SQL_Scripts/DML/重复小区处理SQL-马丁组-安栋.sql
+++ VersionRecords/Version 5.0.2/SQL_Scripts/DML/重复小区处理SQL-马丁组-安栋.sql
@@ -1,11 +1,5 @@
 use mogoroomdb;
 
-/* 系统内所有"未审核"的小区/大楼，统一修改成"审核通过" */
-UPDATE flat_community SET isverify = 1 WHERE isverify = 0;
-
-/* 系统内所有没有房源的小区/大楼，统一做失效删除 */
-UPDATE flat_community SET STATUS = 0 WHERE id NOT IN (SELECT DISTINCT communityId FROM flat_flats);
-
 /* 小区合并功能添加菜单 */
 SELECT @pid := id FROM perm_functioninfo WHERE furl = 'community/findCommunitiesByFeature';
 SELECT @maxSeq := MAX(seq) FROM perm_functioninfo WHERE functionpId = @pid;
