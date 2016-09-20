d765bb78d433798e93e051b6688b95ba225b11c1
pengkun <pengkun@mogoroom.com>
2016-09-20 11:32:21 +0800

    opt@权限#: 头加日志

--- VersionRecords/Version 5.0.2/SQL_Scripts/DML/增加日志类型-发现组.sql
+++ VersionRecords/Version 5.0.2/SQL_Scripts/DML/增加日志类型-发现组.sql
@@ -1,3 +1,5 @@
+/*  Database name `mogoroomdb`  增加日志类型 */
 USE mogoroomdb;
 
+/*插入解锁子帐号*/
 INSERT INTO `comm_busitype`(busiType,busiName,status,sort,level,isLeaf,parentId) VALUES(50102010,'解锁子帐号',1,1,3,1,50102);
