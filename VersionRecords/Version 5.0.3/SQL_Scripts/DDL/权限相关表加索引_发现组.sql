0a880d4ac192beaee9786bf0fced9d24a805aa94
pengkun <pengkun@mogoroom.com>
2016-09-26 10:15:44 +0800

    new@技术#: 慢sql优化

---
+++ VersionRecords/Version 5.0.3/SQL_Scripts/DDL/权限相关表加索引_发现组.sql
@@ -0,0 +1,9 @@
+﻿/*  Database name `mogoroomdb`  优化查询 */
+USE mogoroomdb;
+
+/*用户组织*/
+create index IDX_userId on perm_user_org_role(userId);
+/*组织分类*/
+create index IDX_roleId on mesg_category_role(roleId);
+/*子帐号查房东*/
+create index IDX_userId on user_landlord_childacc_rel(userId);
