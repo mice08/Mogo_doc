446924e2865e6c055cc892311ba203dc7eb322c6
zlj <zlj@mogoroom.com>
2016-10-10 14:09:24 +0800

    更改名称

--- VersionRecords/Version 5.0.3/SQL_Scripts/DML/视频管理BS菜单-发现组-船长.sql
+++
@@ -1,6 +0,0 @@
-
-/*视频管理初始化脚本  */
-use mogoroomdb;
-
-/* 初始化视频菜单根目录 */
-INSERT INTO `coms_menu` (`code`, `name`, `url`, `seq`, `level`, `createBy`, `createTime`, `updateBy`, `updateTime`, `status`, `type`, `pid`, `channel`, `isMenu`) VALUES ('501000001', '根', NULL, '1', '0', NULL, NOW(), NULL, NOW(), '1', '1', '0', '5', '1');
