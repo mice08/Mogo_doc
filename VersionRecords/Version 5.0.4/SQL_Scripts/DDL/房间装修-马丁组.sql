aee517de980040a945e4f9fb014b40b909e63fd6
xuwenrui <xuwenrui@mogoroom.com>
2016-10-10 11:32:15 +0800

    opt@develop#:修改装修标签sql

--- VersionRecords/Version 5.0.4/SQL_Scripts/DDL/房间装修-马丁组.sql
+++ VersionRecords/Version 5.0.4/SQL_Scripts/DDL/房间装修-马丁组.sql
@@ -2,7 +2,7 @@
 use mogoroomdb;
 /*添加装修中标签*/
 INSERT INTO `flat_tag` (category,NAME,userId,userType,createTime) 
-VALUE (1,'装修中',3102371,0,NOW());
+VALUE (1,'装修中',92158,0,NOW());
 
 /*房间标签表*/
 CREATE TABLE `flat_room_tag` (
@@ -17,8 +17,8 @@ CREATE TABLE `flat_room_tag` (
   `updateBy` int(11) NOT NULL COMMENT '修改人',
   `updateByType` int(11) NOT NULL COMMENT '修改人类型(参考字典表组名:userType)',
   `soDoneCode` int(11) NOT NULL COMMENT '操作流水号',
-  `status` tinyint(3) NOT NULL DEFAULT '1' COMMENT '1.进行中0.结束',
-  `valid` tinyint(3) NOT NULL DEFAULT '1' COMMENT '状态，用于标示该记录是否逻辑删除',
+  `status` tinyint(3) NOT NULL DEFAULT '1' COMMENT '标签状态：1：进行中 0：结束',
+  `valid` tinyint(3) NOT NULL DEFAULT '1' COMMENT '数据状态：1：有效 0：删除',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='房间标签表';
