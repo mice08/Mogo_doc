3904905b44d22b021792da537d652b86397c93b8
yb <yb@mogoroom.com>
2016-10-24 20:58:24 +0800

    1

--- VersionRecords/Version 5.0.4/SQL_Scripts/DML/JG01.mesg_read.sql
+++ VersionRecords/Version 5.0.4/SQL_Scripts/DML/JG01.mesg_read.sql
@@ -1,8 +1,38 @@
 /*删除重复的消息查看记录*/
 use mogoroomdb;
 
-delete FROM mesg_read WHERE id IN (SELECT MAX(id) FROM mesg_read GROUP BY userid HAVING COUNT(*) >1);
+delete from mesg_read where id in 
+(
+    select a.id from 
+    (
+        select max(id) id from mesg_read a where EXISTS
+        (
+            select 1 from mesg_read b where a.id=b.id group by userid HAVING count(1)>1
+        )
+        group by id
+    ) a
+);
 
-delete FROM mesg_read WHERE id IN (SELECT MAX(id) FROM mesg_read GROUP BY userid HAVING COUNT(*) >1);
+delete from mesg_read where id in 
+(
+    select a.id from 
+    (
+        select max(id) id from mesg_read a where EXISTS
+        (
+            select 1 from mesg_read b where a.id=b.id group by userid HAVING count(1)>1
+        )
+        group by id
+    ) a
+);
 
-delete FROM mesg_read WHERE id IN (SELECT MAX(id) FROM mesg_read GROUP BY userid HAVING COUNT(*) >1);
+delete from mesg_read where id in 
+(
+    select a.id from 
+    (
+        select max(id) id from mesg_read a where EXISTS
+        (
+            select 1 from mesg_read b where a.id=b.id group by userid HAVING count(1)>1
+        )
+        group by id
+    ) a
+);
