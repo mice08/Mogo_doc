ab57d8d2e4694bb044a311d609e75e2a861af1f0
xuegz <xueguanzhou@mogoroom.com>
2016-10-28 11:20:57 +0800

    opt@C_UI4.0#:增加数据sql

---
+++ VersionRecords/Version C_UI4.0/SQL_Scripts/DML/EQ02.coms_menu插入登录页banner.sql
@@ -0,0 +1,7 @@
+/* Database name `mogoroomdb`£¬ µÇÂ¼Ò³-ÍÆ¼öÎ» */
+
+use mogoroomdb;
+
+
+INSERT INTO `coms_menu` (`code`, `name`, `url`, `seq`, `level`, `createBy`, `createTime`, `updateBy`, `updateTime`, `status`, `type`, `pid`, channel, isMenu) 
+VALUES(213001,'¹ÙÍø_µÇÂ¼Ò³_¶¥²¿ÂÖ²¥','','8','2','2000082',NOW(),'2000082',NOW(),'1','1','0', '3', '0');
