5f05df9c1cb094ea7d18fff4ec3b629c0b1f3bff
jilinqiang <jlq@mogoroom.com>
2016-10-29 17:44:03 +0800

    增加推荐位

--- VersionRecords/Version C_UI4.0/SQL_Scripts/DML/EQ02.coms_menu插入登录页banner.sql
+++ VersionRecords/Version C_UI4.0/SQL_Scripts/DML/EQ02.coms_menu插入登录页banner.sql
@@ -1,7 +1,12 @@
-/* Database name `mogoroomdb`， 登录页-推荐位 */
+/* Database name `mogoroomdb`， CMS增加内容推荐位 */
 
 use mogoroomdb;
 
+INSERT INTO `coms_menu` (`code`, `name`, `url`, `seq`, `level`, `createBy`, `createTime`, `updateBy`, `updateTime`, `status`, `type`, `pid`, channel, isMenu) 
+VALUES(213001,'官网_登录页_顶部轮播','','0','2','2000082',NOW(),'2000082',NOW(),'1','1','0', '3', '0');
 
 INSERT INTO `coms_menu` (`code`, `name`, `url`, `seq`, `level`, `createBy`, `createTime`, `updateBy`, `updateTime`, `status`, `type`, `pid`, channel, isMenu) 
-VALUES(213001,'官网_登录页_顶部轮播','','8','2','2000082',NOW(),'2000082',NOW(),'1','1','0', '3', '0');
+VALUES(201019,'官网_首页_推荐品牌主题','','0','3','2000082',NOW(),'2000082',NOW(),'1','1','0', '3', '0');
+
+INSERT INTO `coms_menu` (`code`, `name`, `url`, `seq`, `level`, `createBy`, `createTime`, `updateBy`, `updateTime`, `status`, `type`, `pid`, channel, isMenu) 
+VALUES(301019,'APP_首页_推荐品牌主题','','0','3','2000082',NOW(),'2000082',NOW(),'1','1','0', '1', '0');
