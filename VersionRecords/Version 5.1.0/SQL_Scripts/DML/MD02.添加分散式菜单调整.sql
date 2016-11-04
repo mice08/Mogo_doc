526530ccfa189e7d21f8ddf80938b560b63126fc
zhangfei <zhangfei@mogoroom.com>
2016-11-04 12:28:25 +0800

    增加sql

---
+++ VersionRecords/Version 5.1.0/SQL_Scripts/DML/MD02.添加分散式菜单调整.sql
@@ -0,0 +1,6 @@
+﻿USE mogoroomdb;
+/* 添加分散式房源菜单调整*/
+UPDATE perm_functioninfo set furl = '/mogoroom-partnerpc/flatsInput/addDisperse' where furl = '/mogoroom-partnerpc/flats/turnInitializeData';
+UPDATE perm_functioninfo set furl = '/mogoroom-partnerpc/flatsInput/editDisperseEntire' where furl ='/mogoroom-partnerpc/flats/editDisperseFlats' and fcode='1110000';
+UPDATE perm_functioninfo set furl = '/mogoroom-partnerpc/flatsInput/editDisperseJoin' where furl = '/mogoroom-partnerpc/flats/editDisperseFlats' and fcode='1130000';
+UPDATE perm_functioninfo set furl = '/mogoroom-partnerpc/flatsInput/editDisperseJoin' where furl = '/mogoroom-partnerpc/flats/editDisperseRoom';
