9112fb73fb735a8424a1677e7662d12962cf9af3
yb <yb@mogoroom.com>
2016-11-08 16:35:08 +0800

    1

--- VersionRecords/Version yuefu/SQL_Scripts/DDL/JG06.loan_landlord_credit.sql
+++ VersionRecords/Version yuefu/SQL_Scripts/DDL/JG06.loan_landlord_credit.sql
@@ -13,22 +13,20 @@ ALTER TABLE `loan_landlord_credit`
   ADD COLUMN `effectTime` DATETIME NULL COMMENT '生效时间',
   ADD COLUMN `effectBy` INT (11) NULL COMMENT '生效操作人',
   ADD COLUMN `effectByType` INT (1) NULL COMMENT '生效操作人类型(参照字典表组名:userType)',
-  ADD COLUMN `changeTime` DATETIME NULL COMMENT '暂停/恢复时间',
-  ADD COLUMN `changeBy` INT (11) NULL COMMENT '暂停/恢复操作人',
-  ADD COLUMN `changeByType` INT (1) NULL COMMENT '暂停/恢复操作人类型(参照字典表组名:userType)',
+  ADD COLUMN `alterTime` DATETIME NULL COMMENT '暂停/恢复时间',
+  ADD COLUMN `alterBy` INT (11) NULL COMMENT '暂停/恢复操作人',
+  ADD COLUMN `alterByType` INT (1) NULL COMMENT '暂停/恢复操作人类型(参照字典表组名:userType)',
   ADD COLUMN `endTime` DATETIME NULL COMMENT '终止时间',
   ADD COLUMN `endBy` INT (11) NULL COMMENT '终止操作人',
   ADD COLUMN `endByType` INT (1) NULL COMMENT '终止操作人类型(参照字典表组名:userType)',
   DROP COLUMN `credentialsId`,
-  DROP COLUMN `valid`,
-  ;
+  DROP COLUMN `valid` ;
 
 
 ALTER TABLE `loan_landlord_credit_his` 
   ADD COLUMN `rentAmount` DECIMAL (12, 2) DEFAULT 0 NULL COMMENT '月租金总额',
   DROP COLUMN `credentialsId`,
-  DROP COLUMN `valid`,
-  ;
+  DROP COLUMN `valid` ;
   
  DROP TABLE IF EXISTS `loan_landlord_credentials`;
  DROP TABLE IF EXISTS `loan_landlord_credentials_his`;
