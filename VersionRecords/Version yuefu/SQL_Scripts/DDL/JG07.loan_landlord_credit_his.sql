77adcdc407f4ad04fb99532db6b3e48fdce2109e
yb <yb@mogoroom.com>
2016-11-10 11:03:33 +0800

    1

--- VersionRecords/Version yuefu/SQL_Scripts/DDL/JG07.loan_landlord_credit_his.sql
+++ VersionRecords/Version yuefu/SQL_Scripts/DDL/JG07.loan_landlord_credit_his.sql
@@ -13,11 +13,3 @@ ALTER TABLE `loan_landlord_credit_his`
   ADD COLUMN `endTime` DATETIME NULL COMMENT '终止时间',
   ADD COLUMN `endBy` INT (11) NULL COMMENT '终止操作人',
   ADD COLUMN `endByType` INT (1) NULL COMMENT '终止操作人类型(参照字典表组名:userType)' ;
-
-  
-  
-  
-  43
-  
-  
-  33 + 11 =44
