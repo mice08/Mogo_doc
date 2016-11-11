0d6fbcf9a68675116fe1d046aac4f7893e4ab2ee
lvchongxin <lcx@mogoroom.com>
2016-11-11 10:56:39 +0800

    opt # 添加系统配置

---
+++ VersionRecords/Version yuefu/SQL_Scripts/DML/JG04.新增蘑菇金融申请开关配置.sql
@@ -0,0 +1,37 @@
+﻿use mogoroomdb;
+/* Database name `mogoroomdb` ，新增蘑菇金融申请开关配置*/
+
+INSERT INTO comm_sysconfig (
+
+	`keyName`,
+	`val`,
+	`groupName`,
+	`name`,
+	`remark`
+)
+VALUES
+	(
+		'loan_mothPaySwich',
+		'0',
+		'LOAN_FINANCE_SERVICE',
+		'月付申请开关',
+		NULL
+	);
+
+INSERT INTO comm_sysconfig(
+
+	`keyName`,
+	`val`,
+	`groupName`,
+	`name`,
+	`remark`
+
+)
+VALUES
+	(
+		'loan_mogoBaoSwich',
+		'0',
+		'LOAN_FINANCE_SERVICE',
+		'蘑菇宝申请开关',
+		NULL
+	);
