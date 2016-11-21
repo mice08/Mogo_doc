e8f37bba6e73d2037ad65bd537da6b7d08688ff1
tzm <tzm@mogoroom.com>
2016-11-21 17:10:26 +0800

    opt@checkoutnew#:修改冲抵表id描述

--- VersionRecords/Version CheckoutNew/SQL_Scripts/DDL/AR02.退房-新增账单冲抵表-架构-田志敏.sql
+++ VersionRecords/Version CheckoutNew/SQL_Scripts/DDL/AR02.退房-新增账单冲抵表-架构-田志敏.sql
@@ -3,7 +3,7 @@ use mogoroomdb;
 
 
 CREATE TABLE `acct_bill_rel` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `billId` int(11) NOT NULL COMMENT '账单标识',
   `billType` int(11) DEFAULT NULL COMMENT '账单类型',
   `billOrder` int(11) DEFAULT NULL COMMENT '冲抵顺序',
