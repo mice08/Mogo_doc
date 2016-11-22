2cfce5e5b84a427f9015e17d140b68e93a595662
wanghui <wanghui@www.mogoroom.com>
2016-11-22 16:00:06 +0800

    --------------

--- VersionRecords/Version 5.1.1/SQL_Scripts/DDL/JB01.物理删除改造-沧浪（不能热发）.sql
+++
@@ -1,7 +0,0 @@
-﻿/*物理删除改造*/
-use mogoroomdb;
-
-ALTER TABLE `bill_account_mapping`ADD COLUMN `valid`  tinyint(1) NOT NULL DEFAULT 1 COMMENT '有效标识 0:无效 1:有效' AFTER `orderId`;
-ALTER TABLE `orde_signedorder_landlord`ADD COLUMN `valid`  tinyint(1) NOT NULL DEFAULT 1 COMMENT '有效标识 0:无效 1:有效' AFTER `status`;
-ALTER TABLE `cntr_flat_furniture`ADD COLUMN `valid`  tinyint(1) NOT NULL DEFAULT 1 COMMENT '有效标识 0:无效 1:有效' AFTER `orderSeq`;
-ALTER TABLE `cntr_room_furniture`ADD COLUMN `valid`  tinyint(1) NOT NULL DEFAULT 1 COMMENT '有效标识 0:无效 1:有效' AFTER `orderSeq`;
