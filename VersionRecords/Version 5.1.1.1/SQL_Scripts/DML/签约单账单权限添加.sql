97255dc1cdc87a8cdf8751d3253442862c1a96d7
qwb <qwb@mogoroom.com>
2016-11-24 14:04:24 +0800

    签约单账单权限添加

---
+++ VersionRecords/Version 5.1.1.1/SQL_Scripts/DML/签约单账单权限添加.sql
@@ -0,0 +1,20 @@
+/*
+SQLyog Ultimate v11.27 (32 bit)
+MySQL - 10.1.14-MariaDB 
+*********************************************************************
+*/
+use mogoroomdb;
+
+insert into `perm_role_function` (`role_id`, `function_id`) values('1','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('18','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('38','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('46','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('57','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('59','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('63','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('69','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('74','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('76','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('79','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('90','754');
+insert into `perm_role_function` (`role_id`, `function_id`) values('162','754');
