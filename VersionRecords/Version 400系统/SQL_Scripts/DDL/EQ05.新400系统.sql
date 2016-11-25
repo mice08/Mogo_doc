9abf9f5bf4ae257259a7d5bebafa50e412f617e8
jingang <jg@mogoroom.com>
2016-11-25 14:06:45 +0800

    opt@400Sys#: 业务日志表增加订单类型字段(orderType)

---
+++ VersionRecords/Version 400系统/SQL_Scripts/DDL/EQ05.新400系统.sql
@@ -0,0 +1,5 @@
+﻿/* Database name `mogoroomdb`, 400新系统相关表修改 */
+
+use mogoroomdb;
+
+ALTER TABLE `comm_business_record` ADD COLUMN `orderType` TINYINT(3) NOT NULL DEFAULT '0' COMMENT '订单类型(0:签约单, 1:400分机号)' AFTER `orderId`;
