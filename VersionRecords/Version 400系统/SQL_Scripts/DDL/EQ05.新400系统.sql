fd8a7d59bbeb095177f356af28f7905b8c823414
wangtao <wangtao@mogoroom.com>
2016-11-25 14:39:15 +0800

    opt@400#: add union index for comm_business_record.

--- VersionRecords/Version 400系统/SQL_Scripts/DDL/EQ05.新400系统.sql
+++ VersionRecords/Version 400系统/SQL_Scripts/DDL/EQ05.新400系统.sql
@@ -3,3 +3,4 @@
 use mogoroomdb;
 
 ALTER TABLE `comm_business_record` ADD COLUMN `orderType` TINYINT(3) NOT NULL DEFAULT '0' COMMENT '订单类型(0:签约单, 1:400分机号)' AFTER `orderId`;
+ALTER TABLE `comm_business_record` ADD INDEX  `orderIdAndType` (`orderId`, `orderType`) ;
