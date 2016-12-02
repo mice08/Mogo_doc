1abbe2585f0da345d558838c00ef38b699c62609
ad <ad@mogoroom.com>
2016-12-02 14:37:24 +0800

    opt@58#:58对接账号表

--- VersionRecords/Version 58join/SQL_Scripts/DDL/MD02.58对接账号表.sql
+++ VersionRecords/Version 58join/SQL_Scripts/DDL/MD02.58对接账号表.sql
@@ -3,11 +3,11 @@ use mogoroomdb;
 /*创建58同城房源对接账号表*/
 CREATE TABLE flat_joint_58account (
   	id int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  	uid varchar(32) DEFAULT NULL COMMENT '58同城用户id',
+  	uid varchar(128) DEFAULT NULL COMMENT '58同城用户id',
   	clientId varchar(32) NOT NULL COMMENT '合作伙伴id',
-  	secret varchar(64) NOT NULL COMMENT '密钥',
-  	accessToken varchar(64) DEFAULT NULL COMMENT '令牌',
-  	refreshToken varchar(64) DEFAULT NULL COMMENT '刷新后令牌',
+  	secret varchar(128) NOT NULL COMMENT '密钥',
+  	accessToken varchar(128) DEFAULT NULL COMMENT '令牌',
+  	refreshToken varchar(128) DEFAULT NULL COMMENT '刷新后令牌',
   	timeSign varchar(16) DEFAULT NULL COMMENT '当前系统时间戳',
   	code varchar(64) DEFAULT NULL COMMENT '临时验证码',
   	expiresIn int(11) DEFAULT NULL COMMENT '令牌有效时间',
