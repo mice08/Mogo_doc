a62d97b53664d35954c2b9842fbe593990bb4887
hmj <hmj@mogoroom.com>
2016-12-04 17:11:45 +0800

    yuefu@初始化月付手续费配置

---
+++ VersionRecords/Version yuefu/SQL_Scripts/DML/JG07.初始化月付手续费配置.sql
@@ -0,0 +1,4 @@
+INSERT INTO `flat_roomprice` (`goodsId`, `goodsIdType`, `bizType`, `startTime`, `endTime`, `createTime`, `createBy`, `createByType`, `createChannel`, `deleteTime`, `deleteBy`, `deleteByType`, `deleteChannel`, `valid`) 
+VALUES('2','3','9997',SYSDATE(),'9999-01-01 00:00:00',SYSDATE(),'0','0','5',NULL,NULL,NULL,NULL,'1');
+INSERT INTO `flat_roompricedtl` (`priceId`, `billType`, `billDtlType`, `amount`) VALUES((SELECT id FROM flat_roomprice  WHERE goodsId = 2 AND goodsIdType= 3   AND bizType = 9997),'10011','10002','150.00');		
+
