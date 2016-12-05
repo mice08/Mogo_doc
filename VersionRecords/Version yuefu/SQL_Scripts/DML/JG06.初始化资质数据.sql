196bc8013b379cfae923d29cab75d5b83bbb5ad5
hmj <hmj@mogoroom.com>
2016-12-05 13:45:08 +0800

    yuefu@修改初始化资质脚本

--- VersionRecords/Version yuefu/SQL_Scripts/DML/JG06.初始化资质数据.sql
+++ VersionRecords/Version yuefu/SQL_Scripts/DML/JG06.初始化资质数据.sql
@@ -17,10 +17,7 @@ WHERE  credit.loanChannel = 3;
 
 
 /* 拉卡拉都是1对1的（1个contract对应1个credit）。现在应该不能申请拉卡拉资质了，这直接做数据初始化也行，做了也没什么用，credit表中要初始化contractId和prodTypeCode */
-/*UPDATE loan_landlord_credit credit,loan_landlord_contract contract      
-SET contractId = contract.id
-WHERE  credit.landlordId = contract.landlordId 
-AND  credit.loanChannel = 1 AND contract.loanChannel = 1;*/
+/*UPDATE loan_landlord_credit credit,loan_landlord_contract contract SET contractId = contract.id WHERE  credit.landlordId = contract.landlordId AND  credit.loanChannel = 1 AND contract.loanChannel = 1;*/
 
 
 /*初始化聚有财历史表(渠道为3)的prodTypeCode(1000001)  contractId*/
