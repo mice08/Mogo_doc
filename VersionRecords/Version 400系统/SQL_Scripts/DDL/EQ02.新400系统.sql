5ac3830ba3ac2d3f2a76a101003e566ec23508fc
jingang <jg@mogoroom.com>
2016-12-08 11:50:45 +0800

    opt@400Sys#: 部分Sql已经在5.1.5版本预先执行过，予以删除

--- VersionRecords/Version 400系统/SQL_Scripts/DDL/EQ02.新400系统.sql
+++ VersionRecords/Version 400系统/SQL_Scripts/DDL/EQ02.新400系统.sql
@@ -2,6 +2,5 @@
 
 use mogoroomdb;
 
-ALTER TABLE `flat_room_user_relation` ADD COLUMN `extNumId` INT(11) NOT NULL DEFAULT '-1' COMMENT '分机号Id' AFTER `roomId`;
 ALTER TABLE `tele_ext_usr_rel` ADD COLUMN `isDefault` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '是默认分机(0:不是默认, 1:是默认)' AFTER `userType`;
 ALTER TABLE `tele_ext_usr_rel_his` ADD COLUMN `isDefault` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '是默认分机(0:不是默认, 1:是默认)' AFTER `userType`;
--- VersionRecords/Version 400系统/SQL_Scripts/DDL/EQ05.新400系统.sql
+++
@@ -1,7 +0,0 @@
-﻿/* Database name `mogoroomdb`, 400新系统相关表修改 */
-
-use mogoroomdb;
-
-ALTER TABLE `comm_business_record` ADD COLUMN `orderType` TINYINT(3) NOT NULL DEFAULT '0' COMMENT '订单类型(0:签约单, 1:400分机号)' AFTER `orderId`;
-ALTER TABLE `comm_business_record` DROP INDEX `orderId`;
-ALTER TABLE `comm_business_record` ADD  INDEX  `orderIdAndType` (`orderId`, `orderType`);
