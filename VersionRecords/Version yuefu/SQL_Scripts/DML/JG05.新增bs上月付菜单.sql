7c645fb601e76fd49fbf631dc715aad3b159390a
hmj <hmj@mogoroom.com>
2016-12-13 19:55:11 +0800

    yuefu@修改bs菜单名称

--- VersionRecords/Version yuefu/SQL_Scripts/DML/JG05.新增bs上月付菜单.sql
+++ VersionRecords/Version yuefu/SQL_Scripts/DML/JG05.新增bs上月付菜单.sql
@@ -42,7 +42,7 @@ VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e
 INSERT INTO `perm_functioninfo` (`fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) 
 VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付'),4,0)),'05')),'蘑菇月付还款列表','monthPay/queryRenterReapyList','5','2',(SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付'),'1',NULL,NULL,NULL,'0','1',NULL,NULL,NULL,NULL,'1','2');
 INSERT INTO `perm_functioninfo` (`fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) 
-VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付'),4,0)),'06')),'蘑菇月付清偿列表','monthPay/loanMonthlyBuyBackList','6','2',(SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付'),'1',NULL,NULL,NULL,'0','1',NULL,NULL,NULL,NULL,'1','2');
+VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付'),4,0)),'06')),'蘑菇月付买回列表','monthPay/loanMonthlyBuyBackList','6','2',(SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付'),'1',NULL,NULL,NULL,'0','1',NULL,NULL,NULL,NULL,'1','2');
 
 
 INSERT INTO `perm_functioninfo` (`fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) 
@@ -101,9 +101,9 @@ VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e
 
 
 INSERT INTO `perm_functioninfo` (`fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) 
-VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付清偿列表'),4,0)),'01')),'查询蘑菇月付清偿列表','monthPay/loanMonthlyBuyBackList','1','3',(SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付清偿列表'),'0',NULL,NULL,NULL,'0','1',NULL,NULL,NULL,NULL,'1','2');
+VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付买回列表'),4,0)),'01')),'查询蘑菇月付买回列表','monthPay/loanMonthlyBuyBackList','1','3',(SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付买回列表'),'0',NULL,NULL,NULL,'0','1',NULL,NULL,NULL,NULL,'1','2');
 INSERT INTO `perm_functioninfo` (`fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) 
-VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付清偿列表'),4,0)),'02')),'导出蘑菇清偿列表','monthPay/loanMonthlyBuyBackReport','3','3',(SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付清偿列表'),'0',NULL,NULL,NULL,'0','1',NULL,NULL,NULL,NULL,'1','2');
+VALUES((SELECT CONCAT(2,(SELECT LPAD((SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付买回列表'),4,0)),'02')),'导出蘑菇买回列表','monthPay/loanMonthlyBuyBackReport','3','3',(SELECT id FROM perm_functioninfo e WHERE e.fname = '蘑菇月付买回列表'),'0',NULL,NULL,NULL,'0','1',NULL,NULL,NULL,NULL,'1','2');
 
 
 COMMIT;
