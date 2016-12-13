d1d52c8f1e398a4815c0e3e7296abc44f2280c9b
qwb <qwb@mogoroom.com>
2016-12-13 15:46:53 +0800

    a

--- VersionRecords/Version 5.1.7/SQL_Scripts/DML/PS01.添加账单类型统计菜单.sql
+++ VersionRecords/Version 5.1.7/SQL_Scripts/DML/PS01.添加账单类型统计菜单.sql
@@ -7,3 +7,7 @@ select @fcode:= CONCAT('3',lpad(@id,4,0),2 ) from perm_functioninfo where fname 
 
 INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
 VALUES ( @fcode, '账单类型统计', 'landlordBillRepo/findLandlordBillTypeRepo', @seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+
+
+INSERT INTO perm_role_function (`function_id`,`role_id`)VALUES
+((SELECT id FROM perm_functioninfo WHERE fname='账单类型统计'),(SELECT id FROM perm_role WHERE rolename='运营报表'));
