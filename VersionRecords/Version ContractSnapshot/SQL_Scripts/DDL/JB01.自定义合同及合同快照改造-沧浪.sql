db287597edc0455903ca3e955d6b1cb76f5d6763
wanghui <wanghui@www.mogoroom.com>
2016-12-14 20:49:11 +0800

    自定义合同DDL修改3

--- VersionRecords/Version ContractSnapshot/SQL_Scripts/DDL/JB01.自定义合同及合同快照改造-沧浪.sql
+++ VersionRecords/Version ContractSnapshot/SQL_Scripts/DDL/JB01.自定义合同及合同快照改造-沧浪.sql
@@ -9,9 +9,8 @@ ALTER TABLE `cntr_salecontract` ADD COLUMN `templetCode`  int NULL COMMENT '模板
 /*模板添加4个字段，变更2个字段的语义*/
 ALTER TABLE `cntr_contract_templet`
 MODIFY COLUMN `templetCode`  varchar(20) NOT NULL COMMENT '合同模板编码（编号）' AFTER `templetName`,
-MODIFY COLUMN `status`  tinyint(1) NOT NULL COMMENT '模板状态（参考字典表组名:cntrTempletStatus）' AFTER `templetPath`,
-ADD COLUMN `templetType`  tinyint(1) NULL COMMENT '模板类型（参考字典表组名:cntrTempletType）' AFTER `templetPath`,
-ADD COLUMN `templetStatus`  tinyint(1) NULL COMMENT '模板流程状态（参考字典表组名:cntrTempletFlow）' AFTER `templetType`,
-ADD COLUMN `belongContractType` tinyint(1) NULL COMMENT '所属合同类型（参考字典表组名:cntrTempletBelong）' ,
+MODIFY COLUMN `status`  tinyint(1) NOT NULL COMMENT '模板状态(0:初始 1:确认生效 2:暂停 3.永久停用)' AFTER `templetPath`,
+ADD COLUMN `templetType`  tinyint(1) NULL COMMENT '模板类型(1:通用 2:私有)' AFTER `templetPath`,
+ADD COLUMN `belongContractType` tinyint(1) NULL COMMENT '所属合同类型(1:租房 2:金融)' ,
 ADD COLUMN `remark`  varchar(128) NULL COMMENT '备注',
-ADD COLUMN `valid`  tinyint(1) NULL COMMENT '0：无效 1：有效';
+ADD COLUMN `valid`  tinyint(1) NULL COMMENT '有效性(0:无效 1:有效)';
