bca7b129d01f71c7ea108d55d259e4cb92909bd7
hmj <hmj@mogoroom.com>
2016-12-14 20:57:49 +0800

    yuefu@修改扩展表字段长度

--- VersionRecords/Version online/SQL_Scripts/DDL/JG02.loanMpayapplyApprove.sql
+++ VersionRecords/Version online/SQL_Scripts/DDL/JG02.loanMpayapplyApprove.sql
@@ -5,3 +5,7 @@ USE mogoroomdb;
 ALTER TABLE loan_mpayapply_approve   
   CHANGE `remark` `remark1` VARCHAR(256)  NULL  COMMENT '拓展专员审核备注',
   ADD COLUMN `remark2` VARCHAR(256) NULL  COMMENT '信审员审核备注' AFTER `remark1`;
+  
+ ALTER TABLE loan_mpayapply_approve_log   
+  CHANGE `remark` `remark1` VARCHAR(256)  NULL  COMMENT '拓展专员审核备注',
+  ADD COLUMN `remark2` VARCHAR(256) NULL  COMMENT '信审员审核备注' AFTER `remark1`;
