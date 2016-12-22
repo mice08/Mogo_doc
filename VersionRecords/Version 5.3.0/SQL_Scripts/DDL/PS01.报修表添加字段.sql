ba5bcfdf9d7646d116d7f76f41b853a11a27d160
wangyan <wangyan@mogoroom.com>
2016-12-22 10:54:17 +0800

    opt@v5.1.9#:上线清单&脚本

--- VersionRecords/Version 5.3.0/SQL_Scripts/DDL/PS01.报修表添加字段.sql
+++ VersionRecords/Version 5.3.0/SQL_Scripts/DDL/PS01.报修表添加字段.sql
@@ -1,7 +1,7 @@
 /* Database name `mogoroomdb`, 增加报修表字段 */
 USE mogoroomdb;
 
-ALTER TABLE supp_repair ADD refuseDec VARCHAR(100) DEFAULT NULL COMMENT '拒绝原因';
+ALTER TABLE supp_repair ADD refuseDesc VARCHAR(100) DEFAULT NULL COMMENT '拒绝原因';
 ALTER TABLE supp_repair ADD refuseTime DATETIME DEFAULT NULL COMMENT '拒绝时间';
 ALTER TABLE supp_repair ADD refuseBy INT(15) DEFAULT NULL COMMENT '拒绝人';
 ALTER TABLE supp_repair ADD refuseByType INT(2) DEFAULT NULL COMMENT '拒绝人类型（参考字典表组名:userType）';
