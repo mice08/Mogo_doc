f971aaa2b6d3effa438d2083ce66400642194b65
xuwenrui <xuwenrui@mogoroom.com>
2016-12-22 14:34:01 +0800

    opt@develop#:修改delete优化sql

--- VersionRecords/Version mdSql/SQL_Scripts/DDL/MD01.delete语句优化.sql
+++ VersionRecords/Version mdSql/SQL_Scripts/DDL/MD01.delete语句优化.sql
@@ -1,11 +1,5 @@
 use mogoroomdb;
 
-/*orga_employeejobrelation表添加valid字段 */
-ALTER TABLE `orga_employeejobrelation` ADD COLUMN valid tinyint(1) DEFAULT '1' COMMENT '是否有效(0:否  1:是 )';
-
-/*orga_tag表添加valid字段 */
-ALTER TABLE `orga_tag` ADD COLUMN valid tinyint(1) DEFAULT '1' COMMENT '是否有效(0:否  1:是 )';
-
 
 
 
