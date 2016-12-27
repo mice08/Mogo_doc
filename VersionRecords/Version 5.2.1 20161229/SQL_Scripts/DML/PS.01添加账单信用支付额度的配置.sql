4fa588f81cb7c4b9c747df5ab280a69a0338740a
wangyan <wangyan@mogoroom.com>
2016-12-27 11:51:04 +0800

    opt@v5.2.1#:上线清单&脚本

---
+++ VersionRecords/Version 5.2.1 20161229/SQL_Scripts/DML/PS.01添加账单信用支付额度的配置.sql
@@ -0,0 +1,4 @@
+﻿/*添加账单信用支付额度的配置*/
+USE mogoroomdb;
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby)
+VALUES('riskCreditPayAmount',30000,'riskCreditPay','账单每月信用支付最高额度限制','账单每月信用支付最高额度限制',NOW(),5,0);
