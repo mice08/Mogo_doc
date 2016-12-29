0e7f9dae5164ad351e3ffd04e9760bd86602200e
wanghui <wanghui@www.mogoroom.com>
2016-12-29 19:42:08 +0800

    opt@5.2.0#:sql to utf-8

--- VersionRecords/Version 5.2.0 20161229/SQL_Scripts/DDL/JB01.合同表templetCode数据类型变更.sql
+++ VersionRecords/Version 5.2.0 20161229/SQL_Scripts/DDL/JB01.合同表templetCode数据类型变更.sql
@@ -1,2 +1,2 @@
 USE mogoroomdb;
-ALTER TABLE `cntr_salecontract` MODIFY COLUMN `templetCode`  varchar(20) CHARACTER SET utf8 NULL DEFAULT '' AFTER `templetId`;
+ALTER TABLE `cntr_salecontract` MODIFY COLUMN `templetCode`  varchar(20) CHARACTER SET utf8 NULL DEFAULT '' COMMENT '模板code(绑定)'  AFTER `templetId`;
