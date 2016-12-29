800e1240908a67f51253cfa2dcab5e6236c5e77c
fyp <charles@mogoroom.com>
2016-12-29 14:36:09 +0800

    opt@5.2.0#:修改sqlCOMMENT

--- VersionRecords/Version 5.2.0 20161229/SQL_Scripts/DDL/AR01_支付记录新增信用字段-冯银鹏.sql
+++ VersionRecords/Version 5.2.0 20161229/SQL_Scripts/DDL/AR01_支付记录新增信用字段-冯银鹏.sql
@@ -1,4 +1,4 @@
 USE mogoroomdb;
 ALTER TABLE `bill_paylog_weixin`  ADD COLUMN `isCredit` TINYINT(4) COMMENT '是否使用信用卡' ;
 ALTER TABLE `bill_paylog_zhifubao` ADD COLUMN `isCredit` TINYINT(4) COMMENT '是否使用信用卡' ;
-ALTER TABLE `bill_paylog_zhifubao` ADD COLUMN `out_channel_type` VARCHAR(50) COMMENT '交易类型' ;
+ALTER TABLE `bill_paylog_zhifubao` ADD COLUMN `out_channel_type` VARCHAR(50) COMMENT '交易类型(参考字典表：outChannelType)' ;
