a46b776def4597d556c9a0339a626480946e3aa3
wanghui <wanghui@www.mogoroom.com>
2016-12-29 18:58:42 +0800

    opt@5.2.0#:ddl变更

--- VersionRecords/Version 5.2.0 20161229/SQL_Scripts/DDL/JB01.合同表templetCode数据类型变更.sql
+++ VersionRecords/Version 5.2.0 20161229/SQL_Scripts/DDL/JB01.合同表templetCode数据类型变更.sql
@@ -1,2 +1,2 @@
 USE mogoroomdb;
-ALTER TABLE `cntr_salecontract` MODIFY COLUMN `templetCode`  varchar(20) NULL COMMENT 'Ä£°åcode(°ó¶¨)' AFTER `confirmDate`;
+ALTER TABLE `cntr_salecontract` MODIFY COLUMN `templetCode`  varchar(20) CHARACTER SET utf8 NULL DEFAULT '' AFTER `templetId`;
