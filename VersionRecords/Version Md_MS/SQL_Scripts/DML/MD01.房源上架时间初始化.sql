e8b8cb29247452591a78c4d5782e83b2eed2cc7f
ad <ad@mogoroom.com>
2016-12-29 11:41:04 +0800

    opt@5.2.0#:房源上架时间初始化

--- VersionRecords/Version Md_MS/SQL_Scripts/DML/MD01.房源上架时间初始化.sql
+++ VersionRecords/Version Md_MS/SQL_Scripts/DML/MD01.房源上架时间初始化.sql
@@ -1,6 +1,11 @@
 use mogoroomdb;
 
 /*房源上架时间初始化*/
-UPDATE flat_room SET putawayTime = '2016-12-20 00:00:00' 
-WHERE STATUS = 1 AND verifyStatus != 3 AND rentStatus = 1 AND onlineStatus = 1
-AND (mogoOfflineEndTime IS NULL OR mogoOfflineEndTime < NOW()); 
+UPDATE flat_room SET putawayTime = NOW() 
+WHERE id IN (
+	SELECT t.id FROM
+	(SELECT fr.id FROM flat_room fr JOIN flat_flats ff ON fr.flatsId = ff.id 
+	WHERE fr.STATUS = 1 AND fr.verifyStatus != 3 AND fr.rentStatus = 1 AND fr.onlineStatus = 1
+	AND (fr.mogoOfflineEndTime IS NULL OR fr.mogoOfflineEndTime < NOW())
+	AND ff.isVerify != 2) t
+);
