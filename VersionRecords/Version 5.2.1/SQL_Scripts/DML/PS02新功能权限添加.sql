f7aef0f9396d74d6f9d1b797d9b9ae56241ce067
qwb <qwb@mogoroom.com>
2017-01-01 10:22:37 +0800

    添加权限脚本

---
+++ VersionRecords/Version 5.2.1/SQL_Scripts/DML/PS02新功能权限添加.sql
@@ -0,0 +1,19 @@
+﻿/*新功能权限添加*/
+
+use mogoroomdb;
+
+/* 3级菜单  拓展ajax */
+SELECT @id:=id FROM perm_functioninfo WHERE fname = '房东管理' ORDER BY id DESC LIMIT 1;
+SELECT @seq:=(MAX(seq)+1) FROM perm_functioninfo WHERE functionpId=@id;
+SELECT @fcode:= CONCAT('3',LPAD(@id,4,0),@seq ) FROM perm_functioninfo WHERE fname = '房东管理' LIMIT 1;
+
+INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES ( @fcode, '风控房东管理房东权限管理', 'landlord/riskFunctionInfo',@seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+
+/* 3级菜单  拓展ajax */
+SELECT @id:=id FROM perm_functioninfo WHERE fname = '房东管理' ORDER BY id DESC LIMIT 1;
+SELECT @seq:=(MAX(seq)+1) FROM perm_functioninfo WHERE functionpId=@id;
+SELECT @fcode:= CONCAT('3',LPAD(@id,4,0),@seq ) FROM perm_functioninfo WHERE fname = '房东管理' LIMIT 1;
+
+INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES ( @fcode, '风控房东管理房东权限修改记录', 'landlord/findRiskFunctionRecord',@seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
