d0af8949139dda04cd7dcfe480b12626fc9c787a
cb <cb@mogoroom.com>
2017-01-04 10:21:34 +0800

    opt@develop#:销售合同表添加押金收取类型字段

--- VersionRecords/Version TP/SQL_Scripts/DDL/JB01.添加字段.销售合同表.sql
+++ VersionRecords/Version TP/SQL_Scripts/DDL/JB01.添加字段.销售合同表.sql
@@ -1,4 +1,4 @@
-/*销售合同表.添加是否免押金字段*/
+/*销售合同表.添加押金收取类型字段*/
 use mogoroomdb;
 
-ALTER TABLE cntr_saleContract ADD gratis TINYINT(4) NOT NULL DEFAULT '0' COMMENT '是否免押金0：不是；1：是';
+ALTER TABLE cntr_saleContract ADD depositIncomeType TINYINT(4) NOT NULL DEFAULT '0' COMMENT '押金收取类型(0:收押金 1:芝麻信用免押金)';
