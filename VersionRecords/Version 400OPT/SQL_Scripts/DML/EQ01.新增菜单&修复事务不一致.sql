c9e70b210f5708231d6dfc2a99bf8fad62546335
zy150817 <zy150817@mogoroom.com>
2017-01-05 13:36:19 +0800

    add@develop#:增加菜单&修复事务不一致数据

--- VersionRecords/Version 400OPT/SQL_Scripts/DML/EQ01.新增菜单&修复事务不一致.sql
+++ VersionRecords/Version 400OPT/SQL_Scripts/DML/EQ01.新增菜单&修复事务不一致.sql
@@ -5,7 +5,7 @@ BEGIN;
 
 /* 增加菜单-补充通话记录 */
 INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
-VALUES ('334348', '通话记录补充', 'tele/supplyCallLog', 1, '2','3434', '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+VALUES ('334348', '通话记录补充', 'tele/supplyCallLog', 9, '2','3434', '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
 
 /**修复事务不一致数据**/
 UPDATE `tele_sungoin_seat` SET cntctName='加盟咨询',phone='15821387697' WHERE id=4140;
