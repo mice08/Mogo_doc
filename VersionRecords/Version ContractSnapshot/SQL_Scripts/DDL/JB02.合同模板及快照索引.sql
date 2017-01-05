0d170c03101f211e9bd2c0f2eca411b17e502d7b
wanghui <wanghui@www.mogoroom.com>
2017-01-05 19:55:48 +0800

    opt@cntr#:合同快照扩充8192

--- VersionRecords/Version ContractSnapshot/SQL_Scripts/DDL/JB02.合同模板及快照索引.sql
+++ VersionRecords/Version ContractSnapshot/SQL_Scripts/DDL/JB02.合同模板及快照索引.sql
@@ -5,3 +5,6 @@ ALTER TABLE `cntr_contract_templet` ADD INDEX IDX_TEMPLETCODE(`templetCode`) USI
 
 /* 合同快照表添加索引 ==  */
 ALTER TABLE `cntr_salecontract_snapshot` ADD INDEX IDX_CONTRACTID(`contractId`) USING HASH;
+
+/*快照长度 扩充8192 */
+ALTER TABLE `cntr_salecontract_snapshot` MODIFY COLUMN `params`  varchar(8192) COMMENT '签订合同时的合同参数json' AFTER `templetId`;
