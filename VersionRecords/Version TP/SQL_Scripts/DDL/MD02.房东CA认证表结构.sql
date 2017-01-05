d29206a82fb1b0fe501d5043839eced4df2b92e2
zlj <zlj@mogoroom.com>
2017-01-05 17:56:53 +0800

    修改字段

--- VersionRecords/Version TP/SQL_Scripts/DDL/MD02.房东CA认证表结构.sql
+++ VersionRecords/Version TP/SQL_Scripts/DDL/MD02.房东CA认证表结构.sql
@@ -1,6 +1,7 @@
 ﻿use mogoroomdb;
 
 /*房东CA认证表*/
+
 CREATE TABLE `user_landlord_caIdentification`(  
 	`id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '房东ca认证表id',
 	`landlordId` INT(11) NOT NULL COMMENT '房东id',
@@ -8,12 +9,12 @@ CREATE TABLE `user_landlord_caIdentification`(
 	`identityId` varchar(20) DEFAULT NULL COMMENT '身份证号',
 	`name` varchar(20) DEFAULT NULL COMMENT '房东姓名',
 	`applyCityId` int(11) DEFAULT NULL COMMENT '房东申请城市id',
-	`landlordType` tinyint(1) NOT NULL COMMENT '房东类型 0:企业版房东；1个人版房东',
+	`acctType` tinyint(1) NOT NULL COMMENT '房东类型 (0:企业版房东,1:个人版房东)',
 	`address` varchar(60) DEFAULT NULL COMMENT '居住地址',
 	`account` varchar(100) DEFAULT NULL COMMENT '账号',
 	`password` varchar(100) DEFAULT NULL COMMENT '密码',
 	`uid` varchar(100) NOT NULL COMMENT '第三方ca认证返回ID',
-	`status` TINYINT(1) DEFAULT '1' COMMENT '资质状态（1: ca未认证 2: ca已认证）',
+	`status` TINYINT(1) DEFAULT '1' COMMENT '资质状态（1: ca未认证,2: ca已认证）',
 	`remark` VARCHAR(512) DEFAULT NULL COMMENT '备注',
 	`createTime` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 	`createBy` INT(11) COMMENT '创建人',
