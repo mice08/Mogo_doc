c59b81fcc9fef25699ee01b3009116dead1f2d43
yb <yb@mogoroom.com>
2017-01-07 10:07:39 +0800

    1

--- VersionRecords/Version online/SQL_Scripts/DDL/JG05.loanMpayapplyApprove.sql
+++ VersionRecords/Version online/SQL_Scripts/DDL/JG05.loanMpayapplyApprove.sql
@@ -2,4 +2,4 @@
 USE mogoroomdb;
 
 ALTER TABLE loan_mpayapply_approve
-ADD COLUMN picGroupId INT(11) NULL  COMMENT '合同图片组id' AFTER `remark2`;
+ADD COLUMN picGroupId INT(11) NULL  COMMENT '信审上传图片组id，如合同等' AFTER `remark2`;
