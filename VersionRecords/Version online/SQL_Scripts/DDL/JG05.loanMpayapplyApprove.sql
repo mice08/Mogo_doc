9b76825a16550ad6a45cb3734b90d3bd0a312a13
hmj <hmj@mogoroom.com>
2017-01-07 10:17:17 +0800

    新增签约合同图片组id

--- VersionRecords/Version online/SQL_Scripts/DDL/JG05.loanMpayapplyApprove.sql
+++ VersionRecords/Version online/SQL_Scripts/DDL/JG05.loanMpayapplyApprove.sql
@@ -3,3 +3,6 @@ USE mogoroomdb;
 
 ALTER TABLE loan_mpayapply_approve
 ADD COLUMN picGroupId INT(11) NULL  COMMENT '信审上传图片组id，如合同等' AFTER `remark2`;
+
+ALTER TABLE loan_mpayapply_approve_log
+ADD COLUMN picGroupId INT(11) NULL  COMMENT '信审上传图片组id，如合同等' AFTER `remark2`;
