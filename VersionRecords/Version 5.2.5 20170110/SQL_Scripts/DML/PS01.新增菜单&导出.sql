2f3a4c5cefd9046f77fa496d908af9cc413516ec
sunsuwen <sunsuwen@mogoroom.com>
2017-01-09 15:18:05 +0800

    opt@5.2.5#更新菜单列表权限

--- VersionRecords/Version 5.2.5 20170110/SQL_Scripts/DML/PS01.新增菜单&导出.sql
+++ VersionRecords/Version 5.2.5 20170110/SQL_Scripts/DML/PS01.新增菜单&导出.sql
@@ -12,3 +12,23 @@ INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLeve
 VALUES ( @fcode, '房间列表导出', 'room/findRoomsByFeatureReport', @seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
 
 COMMIT;
+
+/* 增加菜单-房东贷款买回导出 */
+SELECT @id:=id FROM perm_functioninfo WHERE fname = '房东贷款买回' ORDER BY id DESC LIMIT 1;
+SELECT @seq:=(MAX(seq)+1) FROM perm_functioninfo WHERE functionpId=@id;
+SELECT @fcode:= CONCAT('3',LPAD(@id,4,0),@seq) FROM perm_functioninfo WHERE fname = '房东贷款买回' ORDER BY id DESC LIMIT 1;
+
+INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES ( @fcode, '房东贷款买回导出', 'landlordCredit/listLoanLandlordBuyBackExport', @seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+
+COMMIT;
+
+/* 增加菜单-房东贷款还款导出 */
+SELECT @id:=id FROM perm_functioninfo WHERE fname = '房东贷款还款' ORDER BY id DESC LIMIT 1;
+SELECT @seq:=(MAX(seq)+1) FROM perm_functioninfo WHERE functionpId=@id;
+SELECT @fcode:= CONCAT('3',LPAD(@id,4,0),@seq) FROM perm_functioninfo WHERE fname = '房东贷款还款' ORDER BY id DESC LIMIT 1;
+
+INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES ( @fcode, '房东贷款还款导出', 'landlordCredit/listLandlordRepayPlanExport', @seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+
+COMMIT;
