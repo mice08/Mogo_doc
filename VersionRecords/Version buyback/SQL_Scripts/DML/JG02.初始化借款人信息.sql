c8ea6bdc23e0c572e3d4e9c8f7c3930bd00851fe
lvchongxin <lcx@mogoroom.com>
2017-01-09 19:45:07 +0800

    opt #蘑菇宝快照

---
+++ VersionRecords/Version buyback/SQL_Scripts/DML/JG02.初始化借款人信息.sql
@@ -0,0 +1,7 @@
+﻿/*跟换BS保证金管理菜单表*/
+
+use mogoroomdb;
+
+update perm_functioninfo set  furl='loanManage/queryDepositManage' where  furl='loanManage/depositManageList' ;
+
+update perm_functioninfo set  furl='loanManageExport/queryDepositManageExport' where  furl='loanManageExport/depositManageListExport' ;
