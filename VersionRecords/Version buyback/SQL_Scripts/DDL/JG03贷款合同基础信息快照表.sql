0a4556cf7bc823281b7f918e1d2da680827a2b64
lvchongxin <lcx@mogoroom.com>
2017-01-09 17:36:22 +0800

    opt #蘑菇贷款合同信息快照表

--- VersionRecords/Version buyback/SQL_Scripts/DDL/JG03贷款合同基础信息快照表.sql
+++ VersionRecords/Version buyback/SQL_Scripts/DDL/JG03贷款合同基础信息快照表.sql
@@ -3,6 +3,8 @@
 use mogoroomdb;
 
 
+drop table if exists loan_contract_snapshot;
+
 CREATE TABLE `loan_contract_snapshot` (
 `id`  int(11) NOT NULL AUTO_INCREMENT COMMENT '主键' ,
 `loanContractId`  int(11) NOT NULL COMMENT '贷款合同Id' ,
@@ -13,5 +15,6 @@ CREATE TABLE `loan_contract_snapshot` (
 `roomDesc`  varchar(255) NULL COMMENT '房源描述' ,
 `debitbank`  varchar(8) NULL COMMENT '借记卡开户行' ,
 `debitcard`  varchar(20) NULL COMMENT '借记卡卡号' ,
+`bankMobile`  varchar(16) NULL COMMENT '银行预留手机号码' ,
 PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '贷款合同基础信息快照表';
