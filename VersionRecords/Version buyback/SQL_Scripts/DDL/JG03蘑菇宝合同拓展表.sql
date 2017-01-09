a02ea0483b96bf0edab03d591e4e4b80a7ed23f4
lvchongxin <lcx@mogoroom.com>
2017-01-09 15:56:25 +0800

    opt # 新建蘑菇宝拓展表

--- VersionRecords/Version buyback/SQL_Scripts/DDL/JG03蘑菇宝合同拓展表.sql
+++ VersionRecords/Version buyback/SQL_Scripts/DDL/JG03蘑菇宝合同拓展表.sql
@@ -1,11 +1,11 @@
-﻿/*蘑菇宝合同扩展表*/
+﻿/*贷款合同扩展表*/
 
 use mogoroomdb;
 
 
 CREATE TABLE `loan_contract_extend` (
 `id`  int(11) NOT NULL AUTO_INCREMENT COMMENT '主键' ,
-`contractId`  int(11) NOT NULL COMMENT '合同Id' ,
+`loanContractId`  int(11) NOT NULL COMMENT '贷款合同Id' ,
 `renterId`  int(11) NOT NULL COMMENT '租客id' ,
 `renterPhone`  varchar(16) NULL COMMENT '租客手机号' ,
 `renterName`  varchar(64) NULL COMMENT '租客姓名' ,
@@ -14,4 +14,4 @@ CREATE TABLE `loan_contract_extend` (
 `debitbank`  varchar(8) NULL COMMENT '借记卡开户行' ,
 `debitcard`  varchar(20) NULL COMMENT '借记卡卡号' ,
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '蘑菇宝合同扩展表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '贷款合同扩展表';
