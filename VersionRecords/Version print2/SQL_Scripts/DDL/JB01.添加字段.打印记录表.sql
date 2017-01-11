70d36db6e78a5c04792d94673339dc98a7403e60
cb <cb@mogoroom.com>
2017-01-11 14:37:25 +0800

    opt@develop#:打印记录表添加字段

--- VersionRecords/Version print2/SQL_Scripts/DDL/JB01.添加字段.打印记录表.sql
+++ VersionRecords/Version print2/SQL_Scripts/DDL/JB01.添加字段.打印记录表.sql
@@ -1,5 +1,6 @@
-/*打印记录表.添加房东id、打印编号字段*/
+/*打印记录表.添加字段*/
 use mogoroomdb;
 
-ALTER TABLE comm_print_record ADD landlordId INT(11) COMMENT '房东id';
+ALTER TABLE comm_print_record ADD userId INT(11) COMMENT '用户id';
+ALTER TABLE comm_print_record ADD userType TINYINT(4) COMMENT '用户类型(参考字典表组名:userType)';
 ALTER TABLE comm_print_record ADD printCode INT(11) COMMENT '打印编号';
