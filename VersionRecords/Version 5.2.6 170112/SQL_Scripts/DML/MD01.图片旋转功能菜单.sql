3d5891909ab652e44007d2f4800f4247e1be5b60
Jonathan <zlz@mogoroom.com>
2017-01-12 16:37:43 +0800

    update

--- VersionRecords/Version 5.2.6 170112/SQL_Scripts/DML/MD01.图片旋转功能菜单.sql
+++
@@ -1,4 +0,0 @@
-use mogoroomdb;
-
-/*BS图片旋转功能菜单*/
-insert into `perm_functioninfo` (`fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`) values('3219706','图片旋转','common/rotateImage','6','3','2197','0',NULL,NULL,NULL,'1','1',NULL,NULL,NULL,NULL,'1','2');
