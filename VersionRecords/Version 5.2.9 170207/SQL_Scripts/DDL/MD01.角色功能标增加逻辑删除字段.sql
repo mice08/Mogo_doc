86137b6f353fec4911154778b347e173367ed599
lyl1124 <lyl1124@mogoroom.com>
2017-02-06 19:23:14 +0800

    new@5.2.9:更改脚本

--- VersionRecords/Version 5.2.9 170207/SQL_Scripts/DDL/MD01.角色功能标增加逻辑删除字段.sql
+++ VersionRecords/Version 5.2.9 170207/SQL_Scripts/DDL/MD01.角色功能标增加逻辑删除字段.sql
@@ -2,3 +2,7 @@
 
 /*增加状态字段 */
 ALTER TABLE `perm_role_function` ADD COLUMN `valid` tinyint(1) NOT NULL DEFAULT '1' COMMENT '逻辑删除状态 1:有效 0:无效' AFTER `function_id`;
+
+ALTER TABLE `perm_role_function` DROP PRIMARY KEY,
+  ADD INDEX (`role_id`),
+  ADD INDEX (`function_id`);
