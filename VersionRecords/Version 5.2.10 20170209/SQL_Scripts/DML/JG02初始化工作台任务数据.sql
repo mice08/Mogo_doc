bfc9d7d164cb0a6245476aec1501b9bbd7478501
lvchongxin <lcx@mogoroom.com>
2017-02-08 19:33:17 +0800

    opt # 新增初始化数据

---
+++ VersionRecords/Version 5.2.10 20170209/SQL_Scripts/DML/JG02初始化工作台任务数据.sql
@@ -0,0 +1,69 @@
+﻿/* 初始化工作台任务数据  */
+
+use mogoroomdb;
+
+
+INSERT INTO `loan_workbench_task` (
+	`employeeId`,
+	`eventId`,
+	`taskType`,
+	`valid`
+) SELECT
+	e.financeServicer,
+	e.id,
+	1,
+	1
+FROM
+	loan_workbench_event e
+WHERE
+	e.eventType = 'mogobaoVerify'
+AND e.financeServicer IS NOT NULL;
+
+INSERT INTO `loan_workbench_task` (
+	`employeeId`,
+	`eventId`,
+	`taskType`,
+	`valid`
+) SELECT
+	e.financeServicer,
+	e.id,
+	1,
+	1
+FROM
+	loan_workbench_event e
+WHERE
+	e.eventType = 'mogobaoApplyOne'
+AND e.financeServicer IS NOT NULL;
+
+INSERT INTO `loan_workbench_task` (
+	`employeeId`,
+	`eventId`,
+	`taskType`,
+	`valid`
+) SELECT
+	e.financeServicer,
+	e.id,
+	2,
+	1
+FROM
+	loan_workbench_event e
+WHERE
+	e.eventType = 'loanBuybackRenter'
+AND e.financeServicer IS NOT NULL;
+
+INSERT INTO `loan_workbench_task` (
+	`employeeId`,
+	`eventId`,
+	`taskType`,
+	`valid`
+) SELECT
+	e.financeServicer,
+	e.id,
+	2,
+	1
+FROM
+	loan_workbench_event e
+WHERE
+	e.eventType = 'loanBuybackLandlord'
+AND e.financeServicer IS NOT NULL;
+
