f84b659f2ef68cec02406ad42e2de76d6568dc71
qwb <qwb@mogoroom.com>
2017-02-09 11:13:09 +0800

    PS01创建防套现规则记录表

---
+++ VersionRecords/Version Bizd/SQL_Scripts/DDL/PS01创建防套现规则记录表.sql
@@ -0,0 +1,15 @@
+﻿/*创建防套现规则记录表*/
+
+use mogoroomdb;
+
+CREATE TABLE `risk_cashSuspicion`(  
+  `id` INT(11) AUTO_INCREMENT COMMENT 'id',
+  `createTime` DATETIME COMMENT '创建时间',
+  `creditAmount` DECIMAL(6,2) COMMENT '信用金额',
+  `riskCode` VARCHAR(25) COMMENT '规则编号',
+  `landlordId` INT(11) COMMENT '房东id',
+  `renterId` INT(11) COMMENT '租客id',
+  `content` VARCHAR(25) COMMENT '内容',
+  `contentType` TINYINT(1) COMMENT '内容数据类型(1:租客id 2:支付账号)',
+   KEY(`id`)
+) ENGINE=INNODB CHARSET=utf8mb4 COMMENT='防套现列表';
