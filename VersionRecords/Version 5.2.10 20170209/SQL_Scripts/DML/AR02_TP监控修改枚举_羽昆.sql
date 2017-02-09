2015687abe587ae236f33bae6a61ea6003b21e68
fyp <charles@mogoroom.com>
2017-02-09 19:24:21 +0800

    opt@5.2.10#:脚本修改

--- VersionRecords/Version 5.2.10 20170209/SQL_Scripts/DML/AR02_TP监控修改枚举_羽昆.sql
+++ VersionRecords/Version 5.2.10 20170209/SQL_Scripts/DML/AR02_TP监控修改枚举_羽昆.sql
@@ -1,5 +1,79 @@
 use acct;
-UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2;
-UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1;
-UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0;
-UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 and id<100000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id<100000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id<100000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id<100000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=100000 and id < 200000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=100000 AND id < 200000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=100000 AND id < 200000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=100000 AND id < 200000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=200000 and id < 300000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=200000 AND id < 300000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=200000 AND id < 300000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=200000 AND id < 300000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=300000 and id < 400000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=300000 AND id < 400000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=300000 AND id < 400000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=300000 AND id < 400000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=400000 and id < 500000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=400000 AND id < 500000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=400000 AND id < 500000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=400000 AND id < 500000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=500000 and id < 600000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=500000 AND id < 600000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=500000 AND id < 600000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=500000 AND id < 600000;
+
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=600000 and id < 700000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=600000 AND id < 700000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=600000 AND id < 700000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=600000 AND id < 700000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=700000 and id < 800000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=700000 AND id < 800000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=700000 AND id < 800000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=700000 AND id < 800000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=800000 and id < 900000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=800000 AND id < 900000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=800000 AND id < 900000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=800000 AND id < 900000;
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=900000 and id < 1000000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=900000 AND id < 1000000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=900000 AND id < 1000000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=900000 AND id < 1000000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=1000000 and id < 1100000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=1000000 AND id < 1100000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=1000000 AND id < 1100000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=1000000 AND id < 1100000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=1100000 and id < 1200000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=1100000 AND id < 1200000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=1100000 AND id < 1200000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=1100000 AND id < 1200000;
+
+
+UPDATE acct.`acct_tradelog` SET channel = 5 WHERE channel = 2 AND id>=1200000 and id < 1300000;
+UPDATE acct.`acct_tradelog` SET channel = 4 WHERE channel = 1 AND id>=1200000 AND id < 1300000;
+UPDATE acct.`acct_tradelog` SET channel = 1 WHERE channel = 0 AND id>=1200000 AND id < 1300000;
+UPDATE acct.`acct_tradelog` SET channel = 2 WHERE channel = 3 AND id>=1200000 AND id < 1300000;
