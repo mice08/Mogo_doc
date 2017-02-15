ea5edfd8be40411ab211a3857019eda367566ca6
yb <yb@mogoroom.com>
2017-02-15 10:34:45 +0800

    1

---
+++ VersionRecords/Version 5.2.11 20170214/SQL_Scripts/DDL/JG02.聚有财资质备注.sql
@@ -0,0 +1,11 @@
+﻿/*聚有财资质新增备注*/
+
+use mogoroomdb;
+
+ALTER TABLE `loan_landlord_credit`
+ADD COLUMN `alterRemark`  varchar(120) NULL COMMENT '暂停/恢复备注' AFTER `alterByType`,
+ADD COLUMN `endRemark`  varchar(120) NULL COMMENT '终止备注' AFTER `endByType`;
+
+ALTER TABLE `loan_landlord_credit_his`
+ADD COLUMN `alterRemark`  varchar(120) NULL COMMENT '暂停/恢复备注' AFTER `alterByType`,
+ADD COLUMN `endRemark`  varchar(120) NULL COMMENT '终止备注' AFTER `endByType`;
