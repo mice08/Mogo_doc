140bbdb0606096272481970c8b8cfc116cc9bc4e
szl <szl@mogoroom.com>
2017-02-17 17:46:17 +0800

    增加字段

--- VersionRecords/Version JG_Statistics/SQL_Scripts/DDL/JG01.loanStatistics.sql
+++ VersionRecords/Version JG_Statistics/SQL_Scripts/DDL/JG01.loanStatistics.sql
@@ -98,11 +98,11 @@ CREATE TABLE repo_loan_buyback (
   overdueUnpayBuyBackCount  INT(11) NOT NULL DEFAULT '0' COMMENT '截止当日/月逾期未买回笔数',
   overdueUnpayBuyBackAmount  DECIMAL(14,2) NOT NULL DEFAULT '0.00' COMMENT '截止当日/月逾期未买回总额',
   badBuyBackAmount  DECIMAL(14,2) NOT NULL DEFAULT '0.00' COMMENT '坏账本金',
+  mogoPrincipalBuyBackCount  INT(11) NOT NULL DEFAULT '0' COMMENT '向资方买回笔数',
   mogoPrincipalBuyBackAmount  DECIMAL(14,2) NOT NULL DEFAULT '0.00' COMMENT '向资方买回本金',
   totalPrincipalBuyBackAmount  DECIMAL(14,2) NOT NULL DEFAULT '0.00' COMMENT '累计向资方买回本金',
   totalShouldBuyBackAmount  DECIMAL(14,2) NOT NULL DEFAULT '0.00' COMMENT '累计应还款金额',
   totalUnpayBuyBackAmount  DECIMAL(14,2) NOT NULL DEFAULT '0.00' COMMENT '累计逾期本金',
-  totalBuyBackCount  INT(11) NOT NULL DEFAULT '0' COMMENT '累计向资方买回笔数',
   valid INT(1) NOT NULL DEFAULT '1' COMMENT '是否有效(0:有效 1:无效)',
   PRIMARY KEY (id),
   KEY statDate(statDate)
