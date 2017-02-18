9805a14dcb996f944eaf4ffab50aa1c7d1ae1551
hzm <hzm@mogoroom.com>
2017-02-18 14:35:33 +0800

    增加消息跳转编码长度

---
+++ VersionRecords/Version 5.3.1_beta/SQL_Scripts/DDL/AR03_消息改造.sql
@@ -0,0 +1,9 @@
+USE mogoroomdb;
+
+
+/**修改消息Value长度**/
+ALTER TABLE `mesg_push_tosend`  MODIFY COLUMN jumpCode VARCHAR(50);
+ALTER TABLE `mesg_push_record`  MODIFY COLUMN jumpCode VARCHAR(50);
+ALTER TABLE `mesg_push_tocheck` MODIFY COLUMN jumpCode VARCHAR(50);
+ALTER TABLE `mesg_news_record`  MODIFY COLUMN jumpCode VARCHAR(50);
+
