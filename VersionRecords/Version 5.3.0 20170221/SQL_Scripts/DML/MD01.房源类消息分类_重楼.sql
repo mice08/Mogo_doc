15516dbf2ef7d6e3d604d2825c2028807d835d12
zhangfei <zhangfei@mogoroom.com>
2017-02-20 17:20:41 +0800

    修改脚本

--- VersionRecords/Version 5.3.0 20170221/SQL_Scripts/DML/MD01.房源类消息分类_重楼.sql
+++ VersionRecords/Version 5.3.0 20170221/SQL_Scripts/DML/MD01.房源类消息分类_重楼.sql
@@ -1,4 +1,4 @@
 ﻿/* 调整房源类消息模板业务类型 */
 use mogoroomdb;
 
-UPDATE `mesg_templet` SET businessType=0 WHERE templetCode IN ('fangjianxiajiadSII','sms_picture_unVerify_landlord','fangjianmobanzhaopianshenheweitongguoMABp','sms_flatsType_unVerify_landlord','sms_flats_unVerify_landlord');
+UPDATE `mesg_templet` SET businessType=11 WHERE templetCode IN ('fangjianxiajiadSII','sms_picture_unVerify_landlord','fangjianmobanzhaopianshenheweitongguoMABp','sms_flatsType_unVerify_landlord','sms_flats_unVerify_landlord');
