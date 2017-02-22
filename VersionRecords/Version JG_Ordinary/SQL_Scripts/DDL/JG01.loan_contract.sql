5a9362dfccd5f089f53e5061e2b1ded35be8073c
lvchongxin <lcx@mogoroom.com>
2017-02-22 13:54:02 +0800

    opt # 新增SQL

--- VersionRecords/Version JG_Ordinary/SQL_Scripts/DDL/JG01.loan_contract.sql
+++ VersionRecords/Version JG_Ordinary/SQL_Scripts/DDL/JG01.loan_contract.sql
@@ -2,7 +2,8 @@
 USE mogoroomdb;
 
 ALTER TABLE `loan_contract`
-ADD COLUMN `fundfeeRate`  int(11) NULL DEFAULT  COMMENT '资方放款手续费费率' AFTER `autoRepay`,
-ADD COLUMN `fundfeeAmount`  decimal(12,2) NULL DEFAULT  COMMENT '资方放款手续费金额' AFTER `fundfeeRate`,
+ADD COLUMN `fundfeeRate` int(11) NULL  COMMENT '资方放款手续费费率' AFTER `autoRepay`,
+ADD COLUMN `fundfeeAmount`  decimal(12,2) NULL   COMMENT '资方放款手续费金额' AFTER `fundfeeRate`,
 ADD COLUMN `fundActualAmount`  decimal(12,2) NULL COMMENT '资方实际放款金额' AFTER `fundfeeAmount`;
+
 
