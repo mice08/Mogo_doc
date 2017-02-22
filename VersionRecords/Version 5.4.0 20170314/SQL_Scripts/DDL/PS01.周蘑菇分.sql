00daa8288d9b34b39fda856d474e7a7a7eea9d35
wangyan <wangyan@mogoroom.com>
2017-02-22 14:43:14 +0800

    opt@5.4.0#:上线清单

--- VersionRecords/Version 5.4.0 20170314/SQL_Scripts/DDL/PS01.周蘑菇分.sql
+++ VersionRecords/Version 5.4.0 20170314/SQL_Scripts/DDL/PS01.周蘑菇分.sql
@@ -1,4 +1,5 @@
 /*  Database name `mogoroomdb`  增加房东周数据表*/
+use mogoroomdb;
 
 CREATE TABLE `repo_landlord_weekdata` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -8,6 +9,7 @@ CREATE TABLE `repo_landlord_weekdata` (
   `createBy` int(11) NOT NULL COMMENT '创建人',
   `createByType` int(2) NOT NULL COMMENT '创建人类型',
   `createTime` date NOT NULL COMMENT '创建时间',
+  `valid` TINYINT(4) NOT NULL DEFAULT 1 COMMENT '删除标志（0:删除,1:未删除）',
   PRIMARY KEY (`id`),
   KEY `index_landlordId` (`landlordId`),
   KEY `index_recordDate` (`recordDate`)
