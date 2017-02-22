2813ad18fd87e138fe9390ca4ea3588e92f99a99
zlj <zlj@mogoroom.com>
2017-02-22 17:14:33 +0800

    增加登录来源渠道

--- VersionRecords/Version 5.3.2 20170228/SQL_Scripts/DDL/MD01.增加登录来源渠道.sql
+++ VersionRecords/Version 5.3.2 20170228/SQL_Scripts/DDL/MD01.增加登录来源渠道.sql
@@ -3,7 +3,8 @@
 
 ALTER TABLE `user_loginlog`
 MODIFY COLUMN `loginType`  int(11) NOT NULL COMMENT '登录类型(参考字典表组名:userType)' AFTER `loginAccount`,
-ADD COLUMN `loginSource`  int(11) NULL COMMENT '登录来源渠道(参考字典表组名:loginSourceType)' AFTER `loginType`;
+ADD COLUMN `loginSource`  int(11) NULL COMMENT '登录来源渠道(参考字典表组名:loginSourceType)' AFTER `loginType`,
+ADD COLUMN `channel`  int(11) NULL COMMENT '渠道(参考字典表组名:channel)' AFTER `loginSource`;
 
 
 /*初始化数据用户类型*/
@@ -12,22 +13,22 @@ update user_loginlog set loginType = 0 where loginType = 2;
 
 
 /*初始化登录记录渠道类型类型*/
-update user_loginlog set loginSource = 102 where terminalType = '1' and loginType  in (0,7) and LENGTH(terminalId) = 36 and loginSource is null ;
-update user_loginlog set loginSource = 101 where terminalType = '1' and loginType  in (0,7) and LENGTH(terminalId) != 36 and loginSource is null;
-update user_loginlog set loginSource = 101 where terminalType = '1' and loginType  in (0,7) and terminalId is null and loginSource is null;
+update user_loginlog set loginSource = 102,channel = 4 where terminalType = '1' and loginType  in (0,7) and LENGTH(terminalId) = 36 and loginSource is null ;
+update user_loginlog set loginSource = 101,channel = 4 where terminalType = '1' and loginType  in (0,7) and LENGTH(terminalId) != 36 and loginSource is null;
+update user_loginlog set loginSource = 101,channel = 4 where terminalType = '1' and loginType  in (0,7) and terminalId is null and loginSource is null;
 
-update user_loginlog set loginSource = 102 where terminalType not in ('1','2','3','4') and loginType  in (0,7) and LENGTH(terminalId) = 36 and loginSource is null;
-update user_loginlog set loginSource = 101 where terminalType not in ('1','2','3','4') and loginType  in (0,7) and LENGTH(terminalId) != 36 and loginSource is null;
+update user_loginlog set loginSource = 102,channel = 4 where terminalType not in ('1','2','3','4') and loginType  in (0,7) and LENGTH(terminalId) = 36 and loginSource is null;
+update user_loginlog set loginSource = 101,channel = 4 where terminalType not in ('1','2','3','4') and loginType  in (0,7) and LENGTH(terminalId) != 36 and loginSource is null;
 
-update user_loginlog set loginSource = 105 where terminalType = '1' and loginType  = 4 and terminalId like 'platform=And%' and loginSource is null;
-update user_loginlog set loginSource = 106 where terminalType = '1' and loginType  = 4 and terminalId like 'platform=iOS%' and loginSource is null;
-update user_loginlog set loginSource = 105 where terminalType = '1' and loginType  = 4 and terminalId is null and loginSource is null;
+update user_loginlog set loginSource = 105,channel = 1 where terminalType = '1' and loginType  = 4 and terminalId like 'platform=And%' and loginSource is null;
+update user_loginlog set loginSource = 106,channel = 1 where terminalType = '1' and loginType  = 4 and terminalId like 'platform=iOS%' and loginSource is null;
+update user_loginlog set loginSource = 105,channel = 1 where terminalType = '1' and loginType  = 4 and terminalId is null and loginSource is null;
 
-update user_loginlog set loginSource = 201 where terminalType = '2' and loginType  in (0,7) and loginSource is null;
-update user_loginlog set loginSource = 202 where terminalType = '2' and loginType  = 4 and loginSource is null;
+update user_loginlog set loginSource = 201,channel = 5 where terminalType = '2' and loginType  in (0,7) and loginSource is null;
+update user_loginlog set loginSource = 202,channel = 9 where terminalType = '2' and loginType  = 4 and loginSource is null;
 
-update user_loginlog set loginSource = 301 where terminalType = '3' and loginSource is null;
-update user_loginlog set loginSource = 401 where terminalType = '4' and loginSource is null;
+update user_loginlog set loginSource = 301,channel = 10 where terminalType = '3' and loginSource is null;
+update user_loginlog set loginSource = 401,channel = 11 where terminalType = '4' and loginSource is null;
 
 /*增加字典表登录来源渠道*/
 INSERT INTO `comm_dictionary` (`code`, `groupName`, `sort`, `keyPro`, `value`, `status`, `fcode`, `enValue`) VALUES ('loginSourceType-1', 'loginSourceType', '1', '101', '老房东APP安卓', '1', NULL, 'loginSourceType-1');
