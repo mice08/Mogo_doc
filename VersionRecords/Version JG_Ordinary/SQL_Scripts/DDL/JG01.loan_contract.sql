caf4059e95c30e9d27f572a54972161181cc7848
lvchongxin <lcx@mogoroom.com>
2017-02-22 11:41:30 +0800

    opt # 新增SQL脚本

---
+++ VersionRecords/Version JG_Ordinary/SQL_Scripts/DDL/JG01.loan_contract.sql
@@ -0,0 +1,8 @@
+﻿/*新增资方放款信息字段*/
+USE mogoroomdb;
+
+ALTER TABLE `loan_contract`
+ADD COLUMN `fundfeeRate`  int(11) NULL DEFAULT  COMMENT '资方放款手续费费率' AFTER `autoRepay`,
+ADD COLUMN `fundfeeAmount`  decimal(12,2) NULL DEFAULT  COMMENT '资方放款手续费金额' AFTER `fundfeeRate`,
+ADD COLUMN `fundActualAmount`  decimal(12,2) NULL COMMENT '资方实际放款金额' AFTER `fundfeeAmount`;
+
