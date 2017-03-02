1c8c2e2a3722aaad19965e10f857ba0e70788e11
gbw <gbw@mogoroom.com>
2017-03-02 11:01:02 +0800

    ca 跟踪增加字段说明

--- VersionRecords/Version CA Contract/SQL_Scripts/DDL/JB02.CA请求跟综记录表.sql
+++ VersionRecords/Version CA Contract/SQL_Scripts/DDL/JB02.CA请求跟综记录表.sql
@@ -10,7 +10,7 @@ CREATE TABLE `ca_request_tracker` (
  	`url` varchar(100) NOT NULL COMMENT '第三方接口地址',
   	`requestContent` varchar(2048) DEFAULT NULL COMMENT '请求参数(未加密前的参数)',
   	`responseContent` varchar(2048) NOT NULL COMMENT '请求返回(已经解密或处理后的或者接口调用失败信息)',
-	`result` tinyint(2) NOT NULL COMMENT '请求是否成功',
+	`result` tinyint(2) NOT NULL COMMENT '请求是否成功(1:成功,0:失败)',
   	`createTime` datetime NOT NULL COMMENT '记录创建时间',
  	`requestTime` datetime NOT NULL COMMENT '请求时间',
  	`responseTime` datetime NOT NULL COMMENT '响应时间', 
