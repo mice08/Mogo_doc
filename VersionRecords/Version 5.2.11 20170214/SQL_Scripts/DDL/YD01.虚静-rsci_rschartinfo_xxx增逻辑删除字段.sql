1ad3ea88b85f097510e960b7f6826fd13f5d8479
zlj <zlj@mogoroom.com>
2017-03-02 12:07:55 +0800

    备注修改

--- VersionRecords/Version 5.2.11 20170214/SQL_Scripts/DDL/YD01.虚静-rsci_rschartinfo_xxx增逻辑删除字段.sql
+++ VersionRecords/Version 5.2.11 20170214/SQL_Scripts/DDL/YD01.虚静-rsci_rschartinfo_xxx增逻辑删除字段.sql
@@ -1,16 +1,14 @@
 use mogoroomdb;
 
 ALTER TABLE `rsci_rschartinfo_base`
-	ADD COLUMN `valid` TINYINT NOT NULL DEFAULT '1' COMMENT '逻辑删除字段。1为合法、0为逻辑删除';
+	ADD COLUMN `valid` TINYINT NOT NULL DEFAULT '1' COMMENT '逻辑删除字段(1:合法,0:逻辑删除)';
 
 ALTER TABLE `rsci_rschartinfo_sale`
-	ADD COLUMN `valid` TINYINT NOT NULL DEFAULT '1' COMMENT '逻辑删除字段。1为合法、0为逻辑删除';
+	ADD COLUMN `valid` TINYINT NOT NULL DEFAULT '1' COMMENT '逻辑删除字段(1:合法,0:逻辑删除)';
 
 ALTER TABLE `rsci_rschartinfo_stat`
-	ADD COLUMN `valid` TINYINT NOT NULL DEFAULT '1' COMMENT '逻辑删除字段。1为合法、0为逻辑删除';
+	ADD COLUMN `valid` TINYINT NOT NULL DEFAULT '1' COMMENT '逻辑删除字段(1:合法,0:逻辑删除)';
 
-ALTER TABLE `rsci_rschartinfo_base`
-	DROP INDEX `roomId`;
+ALTER TABLE `rsci_rschartinfo_base` DROP INDEX `roomId`;
 
-ALTER TABLE `rsci_rschartinfo_base`
-	ADD INDEX `roomId` (`roomId`);
+ALTER TABLE `rsci_rschartinfo_base` ADD INDEX `roomId` (`roomId`);
