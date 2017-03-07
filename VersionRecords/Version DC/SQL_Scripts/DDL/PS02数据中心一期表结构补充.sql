d0f67bc53034fb16b9c954c39e38773267c25c37
qwb <qwb@mogoroom.com>
2017-03-07 10:42:24 +0800

    init

---
+++ VersionRecords/Version DC/SQL_Scripts/DDL/PS02数据中心一期表结构补充.sql
@@ -0,0 +1,10 @@
+﻿
+/*后续修改，添加更新时间字段*/
+
+use dc;
+
+ALTER TABLE `dc`.`dc_order_info`   
+  ADD COLUMN `updateTime` DATETIME NULL  COMMENT '更新时间' AFTER `allOnlinePayRentAmount`;
+  
+ALTER TABLE `dc`.`dc_land_dtl_real`   
+  ADD COLUMN `updateTime` DATETIME NULL  COMMENT '更新时间' AFTER `mogoScore`;
