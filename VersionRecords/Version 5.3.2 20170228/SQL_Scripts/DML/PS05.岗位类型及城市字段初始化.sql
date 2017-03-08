7636f7e5f263600e7a97289844b3e9de7ca6dfaa
sunsuwen <sunsuwen@mogoroom.com>
2017-03-08 17:08:54 +0800

    opt@develop #岗位及城市初始化

---
+++ VersionRecords/Version 5.3.2 20170228/SQL_Scripts/DML/PS05.岗位类型及城市字段初始化.sql
@@ -0,0 +1,14 @@
+/*  Database name `mogoroomdb`  BS岗位类型及城市初始化 */
+use mogoroomdb;
+
+BEGIN;
+UPDATE orga_org SET areaCode=021 WHERE id=7
+UPDATE orga_org SET areaCode=010 WHERE id=8
+UPDATE orga_org SET areaCode=0755 WHERE id=9
+
+
+UPDATE orga_org SET orgaOrgType=07 WHERE id IN (17,19,25)
+
+UPDATE orga_org SET orgaOrgType=07,areaCode=021 WHERE fullPath LIKE CONCAT('%','>17','%')
+UPDATE orga_org SET orgaOrgType=07,areaCode=010 WHERE fullPath LIKE CONCAT('%','>19','%')
+UPDATE orga_org SET orgaOrgType=07,areaCode=0755 WHERE fullPath LIKE CONCAT('%','>25','%')
