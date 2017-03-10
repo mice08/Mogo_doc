f3ea3c98fb4feed45911f1c70a452846a92fc0b1
dongshan <sundongshan@mogoroom.com>
2017-03-10 19:03:10 +0800

    add sql

---
+++ VersionRecords/Version 5.3.3 20170314/SQL_Scripts/DML/PS01添加权限.sql
@@ -0,0 +1,11 @@
+﻿/*添加bs投诉列表--》获取房间列表查询权限*/
+
+
+use mogoroomdb;
+
+SELECT @id:=id FROM perm_functioninfo WHERE fname = '投诉列表' AND (furl IS NULL OR furl = 'complain/list') ORDER BY id DESC LIMIT 1;
+SELECT @seq:=(MAX(seq)+1) FROM perm_functioninfo WHERE functionpId=@id;
+SELECT @fcode:= CONCAT('2',LPAD(@id,4,0),@seq) FROM perm_functioninfo WHERE fname = '投诉列表' AND (furl IS NULL OR furl = 'complain/list') ORDER BY id DESC LIMIT 1;
+
+INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES ( @fcode, '获取房间列表', 'flats/findRoomsByFlatsId', @seq, '3', @id, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
