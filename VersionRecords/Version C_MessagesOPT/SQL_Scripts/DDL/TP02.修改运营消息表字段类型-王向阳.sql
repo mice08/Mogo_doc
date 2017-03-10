4fa6e25a11b661eb490d78071e5d3b607d054988
wangxiangyang <wangxiangyang@mogoroom.com>
2017-03-10 15:10:33 +0800

    opt@mogo.mesg#:修改消息记录表中pubMessageId字段类型

--- VersionRecords/Version C_MessagesOPT/SQL_Scripts/DDL/TP02.修改运营消息表字段类型-王向阳.sql
+++ VersionRecords/Version C_MessagesOPT/SQL_Scripts/DDL/TP02.修改运营消息表字段类型-王向阳.sql
@@ -2,4 +2,6 @@
 
 ALTER TABLE `mesg_pub_click` MODIFY `recordId` INT(11) COMMENT '推送消息记录ID';
 
+ALTER TABLE `mesg_record` MODIFY `pubMessageId` INT(11) COMMENT '运营消息任务记录ID';
+
 create index Index_mobile on mesg_pub_receiver(mobile);
