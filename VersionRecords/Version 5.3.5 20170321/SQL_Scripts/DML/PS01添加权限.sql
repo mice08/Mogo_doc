9d8cf9c0d66a1fee5017ec1f2af73e46408832e1
dongshan <sundongshan@mogoroom.com>
2017-03-16 16:11:42 +0800

    签约单权限

--- VersionRecords/Version 5.3.5 20170321/SQL_Scripts/DML/PS01添加权限.sql
+++ VersionRecords/Version 5.3.5 20170321/SQL_Scripts/DML/PS01添加权限.sql
@@ -1,4 +1,4 @@
-/*
+﻿/*
 添加bs签约单列表--》获取账单列表查询权限
 添加bs签约单列表--》获取操作记录查询权限
 添加bs签约单列表--》获取账单详情查询权限
@@ -9,6 +9,7 @@ use mogoroomdb;
 
 
 BEGIN;
+
 -- 获取账单列表
 SELECT @id:=id FROM perm_functioninfo WHERE fname = '签约单列表' AND (furl IS NULL OR furl = 'signedOrder/list') ORDER BY id DESC LIMIT 1;
 SELECT @seq:=(MAX(seq)+1) FROM perm_functioninfo WHERE functionpId=@id;
@@ -32,4 +33,5 @@ SELECT @fcode2:= CONCAT('2',LPAD(@id2,4,0),@seq2) FROM perm_functioninfo WHERE f
 
 INSERT INTO `perm_functioninfo` ( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
 VALUES ( @fcode2, '获取账单详情', 'signedOrder/detailBillInfo', @seq2, '3', @id2, '1', NULL, NULL, NULL, '0', '1', NULL, NULL, NULL, NULL, '1', '2');
+
 COMMIT ;
