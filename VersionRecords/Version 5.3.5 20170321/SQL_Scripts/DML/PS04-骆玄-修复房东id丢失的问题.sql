8a7b205e6d41118938ce97f8e8d6c9581cf26e95
qwb <qwb@mogoroom.com>
2017-03-20 12:05:58 +0800

    修改sql

--- VersionRecords/Version 5.3.5 20170321/SQL_Scripts/DML/PS04-骆玄-修复房东id丢失的问题.sql
+++
@@ -1,7 +0,0 @@
-/*BD系统：修复bd房东数据*/
-use mogoroomdb;
-
-
-UPDATE bizd.`bizd_user_landlord`
-INNER JOIN user_landlord ON bizd_user_landlord.phone = user_landlord.phone
-SET bizd_user_landlord.landlordId = user_landlord.id
