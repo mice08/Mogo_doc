de453aee18448a644d8a3e8e15fa38b1254f19cb
xuwenrui <xuwenrui@mogoroom.com>
2017-03-21 17:19:12 +0800

    opt@develop#:修改个sql规范

--- VersionRecords/Version TP2/SQL_Scripts/DDL/MD01.TP房源标签表.sql
+++ VersionRecords/Version TP2/SQL_Scripts/DDL/MD01.TP房源标签表.sql
@@ -1,9 +1,14 @@
+/*  Database name `mogoroomdb`  增加tp房源资质标签表*/
 use mogoroomdb;
 /*
-新建tp房源资质标签表
+1、预估表容量:所有tp房源数量/每月（先tp房间1000间，后面会有所增长）
+2、每次读取量:小于500
+3、主要查询
+select id, roomId, tagId, isAuto, createBy, createByType,createTime, updateTime, updateBy, updateByType
+where valid=1 tagId=123 and roomId in (...);
 */
 CREATE TABLE `flat_power_tag` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主健',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主健',
   `roomId` int(11) NOT NULL COMMENT '房间ID',
   `tagId` int(11) NOT NULL COMMENT '标签ID(参考flat_tag表)',
   `isAuto` tinyint(3) NOT NULL DEFAULT '1' COMMENT '是不是系统添加(1:是 0:否)',
@@ -15,6 +20,8 @@ CREATE TABLE `flat_power_tag` (
   `updateByType` int(11) NOT NULL COMMENT '修改人类型(参考字典表组名:userType)',
   `soDoneCode` int(11) NOT NULL COMMENT '操作流水号',
   `valid` tinyint(3) NOT NULL DEFAULT '1' COMMENT '数据状态(1:有效 0:删除)',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `INX_ROOMID` (`roomId`),
+  KEY `INX_TAGID` (`tagId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='tp房源资质标签表';
 
