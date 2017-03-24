a33ab48370174c690116efa8179ec5f98eb64eb9
mhh <mhh@mogoroom.com>
2017-03-24 11:30:38 +0800

    opt@jgz: 脚本修改

--- VersionRecords/Version mybank/SQL_Scripts/DDL/JG02.网商银行对接申请扩展表添加字段.sql
+++ VersionRecords/Version mybank/SQL_Scripts/DDL/JG02.网商银行对接申请扩展表添加字段.sql
@@ -6,5 +6,5 @@ USE mogoroomdb;
 alter table loan_contract_mybank
 add column loginId varchar(16) null comment '会员登录帐号，比如支付宝登陆帐号，一般为手机号或邮箱',
 add column outMemId int(11) null comment '外部机构定义的客户编号',
-add column requestId varchar(16) not null comment '外部流水号',
-add column alipayId varchar(16) not null comment '客户的支付宝ID';
+add column requestId varchar(16) not null default '0' comment '外部流水号',
+add column alipayId varchar(16) not null default '0' comment '客户的支付宝ID';
