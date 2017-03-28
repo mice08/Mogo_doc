f0407c20d88e97de3b3a9a63121f72d445a2fe63
zhutong <zhutong@mogoroom.com>
2017-03-28 19:00:04 +0800

    房源增量同步脚本及配置文件

---
+++ VersionRecords/Version ESReconsituti/SQL_Scripts/DDL/EQ02.房源同步补充.sql
@@ -0,0 +1,18 @@
+/* Database name `mogoroomdb` */
+USE mogoroomdb;
+
+/*商圈表添加系统字段 */
+ALTER TABLE city_business_area ADD COLUMN updateTime DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间';
+/*房间图片表添加系统字段 */
+ALTER TABLE flat_room_picture ADD COLUMN updateTime DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间';
+
+/*房源数据综合表添加系统字段 */
+ALTER TABLE find_room_detail_record ADD (
+   flatsId INT(11) COMMENT '公寓id',
+   cityId INT(11) COMMENT '城市id',
+   districtId INT(11) COMMENT '行政区id',
+   areaId INT(11) COMMENT '商圈id',
+   communityId INT(11) COMMENT '小区id',
+   tpShowPrice decimal(12,2) comment 'TP房间展示价格',
+   landlordId INT(11) COMMENT '房东id'
+);
