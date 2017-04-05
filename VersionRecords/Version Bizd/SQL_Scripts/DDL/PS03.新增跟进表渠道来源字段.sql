b822c8548be562700b1e00a181bad597dd9cbce0
dongshan <sundongshan@mogoroom.com>
2017-04-05 13:59:13 +0800

    帕拉斯跟进来源

--- VersionRecords/Version Bizd/SQL_Scripts/DDL/PS03.新增跟进表渠道来源字段.sql
+++ VersionRecords/Version Bizd/SQL_Scripts/DDL/PS03.新增跟进表渠道来源字段.sql
@@ -2,4 +2,4 @@
 use bizd;
 
 -- 更近记录来源
-ALTER TABLE bizd_landlord_followup ADD column channel int not null default 19;
+ALTER TABLE bizd_landlord_followup ADD column channel int not null default 19 comment '来源：参照字典表 channel';
