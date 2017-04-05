e8652c649fa2bd56046875951c57ab19d530b5e7
田东兴 <andubu@tiandongxingdeMacBook-Pro.local>
2017-04-05 11:20:04 +0800

    opt@5.3.7#:更新脚本符号

--- VersionRecords/Version 5.3.7 20170406/SQL_Scripts/DDL/MD02.用户性别添加默认值.sql
+++ VersionRecords/Version 5.3.7 20170406/SQL_Scripts/DDL/MD02.用户性别添加默认值.sql
@@ -1,4 +1,4 @@
 ﻿use mogoroomdb;
 
 /*修改用户模块性别为非空，并且默认值为1 */
-ALTER TABLE `user_landlord` MODIFY COLUMN `sex` tinyint(1) NOT NULL default '1' COMMENT '性别（0：女 1：男）';
+ALTER TABLE `user_landlord` MODIFY COLUMN `sex` tinyint(1) NOT NULL default '1' COMMENT '性别（0:女 1:男）';
