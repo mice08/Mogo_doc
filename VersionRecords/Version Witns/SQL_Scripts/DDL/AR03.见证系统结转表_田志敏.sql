cfc8dc4f12081c87ad28b2fd7b16a5d13bc2c043
tzm <tzm@mogoroom.com>
2017-04-13 10:33:52 +0800

    修改acct_carryover脚本

--- VersionRecords/Version Witns/SQL_Scripts/DDL/AR03.见证系统结转表_田志敏.sql
+++ VersionRecords/Version Witns/SQL_Scripts/DDL/AR03.见证系统结转表_田志敏.sql
@@ -10,16 +10,19 @@ use acct;
 2、每次读取量:在2000-4000条左右
 3、主要查询根据id 或者根据时间段；
 */
+
 CREATE TABLE `acct_carryover` (
-  `id` INT(11) DEFAULT NULL COMMENT '自增id',
-  `soDoneCode` VARCHAR(50) DEFAULT NULL COMMENT '业务侧流水号',
-  `status` TINYINT(4) DEFAULT NULL COMMENT '0:待结转 1:已结转',
-  `fundChannel` TINYINT(4) DEFAULT NULL COMMENT '支付渠道',
-  `amount` DECIMAL(10,2) DEFAULT NULL COMMENT '金额',
-  `createBy` INT(11) DEFAULT NULL COMMENT '创建人',
-  `createByType` TINYINT(3) DEFAULT NULL COMMENT '创建人类型',
-  `createTime` DATETIME DEFAULT NULL COMMENT '创建时间',
-  `updateBy` INT(11) DEFAULT NULL COMMENT '更新人',
-  `updateByType` INT(11) DEFAULT NULL COMMENT '更新人类型',
-  `updateTime` DATETIME DEFAULT NULL COMMENT '更新时间'
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='网商结转表';
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `soDoneCode` varchar(50) DEFAULT NULL COMMENT '业务侧流水号',
+  `status` tinyint(4) DEFAULT NULL COMMENT '0:待结转 1:已结转',
+  `fundChannel` tinyint(4) DEFAULT NULL COMMENT '支付渠道',
+  `amount` decimal(10,2) DEFAULT NULL COMMENT '金额',
+  `createBy` int(11) DEFAULT NULL COMMENT '创建人',
+  `createByType` tinyint(3) DEFAULT NULL COMMENT '创建人类型',
+  `updateBy` int(11) DEFAULT NULL COMMENT '更新人',
+  `updateByType` int(11) DEFAULT NULL COMMENT '更新人类型',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '变更时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_createTime` (`createTime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='网商结转表';
