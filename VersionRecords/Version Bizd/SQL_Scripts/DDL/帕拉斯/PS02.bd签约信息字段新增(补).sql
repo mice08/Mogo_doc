f5bbbdd79a0cdd52eea5db86a07baf39f1a64982
dongshan <sundongshan@mogoroom.com>
2017-04-14 17:01:15 +0800

    帕拉斯新增签约信息字段

---
+++ VersionRecords/Version Bizd/SQL_Scripts/DDL/帕拉斯/PS02.bd签约信息字段新增(补).sql
@@ -0,0 +1,8 @@
+﻿/*  Database name `bizd`  签约信息 */
+
+USE bizd;
+
+ALTER TABLE bizd_landlord_sign
+  ADD COLUMN creditCode VARCHAR(20) NOT NULL COMMENT '信用代码',
+  ADD COLUMN legalRepresentative VARCHAR(20) NOT NULL COMMENT '法人代表姓名',
+  ADD COLUMN acctType TINYINT NOT NULL COMMENT '账号类型 0 个人  1 公司 2 KA';
