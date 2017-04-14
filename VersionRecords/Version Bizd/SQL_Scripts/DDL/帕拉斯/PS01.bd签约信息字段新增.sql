ceae48c887c72e58ec020e5cb3432dd1cf23abd4
dongshan <sundongshan@mogoroom.com>
2017-04-14 18:49:34 +0800

    帕拉斯新增签约信息字段

--- VersionRecords/Version Bizd/SQL_Scripts/DDL/帕拉斯/PS01.bd签约信息字段新增.sql
+++ VersionRecords/Version Bizd/SQL_Scripts/DDL/帕拉斯/PS01.bd签约信息字段新增.sql
@@ -11,8 +11,8 @@ ALTER TABLE bizd_landlord_sign
   ADD COLUMN contractDate datetime DEFAULT NULL COMMENT '签约日期',
   ADD COLUMN emergency varchar(60) DEFAULT NULL COMMENT '紧急联系人',
   ADD COLUMN emergencyPhone varchar(11) DEFAULT NULL COMMENT '紧急联系人电话',
-  ADD COLUMN bankProvinceId INT DEFAULT NULL COMMENT '省',
-  ADD COLUMN bankCityId INT DEFAULT NULL COMMENT '城市',
+  ADD COLUMN bankProvinceId int(11) DEFAULT NULL COMMENT '省',
+  ADD COLUMN bankCityId int(11) DEFAULT NULL COMMENT '城市',
   ADD COLUMN bankId int(11) DEFAULT NULL COMMENT '银行ID',
   ADD COLUMN bankName varchar(16) DEFAULT NULL COMMENT '银行名称',
   ADD COLUMN bankBranch varchar(50) DEFAULT NULL COMMENT '支行名称',
@@ -20,4 +20,8 @@ ALTER TABLE bizd_landlord_sign
   ADD COLUMN bankAccountNum varchar(20) DEFAULT NULL COMMENT '银行卡号',
   ADD COLUMN officialWebName varchar(20) DEFAULT NULL COMMENT '官网联系人姓名',
   ADD COLUMN officialWebPhone varchar(11) DEFAULT NULL COMMENT '官网联系人手机号',
-  ADD COLUMN officialWebSex TINYINT DEFAULT NULL COMMENT '官网联系人性别 ( 0 女 1 男)';
+  ADD COLUMN officialWebSex TINYINT(1) DEFAULT NULL COMMENT '官网联系人性别 ( 0 女 1 男)',
+  ADD COLUMN creditCode VARCHAR(20) NOT NULL COMMENT '信用代码',
+  ADD COLUMN legalRepresentative VARCHAR(20) NOT NULL COMMENT '法人代表姓名',
+  ADD COLUMN signCount INT(11) NOT NULL DEFAULT 0 COMMENT '签约体量',
+  ADD COLUMN acctType TINYINT(1) NOT NULL COMMENT '账号类型 0 个人  1 公司 2 KA';
