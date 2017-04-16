be88f79565738af9b3e80f78862ea96cb4bc8a68
dongshan <sundongshan@mogoroom.com>
2017-04-16 16:10:32 +0800

    帕拉斯新增签约信息字段

--- VersionRecords/Version Bizd/SQL_Scripts/DDL/帕拉斯/PS01.bd签约信息字段新增.sql
+++ VersionRecords/Version Bizd/SQL_Scripts/DDL/帕拉斯/PS01.bd签约信息字段新增.sql
@@ -5,7 +5,7 @@ USE bizd;
 ALTER TABLE bizd_landlord_sign
   ADD COLUMN partyAName VARCHAR(50) NOT NULL COMMENT '合同甲方',
   ADD COLUMN partyAPhone VARCHAR(11) NOT NULL COMMENT '合同甲方电话',
-  ADD COLUMN cardType VARCHAR(20) COMMENT '证件类型,参考字典表组名:credential',
+  ADD COLUMN cardType VARCHAR(20) COMMENT '证件类型（参考字典表组名:credential）',
   ADD COLUMN cardNum VARCHAR(20) COMMENT '证件号码',
   ADD COLUMN contactAddr VARCHAR(50) COMMENT '联系地址',
   ADD COLUMN contrctDate datetime COMMENT '签约日期',
@@ -20,9 +20,9 @@ ALTER TABLE bizd_landlord_sign
   ADD COLUMN bankAcctNum varchar(20) COMMENT '银行卡号',
   ADD COLUMN officlWebsitName varchar(20) COMMENT '官网联系人姓名',
   ADD COLUMN officlWebsitPhone varchar(11) COMMENT '官网联系人手机号',
-  ADD COLUMN officlWebsitSex TINYINT(1) COMMENT '官网联系人性别，参考字典表组名:sex',
+  ADD COLUMN officlWebsitSex TINYINT(1) COMMENT '官网联系人性别（参考字典表组名:sex）',
   ADD COLUMN creditCode VARCHAR(20) COMMENT '信用代码',
   ADD COLUMN legalRpstiv VARCHAR(20) NOT NULL COMMENT '法人代表姓名',
   ADD COLUMN signCount INT(11) NOT NULL DEFAULT 0 COMMENT '签约体量',
-  ADD COLUMN acctType TINYINT(1) COMMENT '账号类型,参考字典表组名:landlordType',
+  ADD COLUMN acctType TINYINT(1) COMMENT '账号类型（参考字典表组名:landlordType）',
   ADD COLUMN remark VARCHAR(400) COMMENT '备注';
