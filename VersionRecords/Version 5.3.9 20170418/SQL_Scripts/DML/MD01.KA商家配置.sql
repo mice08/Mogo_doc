54ec20199601d9a2307ed671331118f5af5456d6
zhangfei <zhangfei@mogoroom.com>
2017-04-18 13:07:12 +0800

    KA配置

---
+++ VersionRecords/Version 5.3.9 20170418/SQL_Scripts/DML/MD01.KA商家配置.sql
@@ -0,0 +1,4 @@
+/*  Database name `mogoroomdb`  KA 商家配置 */
+use mogoroomdb;
+/** 魔方公寓配置 影响条数1 **/
+UPDATE `open_api_user` SET HOST = '139.196.90.183',partnerName='魔方公寓',publicKey='MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVlKsTllcJKkenlLcT9d1J0hntsIierYQ7zZ5geOAgNAWsOlP9oy244fE68qfUUr3LYIsELMn8c9n2IrILlu5nVU73lnJLUVHZF7rLEh0LxwZ4dAMivn11gCDGO+yVu6SLSX959yv1k87/+V3MWKEc2RCyEQHUnvA9g889xecZUwIDAQAB' WHERE userId =3106178 AND userType=0;
