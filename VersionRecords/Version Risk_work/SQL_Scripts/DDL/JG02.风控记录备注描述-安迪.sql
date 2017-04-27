808fe60305911199848fbda78c214cab967ee1a2
yhq <andy@mogoroom.com>
2017-04-27 12:09:36 +0800

    update

--- VersionRecords/Version Risk_work/SQL_Scripts/DDL/JG02.风控记录备注描述-安迪.sql
+++ VersionRecords/Version Risk_work/SQL_Scripts/DDL/JG02.风控记录备注描述-安迪.sql
@@ -3,5 +3,5 @@ use mogoroomdb;
 
 ALTER TABLE `risk_record`   
   ADD COLUMN `hitSubject` VARCHAR(32) NULL COMMENT '命中主体',
-  ADD COLUMN `renterId` INT(11) NULL COMMENT '租客id' AFTER ,
+  ADD COLUMN `renterId` INT(11) NULL COMMENT '租客id' ,
   ADD COLUMN `remark` VARCHAR(256) NULL COMMENT '风险描述备注';
