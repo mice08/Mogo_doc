0ace88efa17558d2047e160ff7d8676b6cb8378b
qwb <qwb@mogoroom.com>
2017-05-03 20:25:20 +0800

    添加sql

---
+++ VersionRecords/Version DC/Version 1.0.4/SQL_Scripts/DDL/PS02.新增统计字段.sql
@@ -0,0 +1,9 @@
+﻿/*dc库相关表添加统计字段*/
+use dc;
+ALTER TABLE `dc`.`dc_land_stat_inc_day`   ADD COLUMN `TPReservationCount` INT(6) NULL  COMMENT 'TP预约单量';
+ALTER TABLE `dc`.`dc_land_stat_inc_day`   ADD COLUMN `allCallCount` INT(6) NULL  COMMENT '400电话拨打量';
+ALTER TABLE `dc`.`dc_land_stat_inc_day`   ADD COLUMN `answedCallCount` INT(6) NULL  COMMENT '400电话接听量';
+ALTER TABLE `dc`.`dc_land_stat_inc_day`   ADD COLUMN `callRenterCount` INT(6) NULL  COMMENT '获客数量';
+ALTER TABLE `dc`.`dc_land_stat_total_day`   ADD COLUMN `allCallCount` INT(6) NULL  COMMENT '400电话拨打量';
+ALTER TABLE `dc`.`dc_land_stat_total_day`   ADD COLUMN `answedCallCount` INT(6) NULL  COMMENT '400电话接听量';
+ALTER TABLE `dc`.`dc_land_stat_total_day`   ADD COLUMN `callRenterCount` INT(6) NULL  COMMENT '获客数量';
