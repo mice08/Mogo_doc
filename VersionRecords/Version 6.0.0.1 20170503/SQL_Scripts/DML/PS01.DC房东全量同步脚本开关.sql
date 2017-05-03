d02ed1b2e4b00a10ce329b900eb9132a0ef117d4
zlj <zlj@mogoroom.com>
2017-05-03 16:09:44 +0800

    上线清单配置

---
+++ VersionRecords/Version 6.0.0.1 20170503/SQL_Scripts/DML/PS01.DC房东全量同步脚本开关.sql
@@ -0,0 +1,5 @@
+﻿use mogoroomdb;
+
+/*添加dc全量同步房东定时任务开关*/
+
+INSERT INTO `comm_sysconfig` (`keyName`, `val`, `groupName`, `name`, `remark`, `updateTime`, `updateByType`, `updateBy`, `extra1`, `extra2`) VALUES ('DcLandDtlRealTask', '1', 'dcTask', '数据中心-房东-明细-实时-天（DC）', 'val=1(启用)val=0(关闭)', now(), '0', '0', NULL, NULL);
