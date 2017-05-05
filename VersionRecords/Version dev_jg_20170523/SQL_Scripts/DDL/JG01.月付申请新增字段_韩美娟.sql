9b17f3fb0caf18f398c5dc9c6416e1231b7b90ea
hmj <hmj@mogoroom.com>
2017-05-05 19:26:18 +0800

    修改字段名称

--- VersionRecords/Version dev_jg_20170523/SQL_Scripts/DDL/JG01.月付申请新增字段_韩美娟.sql
+++ VersionRecords/Version dev_jg_20170523/SQL_Scripts/DDL/JG01.月付申请新增字段_韩美娟.sql
@@ -2,10 +2,10 @@
 USE mogoroomdb;
 
 ALTER TABLE loan_mpayapply_approve   
-ADD COLUMN remark3 VARCHAR(256) NULL  COMMENT '拓展经理审核备注';
+ADD COLUMN busiManagerRemark VARCHAR(256) NULL  COMMENT '拓展经理审核备注';
 
 ALTER TABLE loan_mpayapply_approve_log
-ADD COLUMN remark3 VARCHAR(256) NULL  COMMENT '拓展经理审核备注';
+ADD COLUMN busiManagerRemark VARCHAR(256) NULL  COMMENT '拓展经理审核备注';
 
 ALTER TABLE loan_mpayapply_collection  
 ADD COLUMN busiManagerRemark VARCHAR(128) NULL  COMMENT '经理意见' AFTER STATUS,
