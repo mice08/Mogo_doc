9212421eef620be85e32c25641790a23bb40436d
lvchongxin <lcx@mogoroom.com>
2017-05-06 14:29:19 +0800

    opt 修改表结构

---
+++ VersionRecords/Version Risk_work/SQL_Scripts/DDL/JG05.风控记录表-吕崇新.sql
@@ -0,0 +1,7 @@
+﻿/*  Database name `mogoroomdb`  风控记录表 */
+use mogoroomdb;
+
+
+ALTER TABLE `risk_record`
+DROP COLUMN `riskRuleType`,
+CHANGE COLUMN `riskRuleId` `riskVerifyId`  int(11) NULL DEFAULT NULL COMMENT '审核记录表id' AFTER `verifyUserId`;
