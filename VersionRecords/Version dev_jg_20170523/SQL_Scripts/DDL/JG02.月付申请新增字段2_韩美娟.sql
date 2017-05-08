c14c50bd8e7154a4bcb75672579d9028825ae88d
hmj <hmj@mogoroom.com>
2017-05-08 13:13:11 +0800

    新增字段

--- VersionRecords/Version dev_jg_20170523/SQL_Scripts/DDL/JG02.月付申请新增字段2_韩美娟.sql
+++ VersionRecords/Version dev_jg_20170523/SQL_Scripts/DDL/JG02.月付申请新增字段2_韩美娟.sql
@@ -6,3 +6,9 @@ ALTER TABLE loan_mpayapply_approve
   ADD COLUMN fileGroupId1 INT NULL  COMMENT '签约合同文件组id',
   ADD COLUMN fileGroupId2 INT NULL  COMMENT '信审报告文件组id',
   ADD COLUMN fileGroupId3 INT NULL  COMMENT '授权委托书文件组id';
+
+ALTER TABLE loan_mpayapply_approve_log
+  ADD COLUMN isUpload INT(1) DEFAULT 0  NULL  COMMENT '是否是上传操作(0:否 1:是)',
+  ADD COLUMN fileGroupId1 INT NULL  COMMENT '签约合同文件组id',
+  ADD COLUMN fileGroupId2 INT NULL  COMMENT '信审报告文件组id',
+  ADD COLUMN fileGroupId3 INT NULL  COMMENT '授权委托书文件组id';
