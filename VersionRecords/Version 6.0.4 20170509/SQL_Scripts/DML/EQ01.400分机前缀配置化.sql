8e824f72171f06446f26e77276ee0f3f353cb271
jingang <jg@mogoroom.com>
2017-05-08 15:58:13 +0800

    new@6.0.4#: 添加EQ组配置

---
+++ VersionRecords/Version 6.0.4 20170509/SQL_Scripts/DML/EQ01.400分机前缀配置化.sql
@@ -0,0 +1,17 @@
+﻿/* Database name `mogoroomdb` */
+USE mogoroomdb;
+
+/* 系统配置表新增400分机号城市前缀配置 */
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_289','9','tele_city','上海','上海',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_131','6','tele_city','北京','北京',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_340','8','tele_city','深圳','深圳',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_179','5','tele_city','杭州','杭州',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_315','5','tele_city','南京','南京',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_75','7','tele_city','成都','成都',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_132','7','tele_city','重庆','重庆',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_233','1','tele_city','西安','西安',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_268','1','tele_city','郑州','郑州',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_218','2','tele_city','武汉','武汉',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_332','2','tele_city','天津','天津',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_224','3','tele_city','苏州','苏州',NOW(),5,0);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('city_257','3','tele_city','广州','广州',NOW(),5,0);
---
+++ VersionRecords/Version 6.0.4 20170509/SQL_Scripts/ROLLBACK/EQ01.400分机前缀配置化_rollback.sql
@@ -0,0 +1,5 @@
+﻿/* Database name `mogoroomdb` */
+USE mogoroomdb;
+
+/* 系统配置表新增400分机号城市前缀配置-回滚，影响记录条数：13  。这个表没有逻辑删除字段，只能物理删除 */
+DELETE FROM comm_sysconfig WHERE groupname='tele_city'
