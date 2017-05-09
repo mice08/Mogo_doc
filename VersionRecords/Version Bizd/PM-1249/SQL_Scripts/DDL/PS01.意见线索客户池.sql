7337f7a29f4cbb49d5f5177a688bac2c3268d4dc
dongshan <sundongshan@mogoroom.com>
2017-05-09 17:09:56 +0800

    bd2期

--- VersionRecords/Version Bizd/PM-1249/SQL_Scripts/DDL/PS01.意见线索客户池.sql
+++ VersionRecords/Version Bizd/PM-1249/SQL_Scripts/DDL/PS01.意见线索客户池.sql
@@ -6,8 +6,3 @@ ALTER TABLE comm_suggestion
   ADD COLUMN `remark`  VARCHAR(500) NULL COMMENT '备注' AFTER `suggestionType`,
   ADD COLUMN `updateBy` INT(11) NULL COMMENT '更新人' AFTER `remark`,
   ADD COLUMN `updateTime` DATETIME NULL COMMENT '更新时间/处理时间' AFTER `updateBy`;
-
-USE bizd;
-ALTER TABLE bizd_landlord_followup
-  ADD COLUMN `channel` int(11) NULL COMMENT '来源渠道(参照字典表组名:channel)',
-  ADD COLUMN `communicateTime` DATETIME NULL COMMENT '沟通时间';
