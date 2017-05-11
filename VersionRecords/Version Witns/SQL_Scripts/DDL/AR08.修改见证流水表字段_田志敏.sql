337a18bd274b32d699cfcc2bb0418b1d8ea8c4f8
tzm <tzm@mogoroom.com>
2017-05-11 13:10:16 +0800

    opt@witns#:修改见证流水表字段

---
+++ VersionRecords/Version Witns/SQL_Scripts/DDL/AR08.修改见证流水表字段_田志敏.sql
@@ -0,0 +1,9 @@
+/* 
+Database name `acct`
+修改见证系统流水表状态字段
+*/
+
+use acct;
+ALTER TABLE `acct_paylog_mybank` modify COLUMN `fail_reason` varchar(500) NULL COMMENT '失败原因';
+
+ALTER TABLE `acct_paylog_mybank` modify COLUMN `refund_status` varchar(50) NULL COMMENT '退款状态';
