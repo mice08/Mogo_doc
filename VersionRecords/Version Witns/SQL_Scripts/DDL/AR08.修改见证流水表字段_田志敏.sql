7af27943424254b887239fcdd211e6e4725bb7ae
tzm <tzm@mogoroom.com>
2017-05-11 15:59:59 +0800

    new@wintns#:加长用户标识字段

--- VersionRecords/Version Witns/SQL_Scripts/DDL/AR08.修改见证流水表字段_田志敏.sql
+++ VersionRecords/Version Witns/SQL_Scripts/DDL/AR08.修改见证流水表字段_田志敏.sql
@@ -7,3 +7,5 @@ use acct;
 ALTER TABLE `acct_paylog_mybank` modify COLUMN `fail_reason` varchar(500) NULL COMMENT '失败原因';
 
 ALTER TABLE `acct_paylog_mybank` modify COLUMN `refund_status` varchar(50) NULL COMMENT '退款状态';
+
+ALTER TABLE `acct_paylog_mybank` modify COLUMN `uid` varchar(50) NULL COMMENT '用户标识';
