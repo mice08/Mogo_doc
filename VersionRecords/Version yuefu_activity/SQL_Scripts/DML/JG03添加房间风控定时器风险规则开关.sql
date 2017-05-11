d5f32ae0c0350b5e8fb40f3dc9b161659882e92c
lvchongxin <lcx@mogoroom.com>
2017-05-11 15:35:41 +0800

    opt # 新增开关脚本

--- VersionRecords/Version yuefu_activity/SQL_Scripts/DML/JG03添加房间风控定时器风险规则开关.sql
+++ VersionRecords/Version yuefu_activity/SQL_Scripts/DML/JG03添加房间风控定时器风险规则开关.sql
@@ -4,6 +4,7 @@ USE mogoroomdb;
 
 INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('WZ-HZ-RM1-001',1,'riskRoom','房间风控定时器风险规则开关','val=1(启用)val=0(关闭)',NOW(),6,1);
 INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('WZ-HZ-MJ-001',1,'riskRoom','房间风控定时器风险规则开关','val=1(启用)val=0(关闭)',NOW(),6,1);
+INSERT INTO comm_sysconfig(keyname,val,groupname,NAME,remark,updatetime,updatebytype,updateby) VALUES('FD-Acct-Login-001',1,'riskRule','定时器风险规则','val=1(启用)val=0(关闭)',NOW(),6,1);
 
 
 
