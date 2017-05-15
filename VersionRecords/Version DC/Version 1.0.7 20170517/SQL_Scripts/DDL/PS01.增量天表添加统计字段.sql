5e2ce900a5b432dd0442d5c02a93ae2776725f15
qwb <qwb@mogoroom.com>
2017-05-15 14:45:57 +0800

    修改

--- VersionRecords/Version DC/Version 1.0.7 20170517/SQL_Scripts/DDL/PS01.增量天表添加统计字段.sql
+++ VersionRecords/Version DC/Version 1.0.7 20170517/SQL_Scripts/DDL/PS01.增量天表添加统计字段.sql
@@ -1,3 +1,5 @@
-﻿ALTER TABLE `dc`.`dc_land_stat_inc_day`   
+﻿use dc;
+
+ALTER TABLE `dc`.`dc_land_stat_inc_day`   
   ADD COLUMN `inAmountZhifubao` DECIMAL(10,2) NULL  COMMENT '支付宝总收入' AFTER `callRenterCount`,
   ADD COLUMN `inAmountWeixin` DECIMAL(10,2) NULL  COMMENT '微信总收入' AFTER `inAmountZhifubao`;
