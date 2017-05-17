663e3b891b58aff66428040bc4ab2c4992b134e7
yhq <andy@mogoroom.com>
2017-05-17 16:12:02 +0800

    opt@dev#:风控菜单

---
+++ VersionRecords/Version Risk_work/SQL_Scripts/DML/JG03.风控房源数据初始化-安迪.sql
@@ -0,0 +1,8 @@
+﻿/*风控房源数据初始化*/
+USE mogoroomdb;
+BEGIN;
+
+INSERT INTO `comm_sysconfig` ( `keyName`, `val`, `groupName`, `name`, `remark`, `updateTime`, `updateByType`, `updateBy`, `extra1`, `extra2`)
+VALUES('WZ-ALL-Hid-001','1','riskRoom','房间风控定时器风险规则开关','val=1(启用)val=0(关闭)',CURRENT_TIMESTAMP,6,1,NULL,NULL);
+
+COMMIT;
