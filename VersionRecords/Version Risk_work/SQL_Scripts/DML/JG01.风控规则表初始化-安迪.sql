240aa7b3f1dccf4d33e496e09590cf568eb8fc10
yhq <andy@mogoroom.com>
2017-05-17 15:37:31 +0800

    opt@dev#:风控数据初始化

--- VersionRecords/Version Risk_work/SQL_Scripts/DML/JG01.风控规则表初始化-安迪.sql
+++ VersionRecords/Version Risk_work/SQL_Scripts/DML/JG01.风控规则表初始化-安迪.sql
@@ -1,29 +1,36 @@
 /*  Database name `mogoroomdb`  风控规则表初始化 */
 use mogoroomdb;
 
+begin;
 
+insert  into `risk_rule`(`id`,`ruleType`,`ruleKey`,`ruleStatus`,`isSysprocess`) values 
+(1,1,'W-HZ-DJ-L-01',1,0),
+(2,1,'W-HZ-MJ-H-02',1,0),
+(3,2,'ALL-CZZT-03',1,1),
+(4,1,'W-HZ-MJ-JG-01',1,0),
+(5,1,'W-ZZ-MJ-JG-01',1,0),
+(6,3,'PT-JY-001',1,0),
+(7,3,'PT-JY-002',1,0),
+(8,3,'PT-JY-003',1,0),
+(9,3,'PT-JY-004',1,0),
+(10,4,'MH-FD-001',1,0),
+(11,4,'MH-FD-002',1,0),
+(12,4,'MH-FD-003',1,0),
+(13,4,'MH-ZK-001',1,0),
+(14,4,'MH-ZK-002',1,0),
+(15,4,'MH-ZK-003',1,0),
+(16,3,'TX-ZK-001',1,0),
+(17,3,'TX-POS-002',1,0),
+(18,5,'MR-JE-01',1,0),
+(19,3,'PT-JY-PayID-FDs-001',1,0),
+(20,3,'PT-JY-PayID-ZKs-001',1,0),
+(21,3,'PT-JY-Contr-CHs-001',1,0),
+(22,1,'WZ-HZ-RMs-M-001',1,0),
+(23,1,'WZ-HZ-RMs-M-002',1,0),
+(24,2,'FD-Acct-Login-001',1,1),
+(25,1,'WZ-HZ-RM1-001',1,1),
+(26,1,'WZ-HZ-MJ-001',1,1),
+(28,2,'FD-MGS-001',1,1),
+(29,2,'FD-MGS-002',1,1);
 
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('1','1','W-HZ-DJ-L-01','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('2','1','W-HZ-MJ-H-02','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('3','2','ALL-CZZT-03','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('4','1','W-HZ-MJ-JG-01','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('5','1','W-ZZ-MJ-JG-01','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('6','3','PT-JY-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('7','3','PT-JY-002','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('8','3','PT-JY-003','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('9','3','PT-JY-004','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('10','4','MH-FD-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('11','4','MH-FD-002','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('12','4','MH-FD-003','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('13','4','MH-ZK-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('14','4','MH-ZK-002','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('15','4','MH-ZK-003','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('16','3','TX-ZK-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('17','3','TX-POS-002','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('18','5','MR-JE-01','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('19','3','PT-JY-PayID-FDs-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('20','3','PT-JY-PayID-ZKs-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('21','3','PT-JY-Contr-CHs-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('22','1','WZ-HZ-RMs-M-001','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('23','1','WZ-HZ-RMs-M-002','1');
-insert into `risk_rule` (`id`, `ruleType`, `ruleKey`, `ruleStatus`) values('24','2','FD-Acct-Login-001','1');
+commit;
---
+++ VersionRecords/Version Risk_work/SQL_Scripts/DML/JG02.bs风险工作台菜单_回滚-吕崇新.sql
@@ -0,0 +1,146 @@
+﻿/**默认不提交事务**/
+begin;
+/**获取开始时间**/
+select @begindate:=NOW();
+/**根据节点名称读取父节点id和level**/
+select @rootId:=id,@rootLevel:=functionLevel from perm_functioninfo where fname = '风控管理';
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@rootId;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@rootLevel+1,lpad(@rootId,4,0),@seq ) from dual;
+/**插入新节点记录-'房东风险工作台'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '房东风险工作台', '', @seq, @rootLevel+1, @rootId, 1, NULL, NULL, NULL, 0, 1, NULL, now(), NULL, NULL, 1, 2);
+
+/**读取父节点id和level，防止重名，根据时间查**/
+select @parentid2:=id,@level2:=functionLevel from perm_functioninfo where fname = '房东风险工作台' and createdTime>=@begindate;
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid2;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level2+1,lpad(@parentid2,4,0),@seq ) from dual;
+/**插入新节点记录-'房东风险工作台'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '房东风险工作台', 'risk/workbrench', @seq, @level2+1, @parentid2, 1, NULL, NULL, NULL, 0, 1, NULL, now(), NULL, NULL, 1, 2);
+
+/**读取父节点id和level，防止重名，根据时间查**/
+select @parentid3:=id,@level3:=functionLevel from perm_functioninfo where fname = '房东风险工作台' and createdTime>=@begindate;
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'房东风险审核'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '房东风险审核', 'risk/findLandlordRiskVerify', @seq, @level3+1, @parentid3, 0, NULL, NULL, NULL, 0, 0, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'权限控制记录'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '权限控制记录', 'risk/findAuthorityControlRecord', @seq, @level3+1, @parentid3, 0, NULL, NULL, NULL, 0, 0, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'命中规则记录'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '命中规则记录', 'risk/findHitRuleRecord', @seq, @level3+1, @parentid3, 0, NULL, NULL, NULL, 0, 0, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'审核记录'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '审核记录', 'risk/findVerifyRecord', @seq, @level3+1, @parentid3, 0, NULL, NULL, NULL, 0, 0, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'风险池'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '风险池', 'risk/disposePool', @seq, @level3+1, @parentid3, 1, NULL, NULL, NULL, 0, 1, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'改变房东风险的状态'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '改变房东风险的状态', 'risk/changeRiskLandlordRiskStatus', @seq, @level3+1, @parentid3, 0, NULL, NULL, NULL, 0, 0, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'规则统计'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '规则统计', 'risk/ruleStatistics', @seq, @level3+1, @parentid3, 1, NULL, NULL, NULL, 0, 1, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'审核统计'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '审核统计', 'risk/verifyStatistics', @seq, @level3+1, @parentid3, 1, NULL, NULL, NULL, 0, 1, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'房东统计'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '房东统计', 'risk/landlordStatistics', @seq, @level3+1, @parentid3, 1, NULL, NULL, NULL, 0, 1, NULL, now(), NULL, NULL, 1, 2);
+
+/**生成新的顺序号,null默认是1**/
+select @seq:=(max(seq)+1) from perm_functioninfo where functionpId=@parentid3;
+select @seq:=(case when @seq is null then 1 else @seq end);
+
+/**生成新的fcode**/
+select @fcode:= CONCAT(@level3+1,lpad(@parentid3,4,0),@seq ) from dual;
+/**插入新节点记录-'操作详情'**/
+INSERT INTO `perm_functioninfo`
+( `fcode`, `fname`, `furl`, `seq`, `functionLevel`, `functionpId`, `functionisMenu`, `functionVcode`, `functionParam`, `functionFaclass`, `isAjax`, `functionType`, `createdBy`, `createdTime`, `updatedBy`, `updatedTime`, `status`, `channel`)
+VALUES (@fcode, '操作详情', 'risk/landlordOpereateRecord', @seq, @level3+1, @parentid3, 1, NULL, NULL, NULL, 0, 1, NULL, now(), NULL, NULL, 1, 2);
+
+
+commit;
